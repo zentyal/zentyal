3d4984c8eb830f0b3a9b112e5bd06cd67e6cd64e
Ricardo Muñoz <rmunoz@warp.es>
2005-07-01 13:50:48 +0000

    added cgis to modify virtual domains maildir size
    git-svn-id: http://svn.zentyal.org/zentyal/trunk@4147 866bc4c1-54da-0310-a958-9d5a9a0e8d04

---
+++ client/mail/src/EBox/CGI/EditVDomain.pm
@@ -0,0 +1,55 @@
+# Copyright (C) 2005 Warp Netwoks S.L., DBS Servicios Informaticos S.L.
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License, version 2, as
+# published by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+package EBox::CGI::Mail::EditVDomain;
+
+use strict;
+use warnings;
+
+use base 'EBox::CGI::ClientBase';
+
+use EBox::Global;
+use EBox::Gettext;
+use EBox::Mail;
+
+## arguments:
+## 	title [required]
+sub new {
+	my $class = shift;
+	my $self = $class->SUPER::new('title' => 'Mail', 
+		'template' => '/mail/editvdomains.mas', @_);
+	$self->{domain} = "ebox-mail";	
+	#$self->{redirect} = "Mail/VDomains";	
+	bless($self, $class);
+	return $self;
+}
+
+sub _process($) {
+	my $self = shift;
+	my $mail = EBox::Global->modInstance('mail');
+ 
+	$self->_requireParam('vdomain', __('vdomain'));
+
+	my $vdomain = $self->param('vdomain');
+	print STDERR "El vdomain es $vdomain\n";
+	
+	my @array = ();
+	push(@array, 'vdomain' => $vdomain);
+	push(@array, 'mdsize' => $mail->{vdomains}->getMDSize($vdomain));
+
+	$self->{params} = \@array;
+}
+
+1;
---
+++ client/mail/src/EBox/CGI/ModifyVDomain.pm
@@ -0,0 +1,52 @@
+# Copyright (C) 2005 Warp Netwoks S.L., DBS Servicios Informaticos S.L.
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License, version 2, as
+# published by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+package EBox::CGI::Mail::ModifyVDomain;
+
+use strict;
+use warnings;
+
+use base 'EBox::CGI::ClientBase';
+
+use EBox::Global;
+use EBox::Gettext;
+use EBox::Mail;
+
+## arguments:
+## 	title [required]
+sub new {
+	my $class = shift;
+	my $self = $class->SUPER::new('title' => 'Mail', @_);
+	$self->{domain} = "ebox-mail";	
+	$self->{redirect} = "Mail/VDomains";	
+	bless($self, $class);
+	return $self;
+}
+
+sub _process($) {
+	my $self = shift;
+	my $mail = EBox::Global->modInstance('mail');
+ 
+	$self->_requireParam('vdomain', __('vdomain'));
+	$self->_requireParam('mdsize', __('mdsize'));
+
+	my $vdomain = $self->param('vdomain');
+	my $mdsize = $self->param('mdsize');
+	print STDERR "El vdomain es $vdomain, el mdsize $mdsize\n";
+	
+	$mail->{vdomains}->setMDSize($vdomain, $mdsize);
+}
+
+1;
