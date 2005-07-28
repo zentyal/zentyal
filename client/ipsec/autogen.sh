cbb7838e95f9543c296229fde2c8ba246db717ef
Guillermo Ontañon <gontanon@warp.es>
2005-07-28 19:18:42 +0000

    fix build mess
    git-svn-id: http://svn.zentyal.org/zentyal/trunk@4229 866bc4c1-54da-0310-a958-9d5a9a0e8d04

--- client/ipsec/autogen.sh
+++ client/ipsec/autogen.sh
@@ -1,13 +1,12 @@
 #!/bin/sh
 
-if [ -f Makefile ]
-then
+if [ -f Makefile ] ; then
 	make maintainer-clean
 fi
-po-am.generator > po/Makefile.am
-mkdir -p config
-aclocal -I m4
-autoconf
-automake --add-missing
 
-./configure $*
+mkdir -p po
+./tools/po-am.generator > po/Makefile.am || exit 1
+mkdir -p config
+aclocal -I m4 || exit 1
+autoconf || exit 1
+automake --add-missing || exit 1
--- client/samba/autogen.sh
+++ client/samba/autogen.sh
@@ -1,18 +1,12 @@
 #!/bin/sh
 
-if [ -f Makefile ]
-then
+if [ -f Makefile ] ; then
 	make maintainer-clean
 fi
-if [ ! -d po ]
-then
-	mkdir po
-fi
 
-po-am.generator > po/Makefile.am
+mkdir -p po
+./tools/po-am.generator > po/Makefile.am || exit 1
 mkdir -p config
-aclocal -I m4
-autoconf
-automake --add-missing
-
-./configure $*
+aclocal -I m4 || exit 1
+autoconf || exit 1
+automake --add-missing || exit 1
