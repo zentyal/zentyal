c32fbd3078863ba9a8633593fcb2f4503cc9ee55
Ricardo Muñoz <rmunoz@warp.es>
2005-08-10 08:18:12 +0000

    Amavisd, spamassassin and clamav daemons send actions management added.
    git-svn-id: http://svn.zentyal.org/zentyal/trunk@4246 866bc4c1-54da-0310-a958-9d5a9a0e8d04

--- client/mailfilter/src/EBox/MailFilter.pm
+++ client/mailfilter/src/EBox/MailFilter.pm
@@ -21,7 +21,7 @@ use warnings;
 use base 'EBox::GConfModule';
 
 use EBox::Gettext;
-#use EBox::Sudo qw( :all );
+use EBox::Sudo qw( :all );
 #use EBox::Validate qw( :all );
 use EBox::Summary::Module;
 use EBox::Summary::Status;
@@ -29,6 +29,9 @@ use EBox::Summary::Status;
 use constant AMAVISPIDFILE			=> "/var/run/amavis/amavisd.pid";
 use constant SAPIDFILE				=> "/var/run/spamd.pid";
 use constant CLAMAVPIDFILE			=> "/var/run/clamav/clamd.pid";
+use constant AMAVISINIT				=> '/etc/init.d/amavis';
+use constant SAINIT					=> '/etc/init.d/spamassassin';
+use constant CLAMAVINIT				=> '/etc/init.d/clamav-daemon';
 
 #
 # Method: _create
@@ -48,6 +51,13 @@ sub _create
 #
 sub _regenConfig
 {
+	my $self = shift;
+
+#	@array = ();
+#	$self->writeConfFile(AMAVISCONFFILE, "mailfilter/amavisd.conf.mas", \@array);
+
+	print STDERR "do daemon!!!\n";
+	$self->_doDaemon();
 }
 
 
@@ -64,12 +74,19 @@ sub isRunning
 {
 	my $self = shift;
 
-	if ($self->pidFileRunning(AMAVISPIDFILE) and $self->pidFileRunning(SAPIDFILE)
-			and $self->pidFileRunning(CLAMAVPIDFILE)) {
-		return 1;
+	if ($self->pidFileRunning(AMAVISPIDFILE)) {
+		print STDERR "Amavis esta rulando\n";
+	}
+	if ($self->pidFileRunning(SAPIDFILE)) {
+		print STDERR "SA esta rulando\n";
+	}
+	if ($self->pidFileRunning(CLAMAVPIDFILE)) {
+		print STDERR "Clamav esta rulando\n";
 	}
 
-	return 0;
+	return (($self->pidFileRunning(AMAVISPIDFILE)) and
+		($self->pidFileRunning(SAPIDFILE)) and
+		($self->pidFileRunning(CLAMAVPIDFILE)));
 }
 
 #
@@ -85,6 +102,75 @@ sub service
 {
 	my $self = shift;
 	return $self->get_bool('active');
+}
+
+#
+# Method: _doDaemon
+#
+#  Sends restart/start/stop command to the daemons depending of their actual
+#  state and the state stored in gconf
+#
+sub _doDaemon
+{
+	my $self = shift;
+
+	if ($self->service() and $self->isRunning()) {
+		print STDERR "RESTART\n";
+		$self->_daemon('restart');
+	} elsif ($self->service()) {
+		print STDERR "START\n";
+		$self->_daemon('start');
+	} elsif ($self->isRunning()) {
+		print STDERR "STOP\n";
+		$self->_daemon('stop');
+	}
+}
+
+#
+# Method: _daemon
+#
+#  Execute the action passed as parameter to the service daemons
+#
+# Parameters:
+#
+#  action - restart/start/stop
+#
+sub _daemon
+{
+	my ($self, $action) = @_;
+
+	my $amaviscmd = AMAVISINIT . " " . $action . " 2>&1";
+	my $sacmd = SAINIT . " " . $action . " 2>&1";
+	my $clamavcmd = CLAMAVINIT . " " . $action . " 2>&1";
+	
+	if ( $action eq 'start') {
+		root($clamavcmd);
+		root($sacmd);
+		root($amaviscmd);
+	} elsif ( $action eq 'stop') {
+		root($clamavcmd);
+		root($sacmd);
+		root($amaviscmd);
+	} elsif ( $action eq 'restart') {
+		root($clamavcmd);
+		root($sacmd);
+		root($amaviscmd);
+	} else {
+		throw EBox::Exceptions::Internal("Bad argument: $action");
+	}
+}
+
+#
+# Method: _stopService
+#
+#  Stops the service daemons
+#
+sub _stopService
+{
+	my $self = shift;
+	if ($self->isRunning('active')) {
+		$self->_daemon('stop');
+	}
 }
 
 #
@@ -177,7 +263,9 @@ sub rootCommands
 {
 	my $self = shift;
 	my @array = ();
-	push(@array, "/bin/true");
+	push(@array, AMAVISINIT);
+	push(@array, SAINIT);
+	push(@array, CLAMAVINIT);
 	return @array;
 }
 
