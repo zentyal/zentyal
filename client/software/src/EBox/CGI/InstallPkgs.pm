12b33bac6200a88e2130ab66eeaec8ecd5606732
Isaac Clerencia <iclerencia@zentyal.com>
2005-10-14 17:31:01 +0000

    don't break ebox component installation (chain should only be set when doit =yes)
    git-svn-id: http://svn.zentyal.org/zentyal/trunk@4385 866bc4c1-54da-0310-a958-9d5a9a0e8d04

--- client/software/src/EBox/CGI/InstallPkgs.pm
+++ client/software/src/EBox/CGI/InstallPkgs.pm
@@ -55,10 +55,14 @@ sub _process($) {
 		$action = 'upgrade';
 		$doit = 'yes';
 	} elsif (defined($self->param('ebox-install'))) {
-		$self->{chain} = "Software/EBox";
+		if($doit eq 'yes') {
+			$self->{chain} = "Software/EBox";
+		}
 		$action = 'install';	
 	} elsif (defined($self->param('ebox-remove'))) {
-		$self->{chain} = "Software/EBox";
+		if($doit eq 'yes') {
+			$self->{chain} = "Software/EBox";
+		}
 		$action = 'remove';
 	} else {
 		$self->{redirect} = "Summary/Index";
