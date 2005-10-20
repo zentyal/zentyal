a803d73cca9e69c1d1cfda8e7ddba7d3ec948d52
Isaac Clerencia <iclerencia@zentyal.com>
2005-10-20 17:16:22 +0000

    Get the package descriptions for software (forgot to commit a week ago :P)
    git-svn-id: http://svn.zentyal.org/zentyal/trunk@4425 866bc4c1-54da-0310-a958-9d5a9a0e8d04

--- client/software/autogen.sh
+++ client/software/autogen.sh
@@ -4,6 +4,10 @@ if [ -f Makefile ] ; then
 	make maintainer-clean
 fi
 
+pushd ..
+grep -h ^Description */debian/control | cut -c14- | sed s/^.*$/__\(\"\&\"\)\;/ > software/src/EBox/PackageDescriptions.pm
+popd
+
 mkdir -p po
 ./tools/po-am.generator > po/Makefile.am || exit 1
 mkdir -p config
