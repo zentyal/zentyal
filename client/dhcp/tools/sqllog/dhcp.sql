9a39653e2db2a2c5b571cbec6bfc28a31b2ac3fd
Javier Uruen <juruen@zentyal.com>
2005-11-11 23:23:41 +0000

    - use VARCHAR for events
    git-svn-id: http://svn.zentyal.org/zentyal/trunk@4663 866bc4c1-54da-0310-a958-9d5a9a0e8d04

--- client/dhcp/tools/sqllog/dhcp.sql
+++ client/dhcp/tools/sqllog/dhcp.sql
@@ -1,1 +1,1 @@
-CREATE TABLE leases(interface CHAR(16), mac MACADDR, ip INET, timestamp TIMESTAMP, event CHAR(255));
+CREATE TABLE leases(interface CHAR(16), mac MACADDR, ip INET, timestamp TIMESTAMP, event VARCHAR(255));
