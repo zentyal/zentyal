50fe0caa707509702a2b6655195c7a699b55e0f4
Javier Uruen <juruen@zentyal.com>
2006-01-16 14:55:14 +0000

    - added support to select which extensions are allowed to download thru   dansguardian
    git-svn-id: http://svn.zentyal.org/zentyal/trunk@4921 866bc4c1-54da-0310-a958-9d5a9a0e8d04

---
+++ client/squid/src/EBox/CGI/Extensions.pm
@@ -0,0 +1,59 @@
+# Copyright (C) 2005 Warp Networks S.L., DBS Servicios Informaticos S.L.
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License, version 2, as
+# published by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+package EBox::CGI::Squid::Extensions;
+
+use strict;
+use warnings;
+
+use base 'EBox::CGI::ClientBase';
+
+use EBox::Global;
+use EBox::Gettext;
+
+## arguments:
+## 	title [required]
+sub new {
+	my $class = shift;
+	my $self = $class->SUPER::new(@_);
+	$self->{domain} = 'ebox-squid';
+	$self->{redirect} = 'Squid/ExtensionsUI';
+	bless($self, $class);
+	return $self;
+}
+
+sub _process($) 
+{
+	my $self = shift;
+	my $squid = EBox::Global->modInstance('squid');
+
+	my @extens =  grep(s/^bool-//, @{$self->params()});
+
+	my @allow;
+	my @ban;
+
+	for my $ex (@extens) {
+		if ($self->param("bool-$ex")) {
+			push @allow, $ex;
+		} else {
+			push @ban, $ex;
+		}
+	}
+	
+	$squid->setAllowedExtensions(@allow);
+	$squid->setBannedExtensions(@ban);
+}
+
+1;
