6e193c1496b4554ae5d38e7979e310327b17a580
Javier Uruen <juruen@zentyal.com>
2006-02-01 17:42:12 +0000

    - better name for the index
    git-svn-id: http://svn.zentyal.org/zentyal/trunk@5034 866bc4c1-54da-0310-a958-9d5a9a0e8d04

--- client/dhcp/tools/sqllog/dhcp.sql
+++ client/dhcp/tools/sqllog/dhcp.sql
@@ -1,2 +1,2 @@
 CREATE TABLE leases(interface CHAR(16), mac MACADDR, ip INET, timestamp TIMESTAMP, event VARCHAR(255));
-CREATE INDEX leases_i on leases(timestamp);
+CREATE INDEX timestamp_i on leases(timestamp);
