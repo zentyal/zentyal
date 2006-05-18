42ef3d4551c3810fe79cea48411117639d064f03
Javier Amor García <jamor@zentyal.com>
2006-05-18 14:25:09 +0000

    added a few mre cases
    git-svn-id: http://svn.zentyal.org/zentyal/trunk@5126 866bc4c1-54da-0310-a958-9d5a9a0e8d04

--- client/ntp/src/EBox/NTP/Test.pm
+++ client/ntp/src/EBox/NTP/Test.pm
@@ -6,12 +6,95 @@ use warnings;
 
 use base 'EBox::Test::Class';
 use Test::More;
+use Test::Exception;
+use Test::Differences;
+
+use File::Slurp;
 
 use lib '../..';
 
-sub useTest : Test(1)
+sub setModuleGlobalConf : Test(startup)
+{
+    EBox::Global::Mock::setAllEBoxModules('ntp' => 'EBox::NTP');
+}
+
+sub _useAndCreationTest : Test(3)
 {
     use_ok 'EBox::NTP';
+
+    my $ntp = EBox::Global->modInstance('ntp');
+    lives_ok { $ntp = EBox::Global->modInstance('ntp') };
+    isa_ok ($ntp, 'EBox::NTP');
 }
+
+
+sub setAndGetServersDeviantTest : Test(18)
+{
+    my @deviantCases = (
+			# bad IPs:
+			['543.32.23.12'],
+			['134.23.12.23', '543.32.23.12'],
+			['134.23.12.23',  '21.46.12.34', '543.12.32.34'],
+			# bad hosts:
+			['badHost+'],
+			['macaco', 'badHost='],
+			['macaco', 'rhesus.mono.com',  'badHost|'],
+			# blank entries
+			[''],
+			['192.168.3', ''],
+			['192.168.3', 'ntp.monos.org', ''],
+			['192.168.3',  '', 'ntp.monos.org'],
+			['192.168.3',  '', ''],
+			# repeated IPs:
+			['192.168.3.1', '10.4.6.32', '192.168.3.1'],
+			['192.168.3.1', '192.168.3.1', '10.4.56.23'],
+			['10.4.6.31', '192.168.3.1', '192.168.3.1'],
+			['192.168.3.1', '192.168.3.1', '192.168.3.1'],
+			# repeated hosts
+			['macaco', 'macaco'],
+			['macaco', 'ntp.rhesus.org',  'macaco'],
+			['macaco', 'macaco',  'ntp.rhesus.org'],
+			['macaco', 'macaco',  'macaco'],
+
+       );
+
+    my $ntp = EBox::Global->modInstance('ntp');
+
+    foreach my $case_r (@deviantCases) {
+	my @params = @{ $case_r};
+	dies_ok { $ntp->setServers(@params)   } "Checking deviant case with parameters: @params";
+    }
+}
+
+
+sub setAndGetServersTest : Test(16)
+{
+    my @cases = (
+		 # ips
+		 ['192.168.3.4'],
+		 ['192.168.3.4', '10.4.3.24'],
+		 ['192.168.3.4', '10.4.3.24', '127.0.0.1'],
+		 # hosts
+		 ['ntp.macaco.org'],
+		 ['ntp.macaco.org', 'saturno.pantheon.org'],
+		 ['ntp.macaco.org', 'saturno.pantheon.org', 'madonna.timegososlowly.net'],                 # host and ips
+		 ['192.168.3.4', 'ntp.macaco.org'],
+		 ['192.168.3.4', 'ntp.macaco.org', '10.45.21.23'],
+	    );
+
+
+    my $ntp = EBox::Global->modInstance('ntp');
+
+    foreach my $case_r (@cases) {
+	my @expectedServers = @{ $case_r};
+	lives_ok { $ntp->setServers(@expectedServers)   } "Checking deviant case with parameters: @expectedServers";
+
+	my @actualServers = $ntp->servers();
+	eq_or_diff \@actualServers, \@expectedServers, 'Checking that servers are stored and retrieved normally';
+    }
+}
+
+
+
 
 1;
