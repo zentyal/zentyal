34bc73d215dc6e6a57a535111c966f6461c39d88
Javier Amor García <jamor@zentyal.com>
2006-06-08 09:50:07 +0000

    removed deprecated subs calls
    git-svn-id: http://svn.zentyal.org/zentyal/trunk@5210 866bc4c1-54da-0310-a958-9d5a9a0e8d04

--- client/ebox/src/EBox/Global/Mock.pm
+++ client/ebox/src/EBox/Global/Mock.pm
@@ -23,9 +23,9 @@ sub setEBoxModule
 {
     my ($name, $module, $depends) = @_;
 
-    EBox::GConfModule::Mock::setArbitraryEntry("/ebox/modules/global/modules/$name/class", $module);
-    EBox::GConfModule::Mock::setArbitraryEntry("/ebox/modules/global/modules/$name/changed", undef);
-    EBox::GConfModule::Mock::setArbitraryEntry("/ebox/modules/global/modules/$name/depends", $depends) if defined $depends;
+    EBox::GConfModule::Mock::setEntry("/ebox/modules/global/modules/$name/class", $module);
+    EBox::GConfModule::Mock::setEntry("/ebox/modules/global/modules/$name/changed", undef);
+    EBox::GConfModule::Mock::setEntry("/ebox/modules/global/modules/$name/depends", $depends) if defined $depends;
 
 }
 
