16b8eb16a990dd5af72e7a1f6af60232bf3e53bc
Javier Amor García <jamor@zentyal.com>
2006-06-20 12:03:35 +0000

    refactored
    git-svn-id: http://svn.zentyal.org/zentyal/trunk@5221 866bc4c1-54da-0310-a958-9d5a9a0e8d04

--- client/logs/src/EBox/CGI/Index.pm
+++ client/logs/src/EBox/CGI/Index.pm
@@ -84,6 +84,117 @@ sub _actualPage
 }
 
 
+sub addToMasonParameters
+{
+    my ($self, @masonParams) = @_;
+
+
+    defined $self->{params} or $self->{params} = [];
+    my $oldParams_r= $self->{params};
+    if (defined $oldParams_r) {
+	push @masonParams, @{ $oldParams_r };
+    }
+
+
+    
+    
+    $self->{params}  = \@masonParams;
+}
+
+
+sub _fromDate
+{
+    my ($self) = @_;
+    my $defaultPeriod = -24*3600; # one day
+    my $fromDate = $self->_getDateArray('from', $defaultPeriod);
+
+    return $fromDate;
+}
+
+sub _toDate
+{
+    my ($self) = @_;
+    my $toDate = $self->_getDateArray('to');
+
+    return $toDate;
+}
+
+
+sub _getDateArray
+{
+    my ($self, $prefix, $defaultTimeAdjust) = @_;
+    defined $defaultTimeAdjust or $defaultTimeAdjust = 0;
+
+    my %time;
+
+    my @localtime = localtime((time() + $defaultTimeAdjust));
+    $time{$prefix . 'sec'}   = $localtime[0];
+    $time{$prefix . 'min'}   = $localtime[1];
+    $time{$prefix . 'hour'}  = $localtime[2];
+    $time{$prefix . 'day'}   = $localtime[3];
+    $time{$prefix . 'month'} = $localtime[4] + 1;
+    $time{$prefix . 'year'}  = $localtime[5]  + 1900;
+    
+    foreach my $key (keys %time) {
+	my $paramValue = $self->param($key);
+	if (defined $paramValue) {
+	    $time{$key} = $paramValue;
+	}
+    }
+
+    my @wantedOrder = map { $prefix . $_  }  qw(day month year hour min sec) ;
+    my @dateArray = map {  $time{$_} }  @wantedOrder;
+
+    return \@dateArray;
+}
+
+
+sub _searchLogs
+{
+    my ($self, $logs, $selected) = @_;
+
+    my %hret;
+    my $hfilters;
+    my $tableinfo = $logs->getTableInfo($selected);
+    my $table     = $tableinfo->{'tablename'};
+    my $timecol   = $tableinfo->{'timecol'};
+
+    my $tpages = ceil($logs->totalRecords($table) / PAGESIZE) - 1;
+    my $page = $self->_actualPage($tpages);
+    
+    my @fromdate = @{ $self->_fromDate() };
+    my @todate   = @{ $self->_toDate() };
+    
+    foreach my $filter (grep(s/^filter-//, @{$self->params()})) {
+	$hfilters->{$filter} = 
+	    $self->unsafeParam("filter-$filter");
+    }
+    
+    %hret = %{$logs->search($fromdate[2].'-'.$fromdate[1].'-'.$fromdate[0].' '.$fromdate[3].':'.$fromdate[4].':0',
+			    $todate[2].'-'.$todate[1].'-'.$todate[0].' '.$todate[3].':'.$todate[4].':0',
+			    $selected, 
+			    PAGESIZE,
+			    $page,
+			    $timecol,
+			    $hfilters)};
+    
+    $tpages = ceil ($hret{'totalret'} / PAGESIZE) -1;
+    $page = $self->_actualPage($tpages);
+
+    my @masonParameters;
+    push(@masonParameters, 'filters' => _encode_filters($hfilters));
+    push(@masonParameters, 'tableinfo' => $tableinfo);
+
+    push(@masonParameters, 'page' => $page);
+    push(@masonParameters, 'tpages' => $tpages);
+    push(@masonParameters, 'data' => $hret{'arrayret'});
+    push(@masonParameters, 'fromdate' => \@fromdate);
+    push(@masonParameters, 'todate' => \@todate);
+	
+    $self->addToMasonParameters(@masonParameters);
+
+}
+
 sub _encode_filters {
 	my ($par) = @_;
 
@@ -95,107 +206,25 @@ sub _encode_filters {
 
 sub _process
 {
-	my $self = shift;
-	my @array;
+	my ($self) = @_;
 	
 	my $logs = EBox::Global->modInstance('logs');
-	my %hret = ();
-	my @fromdate = ();
-	my @todate = ();
-	my $tpages = 0;
-	my ($toSec, $toMinute, 
-	    $toHour, $toDay, 
-	    $toMonth, $toYear, 
-	    $toWeekday, $toDayofyear, $tolsDST) = localtime(time());
-	my ($fromSec, $fromMinute, 
-	    $fromHour, $fromDay, 
-	    $fromMonth, $fromYear, 
-	    $fromWeekday, $froomDayofyear, $fromlsDST) = localtime(time()-24*3600);
 
-	$toYear += 1900;
-	$fromYear += 1900;
-	$toMonth += 1;
-	$fromMonth += 1;
 
-	my $page;
-	
 	my $selected = $self->param('selected');
-	my $tableinfo;
-	my $hfilters;
+
 	if (defined($selected)) {
-		$tableinfo = $logs->getTableInfo($selected);
-		my $table = $tableinfo->{'tablename'};
-		my $timecol = $tableinfo->{'timecol'};
-
-		$tpages = ceil($logs->totalRecords($table) / PAGESIZE) - 1;
-		$page = $self->_actualPage($tpages);
-
-	
-		my $fromday = $self->param('fromday');
-		my $frommonth = $self->param('frommonth');
-		my $fromyear = $self->param('fromyear');
-		my $fromhour = $self->param('fromhour');
-		my $frommin = $self->param('frommin');
-		my $fromsec = $self->param('fromsec');
-
-		my $today = $self->param('today');
-		my $tomonth = $self->param('tomonth');
-		my $toyear = $self->param('toyear');
-		my $tohour = $self->param('tohour');
-		my $tomin = $self->param('tomin');
-		my $tosec = $self->param('tosec');
-
-		defined($fromday) or $fromday = $fromDay;
-		defined($frommonth) or $frommonth = $fromMonth;
-		defined($fromyear) or $fromyear = $fromYear;
-		defined($frommin) or $frommin = $fromMinute;
-		defined($fromhour) or $fromhour = $fromHour;
-		defined($fromsec) or $fromsec = $fromSec;
-
-		defined($today) or $today = $toDay;
-		defined($tomonth) or $tomonth = $toMonth;
-		defined($toyear) or $toyear = $toYear;
-		defined($tomin) or $tomin = $toMinute;
-		defined($tohour) or $tohour = $toHour;
-		defined($tosec) or $tosec = $toSec;
-
-		@fromdate = ($fromday, $frommonth, $fromyear, $fromhour, 
-			$frommin, $fromsec);
-
-		@todate = ($today, $tomonth, $toyear, $tohour,
-			$tomin, $tosec);
-		
-		foreach my $filter (grep(s/^filter-//, @{$self->params()})) {
-			$hfilters->{$filter} = 
-				$self->unsafeParam("filter-$filter");
-		}
-		
-		%hret = %{$logs->search($fromdate[2].'-'.$fromdate[1].'-'.$fromdate[0].' '.$fromdate[3].':'.$fromdate[4].':0',
-			$todate[2].'-'.$todate[1].'-'.$todate[0].' '.$todate[3].':'.$todate[4].':0',
-			$selected, 
-			PAGESIZE,
-			$page,
-			$timecol,
-			$hfilters)};
-		
-		$tpages = ceil ($hret{'totalret'} / PAGESIZE) -1;
-		$page = $self->_actualPage($tpages);
+	    $self->_searchLogs($logs, $selected);
 	} 
 	else {
 		$selected = 'none';
 	}
 
-	push(@array, 'logdomains' => $logs->getLogDomains());
-	push(@array, 'filters' => _encode_filters($hfilters));
-	push(@array, 'tableinfo' => $tableinfo);
-	push(@array, 'selected' => $selected);
-	push(@array, 'page' => $page);
-	push(@array, 'tpages' => $tpages);
-	push(@array, 'data' => $hret{'arrayret'});
-	push(@array, 'fromdate' => \@fromdate);
-	push(@array, 'todate' => \@todate);
-	
-	$self->{params} = \@array;
+	my @masonParameters;
+	push(@masonParameters, 'logdomains' => $logs->getLogDomains());
+	push(@masonParameters, 'selected' => $selected);
+
+	$self->addToMasonParameters(@masonParameters);
 }
 
 
