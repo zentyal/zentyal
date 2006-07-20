a8e0b724dd96ea7cd39e6627922d978c1063ef1c
Javier Amor García <jamor@zentyal.com>
2006-07-20 07:40:38 +0000

    fixed file problem
    git-svn-id: http://svn.zentyal.org/zentyal/trunk@5272 866bc4c1-54da-0310-a958-9d5a9a0e8d04

--- common/libebox/src/EBox/Sudo/TestStub.pm
+++ common/libebox/src/EBox/Sudo/TestStub.pm
@@ -1,136 +1,79 @@
-package EBox::NetWrappers::TestStub;
+package EBox::Sudo::TestStub;
 # Description:
 # 
 use strict;
 use warnings;
-use Test::MockObject;
-use EBox::NetWrappers;
 
-my %fakeIfaces;
-my %fakeRoutes;
+#use Test::MockModule;
+use EBox::Sudo;
+
+# XXX There are problems with symbol imporation and Test::MockModule
+# until we found a solution we will use a brute redefiniton
+# XXX Currently i see not way for mocking EBox::Sudo::sudo
+# XXX there are unclaer situation with comamnds containig ';' but this is also de case of EBox::Sudo
+
+#my $mockedSudoModule = undef;
+
+my $oldRootSub = undef;
 
 sub fake
 {
-  Test::MockObject->fake_module('EBox::NetWrappers',
-				iface_exists => \&iface_exists,
-				list_ifaces  => \&list_ifaces,
-				iface_is_up  => \&iface_is_up,
-				iface_mac_address => \&iface_mac_address,
-				iface_address => \&iface_address,
-				iface_address_with_netmask => \&iface_address_with_netmask,
-				list_routes  => \&list_routes,
-				route_is_up  => \&route_is_up,
-			       );
-}
+    $oldRootSub = \&EBox::Sudo::root if !defined $oldRootSub;
 
+    no warnings 'redefine';
+    my $redefinition = '
+    sub EBox::Sudo::root
+    {
+	return EBox::Sudo::TestStub::_fakedRoot(@_);
+    }';
 
-
-
-sub setFakeIfaces
-{
-  my ($fakeIfaces_r) = @_;
-  %fakeIfaces = %{$fakeIfaces_r};
-}
-
-sub fakeIfaces
-{
-  return \%fakeIfaces;
-}
-
-
-sub setFakeRoutes
-{
-  my ($fakeRoutes_r) = @_;
-  %fakeRoutes = %{$fakeRoutes_r};
-}
-
-
-sub fakeRoutes
-{
-  return \%fakeRoutes;
-}
-
-
-# fake methods:
-sub iface_exists
-{
-  my ($iface) = @_;
-  return exists $fakeIfaces{$iface};
-}
-
-sub list_ifaces
-{
-  return keys %fakeIfaces;
-}
-
-sub iface_is_up
-{
-  my ($iface) = @_;
-  return _ifacePropierty($iface, 'up');
-}
-
-
-
-sub iface_mac_address
-{
-  my ($iface) = @_;
-  return _ifacePropierty($iface, 'mac_address');
-}
-
-sub iface_address
-{
-  my ($iface) = @_;
-  my $address_r =  _ifacePropierty($iface, 'address') ;
-  return keys %{ $address_r };
-}
-
-sub iface_address_with_netmask
-{
-  my ($iface) = @_;
-  return _ifacePropierty($iface, 'address');
-}
-
-
-sub _ifacePropierty
-{
-  my ($iface, $propierty) = @_;
-
-  unless (exists $fakeIfaces{$iface}) {
-      throw EBox::Exceptions::DataNotFound(
-						data => __('Interface'),
-						value => $iface);
+    eval $redefinition;
+   if ($@) {
+    throw EBox::Exceptions::Internal ("Error while redifinition of root for test purposes: $@");
   }
 
-  unless (exists $fakeIfaces{$iface}->{$propierty} ) {
-    die "You had not setted a $propierty propierty for the fake iface $iface";
-  }
-
-  return $fakeIfaces{$iface}->{$propierty};
 }
 
 
-sub list_routes
+# XXX fix:  restores behaviour but no implementation 
+sub unfake
 {
-  my @routes;
-  while (my ($dest, $router) = each %fakeRoutes) {
-    push @routes, { network => $dest,  router => $router };
-  }
-  return @routes;
-}
+    defined $oldRootSub or die "Module was not mocked";
 
 
-sub route_is_up
-{
-  my ($network, $router) = @_;
+    no warnings 'redefine';
+    my $redefinition = ' sub EBox::Sudo::root
+     {
+ 	return $oldRootSub->(@_);
+     }';
 
-  if (exists $fakeRoutes{$network}) {
-    if ($fakeRoutes{$network} eq $router) {
-      return 1;
+    eval $redefinition;
+    if ($@) {
+	throw EBox::Exceptions::Internal ("Error while redifinition of root for test purposes: $@");
     }
-  }
 
-  return undef;
 }
+
+
+sub _fakedRoot
+{
+    my ($cmd) = @_;
+
+    my @output = `$cmd`;
+    unless($? == 0) {
+	_rootCommandException($cmd, $!);
+    }
+    return \@output;
+}
+
+
+
+sub _rootCommandException
+{
+    my ($cmd, $error) = @_;
+    throw EBox::Exceptions::Internal("(Mocked EBox::Sudo) Root command $cmd failed. $error");
+}
+
 
 
 1;
