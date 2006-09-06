757b7ec8d7cb082322ca8020f9456844d765f209
Javier Amor García <jamor@zentyal.com>
2006-09-06 09:54:30 +0000

    refs #131
    git-svn-id: http://svn.zentyal.org/zentyal/trunk@5373 866bc4c1-54da-0310-a958-9d5a9a0e8d04

---
+++ client/backup/src/EBox/Backup/OpticalDiscDrives.pm
@@ -0,0 +1,89 @@
+package EBox::Backup::OpticalDiscDrives;
+# module to get cdrom info from  /proc/sys/devices/cdrom
+use strict;
+use warnings;
+
+use File::Slurp qw(read_file);
+use Readonly;
+Readonly::Scalar my $CDROM_INFO_PATH => '/proc/sys/dev/cdrom/info';
+
+use EBox::Gettext;
+
+
+sub info
+{
+  if (! -e $CDROM_INFO_PATH) {
+    throw EBox::Exceptions::External (__x('Unable to find {path}. Make sure that the cdrom driver is compiled in your kerner or loaded as module', path -> $CDROM_INFO_PATH));
+  }
+  if (! -r $CDROM_INFO_PATH) {
+    throw EBox::Exceptions::External (__x('Unable to read {path}.', path -> $CDROM_INFO_PATH));
+  }
+
+  my @infoFile = read_file($CDROM_INFO_PATH);
+
+  # we have not use for the first two lines
+  shift @infoFile;
+  shift @infoFile;
+  # chomp remaining lines
+  chomp @infoFile;
+
+  my %info;
+  my @names;
+  
+  my $driveNameLine = shift @infoFile;
+  if (! $driveNameLine =~ m{^drive name:} ) {
+    throw EBox::Exceptions::Internal("Parse error in $CDROM_INFO_PATH: supposed drive name line does not match");
+  }
+  (undef, undef,  @names) = split '\s+', $driveNameLine; # get the drive names
+  $info{$_} = {} foreach @names; # create entries in  info foreach drive
+
+  foreach my $line (@infoFile) {
+    next if $line =~ m{^\s*$};
+    
+    my ($section, $valuesString) = split ':', $line, 2;
+    my @values = split '\s+', $valuesString;
+    shift @values; # remove empty leading field
+
+    (@values == @names) or throw EBox::Exceptions::Internal("Error parsing $CDROM_INFO_PATH: number of elements and values in section $section don't match");
+    foreach my $n (0 .. scalar @names -1) {
+      $info{$names[$n]}->{$section} = $values[$n];
+    }
+  }
+
+  return \%info;
+}
+
+
+sub dvdWriters
+{
+  return _selectByCapability('Can write DVD-R');
+}
+
+
+sub cdrwWriters
+{
+  return _selectByCapability('Can write CD-R');
+}
+
+sub cdrWriters
+{
+  return _selectByCapability('Can write CD-RW');
+}
+
+
+sub _selectByCapability
+{
+  my ($capability) = @_;
+  my @selectedDevices;
+
+  my %deviceInfo = %{ info()  };
+  while (my ($device, $capabilities_r) = each %deviceInfo) {
+    if ($capabilities_r->{$capability}) {
+      push @selectedDevices, $device;
+    }
+  }
+
+  return @selectedDevices;
+}
+
+1;
