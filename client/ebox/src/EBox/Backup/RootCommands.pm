c36e423cec2a726457eb7bf1fd5c1c6073cfde32
Isaac Clerencia <iclerencia@zentyal.com>
2006-11-21 22:15:59 +0000

    Executable scripts should be put in the libexec/ directory in the module and go to /usr/lib/ebox, in any case, they can't be directly installed with an "install ... /some/absolute/path", but handled by the autostuff
    I guess everything is ok, but please double check it, specially if the path
    needs to be changed anywhere else
    
    
    git-svn-id: http://svn.zentyal.org/zentyal/trunk@5822 866bc4c1-54da-0310-a958-9d5a9a0e8d04

--- client/ebox/src/EBox/Backup/RootCommands.pm
+++ client/ebox/src/EBox/Backup/RootCommands.pm
@@ -3,17 +3,18 @@ package EBox::Backup::RootCommands;
 # With the changes in sudoers this has lost sense and we may change this in the fuutres
 use strict;
 use warnings;
-use EBox::Sudo;
+use EBox::Config;
 use EBox::Gettext;
+use EBox::Sudo;
 
 use Readonly;
 Readonly::Scalar our $CDRECORD_PATH=>'/usr/bin/cdrecord';
 Readonly::Scalar our $CDRDAO_PATH=>'/usr/bin/cdrdao';
 Readonly::Scalar our $MKISOFS_PATH=>'/usr/bin/mkisofs';
-Readonly::Scalar our $GROWISOFS_PATH=>'/usr/bin/growisofs-sudo';
 Readonly::Scalar our $DVDRWFORMAT_PATH=>'/usr/bin/dvd+rw-format';
 Readonly::Scalar our $DVDMEDIAINFO_PATH => '/usr/bin/dvd+rw-mediainfo';
 Readonly::Scalar our $EJECT_PATH  => '/usr/bin/eject';
+Readonly::Scalar our $GROWISOFS_PATH=>EBox::Config->libexec() . 'growisofs-sudo';
 
 sub checkExecutable
 {
