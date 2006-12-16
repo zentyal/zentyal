a396c01ec1f9afde0a27961c3c700b1de7598925
Isaac Clerencia <iclerencia@zentyal.com>
2006-12-16 23:18:55 +0000

    Modify the admin table to follow the previous code changes
    git-svn-id: http://svn.zentyal.org/zentyal/trunk@6095 866bc4c1-54da-0310-a958-9d5a9a0e8d04

--- client/ebox/tools/sqllog/admin.sql
+++ client/ebox/tools/sqllog/admin.sql
@@ -1,1 +1,1 @@
-CREATE TABLE admin(timestamp TIMESTAMP, clientaddress VARCHAR(20), module VARCHAR(40), message VARCHAR(200), committed BOOLEAN);
+CREATE TABLE admin(timestamp TIMESTAMP, clientaddress VARCHAR(20), module VARCHAR(40), action VARCHAR(40), params VARCHAR(200), committed BOOLEAN);
