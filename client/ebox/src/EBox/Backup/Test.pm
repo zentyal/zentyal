6bdff2822491f6635b5d7d66bac40730bdd5f5c5
Javier Amor García <jamor@zentyal.com>
2006-12-26 17:42:36 +0000

    test needs the mangled gconf libraries closes #349
    git-svn-id: http://svn.zentyal.org/zentyal/trunk@6180 866bc4c1-54da-0310-a958-9d5a9a0e8d04

--- client/ebox/src/EBox/Backup/Test.pm
+++ client/ebox/src/EBox/Backup/Test.pm
@@ -47,6 +47,7 @@ sub testDir
 sub notice : Test(startup)
 {
   diag 'This test use GConf and may left behind some test entries in the tree /ebox'; 
+  diag 'Remember you need the special GConf packages from eBox repository. Otherwise this tests will fail in weird ways';
 }
 
 
