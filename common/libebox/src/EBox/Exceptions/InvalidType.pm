33a9901978994facd0e0fefa62362be5bd4207bf
Enrique J. Hernández Blasco <ejhernandez@zentyal.com>
2007-01-08 14:57:39 +0000

    A brand new exception is added called InvalidType to say an argument has a mistaken type (class) indeed refs #361
    git-svn-id: http://svn.zentyal.org/zentyal/trunk@6278 866bc4c1-54da-0310-a958-9d5a9a0e8d04

---
+++ common/libebox/src/EBox/Exceptions/InvalidType.pm
@@ -0,0 +1,52 @@
+# Copyright (C) 2005 Warp Networks S.L., DBS Servicios Informaticos S.L.
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License, version 2, as
+# published by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+package EBox::Exceptions::InvalidType;
+
+use base 'EBox::Exceptions::Internal';
+use Log::Log4perl;
+
+# Constructor: new
+#
+#     This exception is taken to say the type of an argument is not
+#     the correct one.
+#
+# Parameters:
+#
+#     arg  - the mistaken argument
+#     type - the correct type
+#
+# Returns:
+#
+#     The newly created <EBox::Exceptions::InvalidType> exception
+#
+sub new # (arg, type)
+  {
+    my $class = shift;
+    my ($arg, $type) = @_;
+
+    local $Error::Depth = $Error::Depth + 1;
+    local $Error::Debug = 1;
+
+    $Log::Log4perl::caller_depth++;
+    $self = $class->SUPER::new("Invalid type for argument: $arg. It should be this type: $type");
+    $Log::Log4perl::caller_depth--;
+
+    bless ($self, $class);
+
+    return $self;
+}
+
+1;
