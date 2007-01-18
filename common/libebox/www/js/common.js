0548484e85c907ee40b974f70ecfd0e9c90df245
Enrique J. Hernández Blasco <ejhernandez@zentyal.com>
2007-01-18 14:33:04 +0000

    Add a new CSS class highlight under a data table which background is orange wherever it is shown refs #207
    git-svn-id: http://svn.zentyal.org/zentyal/trunk@6383 866bc4c1-54da-0310-a958-9d5a9a0e8d04

--- common/libebox/www/js/common.js
+++ common/libebox/www/js/common.js
@@ -84,7 +84,8 @@ function checkAll(id, allElementName){
 	}
 }
 
-
+/*
+ */
 function stripe(theclass,evenColor,oddColor) {
     var tables = getElementByClass(theclass);
 
@@ -96,16 +97,16 @@ function stripe(theclass,evenColor,oddColor) {
         for (var h = 0; h < tbodies.length; h++) {
             var trs = tbodies[h].getElementsByTagName("tr");
             for (var i = 0; i < trs.length; i++) {
-                if (! trs[i].style.backgroundColor) {
-                    var tds = trs[i].getElementsByTagName("td");
-                    for (var j = 0; j < tds.length; j++) {
-                        var mytd = tds[j];
-                        if (! mytd.style.backgroundColor) {
-                            mytd.style.backgroundColor = even ? evenColor : oddColor;
-                        }
-                    }
-                }
-                even =  ! even;
+	      if (! trs[i].style.backgroundColor && (trs[i].className.indexOf("highlight") == -1)) {
+		var tds = trs[i].getElementsByTagName("td");
+		for (var j = 0; j < tds.length; j++) {
+		  var mytd = tds[j];
+		  if (! mytd.style.backgroundColor) {
+		    mytd.style.backgroundColor = even ? evenColor : oddColor;
+		  }
+		}
+	      }
+	      even =  ! even;
             }
         }
     }
