135effb6afa4081f2d8433ef2e76a3cb0db9349e
Enrique J. Hernández Blasco <ejhernandez@zentyal.com>
2007-01-31 08:56:42 +0000

    Changes file mode to be readable not only owner but also others users such as nobody
    git-svn-id: http://svn.zentyal.org/zentyal/trunk@6511 866bc4c1-54da-0310-a958-9d5a9a0e8d04

--- client/ca/src/EBox/CA.pm
+++ client/ca/src/EBox/CA.pm
@@ -71,8 +71,8 @@ use constant LASTESTCRL   => CRLDIR   . "latest.pem";
 use constant CAPRIVKEY   => PRIVDIR . "cakey.pem";
 use constant CAPUBKEY    => KEYSDIR . "capubkey.pem";
 
-# Directory mode to allow only owner
-use constant DIRMODE     => 00700;
+# Directory mode to allow only owner and readable from others
+use constant DIRMODE     => 00704;
 
 # Use Certification Version 3
 use constant EXTENSIONS_V3 => "1";
