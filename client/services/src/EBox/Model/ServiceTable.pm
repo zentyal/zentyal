fea4111bc8d68db5ff3dfc39c0e9605e179f6980
Javier Uruen <juruen@zentyal.com>
2007-09-03 12:33:13 +0000

    adding services module
    git-svn-id: http://svn.zentyal.org/zentyal/trunk@7961 866bc4c1-54da-0310-a958-9d5a9a0e8d04

---
+++ client/services/src/EBox/Model/ServiceTable.pm
@@ -0,0 +1,278 @@
+# Copyright (C) 2005 Warp Networks S.L., DBS Servicios Informaticos S.L.
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License, version 2, as
+# published by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+# Class: EBox::Services::Model::ServiceTable
+#
+#   This class describes the data model used to store services.
+#   That is, a set of abstractions for protocols and ports.
+#
+#   This table stores basically the following fields:
+#
+#   name - service's name
+#   description - service's description (optional)
+#   configuration - hasMany relation with model
+#                   <EBox::Services::ModelServiceConfigurationTable>
+#   
+#
+#   Let's see an example of the structure returned by printableValueRows()
+#
+#   [
+#     {
+#       'name' => 'ssh',
+#       'id' => 'serv7999',
+#       'description' => 'Secure Shell'
+#
+#       'configuration' => {
+#           'model' => 'ServiceConfigurationTable',
+#           'values' => [
+#            {
+#               'source' => 'any',
+#               'protocol' => 'TCP',
+#               'destination' => '22',
+#               'id' => 'serv16'
+#            }
+#           ],
+#       },
+#            
+#            
+#     },
+#     {
+#        'id' => 'serv7867',
+#        'name' => 'ftp',
+#        'description' => 'File transfer protocol'
+#        'configuration' => {
+#            'model' => 'ServiceConfigurationTable',
+#            'values' => [
+#            {
+#                'source' => 'any',
+#                'protocol' => 'TCP',
+#                'destination' => '21:22',
+#                'id' => 'serv6891'
+#            }
+#            ],
+#        },
+#     }
+#    ]
+
+package EBox::Services::Model::ServiceTable;
+
+use EBox::Global;
+use EBox::Gettext;
+use EBox::Validate qw(:all);
+use EBox::Types::Text;
+use EBox::Types::Boolean;
+use EBox::Types::HasMany;
+use EBox::Sudo;
+use EBox::Model::ModelManager;
+
+use EBox::Exceptions::Internal;
+
+
+use strict;
+use warnings;
+
+use base 'EBox::Model::DataTable';
+
+sub new 
+{
+        my $class = shift;
+        my %parms = @_;
+
+        my $self = $class->SUPER::new(@_);
+        bless($self, $class);
+
+        return $self;
+}
+
+sub _table
+{
+
+    my @tableHead = 
+        ( 
+         new EBox::Types::Text(
+             'fieldName' => 'translationDomain',
+             'printableName' => __('Domain'),
+             'size' => '10',
+             'optional' => 1,
+             ),
+         new EBox::Types::Text(
+             'fieldName' => 'name',
+             'printableName' => __('Service name'),
+             'localizable' => 1,
+             'size' => '8',
+             'unique' => 1,
+             'editable' => 1
+             ),
+         new EBox::Types::Text(
+             'fieldName' => 'description',
+             'printableName' => __('Description'),
+             'size' => '16',
+             'editable' => 1,
+             'optional' => 1,
+             ),
+         new EBox::Types::Boolean(
+                 'fieldName' => 'internal',
+                 'printableName' => __('Internal'),
+                 ),
+         new EBox::Types::HasMany (
+                 'fieldName' => 'configuration',
+                 'printableName' => __('Configuration'),
+                 'foreignModel' => 'ServiceConfigurationTable',
+                 'view' => '/ebox/Services/View/ServiceConfigurationTable',
+                 'size' => '1',
+                 )
+             );
+
+
+
+    my $dataTable = 
+    { 
+        'tableName' => 'ServiceTable',
+        'automaticRemove' => 1,
+        'printableTableName' => __('Services'),
+        'defaultController' =>
+            '/ebox/Services/Controller/ServiceTable',
+        'defaultActions' =>
+            [	'add', 'del', 'editField', 'changeView' ],
+        'tableDescription' => \@tableHead,
+        'menuNamespace' => 'Services/View/ServiceTable',
+        'class' => 'dataTable',
+        'help' => __x('bbbb'),
+        'printableRowName' => __('service')
+    };
+
+    return $dataTable;
+}
+
+# Method: _tailoredOrder
+#
+#        Overrides <EBox::Model::DataTable::_tailoredOrder>
+#
+#
+sub _tailoredOrder # (rows)
+{
+
+    my ($self, $rows_ref) = @_;
+
+    # Order rules per priority
+    my @orderedRows = sort { $a->{valueHash}->{name}->value()
+        cmp $b->{valueHash}->{name}->value() }
+    @{$rows_ref};
+
+    return \@orderedRows;
+
+}
+
+# Method: addService 
+#
+#   Add service to the services table. Note this method must exist
+#   because we add services manually from other modules	
+#
+# Parameters:
+#
+#   (NAMED)
+#   name       - service's name
+#   description - service's description
+#   protocol   - it can take one of these: any, tcp, udp, tcp/udp, grep, icmp
+#   sourcePort - it can take: 
+#               "any"
+#               An integer from 1 to 65536 -> 22
+#               Two integers separated by colons -> 22:25 
+#   destinationPort - same as source
+#   internal - booelan, to indicate if the service is internal or not
+#
+#   Example:
+#
+#        'protocol' => 'tcp',
+#        'source' => 'any',
+#       'destination' => '21:22',
+sub addService 
+{
+    my ($self, %params) = @_;
+
+    my $name = delete $params{'name'};
+    my $description = delete $params{'description'};
+    my $internal = delete $params{'internal'};
+    my $readonly = delete $params{'readOnly'};
+    
+    my $id = $self->addRow('name' => $name, 
+                           'description' => $description,
+                           'internal' => $internal,
+			   'readOnly' => $readonly);
+
+    unless (defined($id)) {
+        throw EBox::Exceptions::Internal("Couldn't add name and description");
+    }
+
+    my $protocol = delete $params{'protocol'};
+    my $sourcePort = delete $params{'sourcePort'};
+    my $destinationPort = delete $params{'destinationPort'};
+    
+    my $sourcePortType;
+    my $sourcePortFrom;
+    my $sourcePortTo;
+    my $sourceSinglePort;
+    
+    if ($sourcePort eq 'any') {
+        $sourcePortType  = 'any';
+    } elsif ($sourcePort =~ /^\d+$/) {
+        $sourcePortType = 'single';
+        $sourceSinglePort = $sourcePort;
+    } elsif ($sourcePort =~ /^\d+:\d+/) {
+        $sourcePortType = 'range';
+        ($sourcePortFrom, $sourcePortTo) = split (/:/, $sourcePort);
+    }
+
+    my $destinationPortType;
+    my $destinationPortFrom;
+    my $destinationPortTo;
+    my $destinationSinglePort;
+    
+    if ($destinationPort eq 'any') {
+        $destinationPortType  = 'any';
+    } elsif ($destinationPort =~ /^\d+$/) {
+        $destinationPortType = 'single';
+        $destinationSinglePort = $destinationPort;
+    } elsif ($destinationPort =~ /^\d+:\d+/) {
+        $destinationPortType = 'range';
+        ($destinationPortFrom, $destinationPortTo) = 
+                          split (/:/, $destinationPort);
+    }
+
+    my $serviceConf = EBox::Model::ModelManager
+                                       ->instance()
+                                       ->model('ServiceConfigurationTable');
+    unless (defined($serviceConf)) {
+        throw EBox::Exceptions::Internal(
+                    "Couldn't get ServiceConfigurationTable");
+    }
+
+	
+    $serviceConf->setDirectory($self->{'directory'} . "/$id/configuration");
+    $serviceConf->addRow('protocol' => $protocol, 
+                       'source_range_type' => $sourcePortType,
+                       'source_single_port' => $sourceSinglePort,
+                       'source_from_port' => $sourcePortFrom,
+                       'source_to_port' => $sourcePortTo,
+                       'destination_range_type' => $destinationPortType,
+                       'destination_single_port' => $destinationSinglePort,
+                       'destination_from_port' => $destinationPortFrom,
+                       'destination_to_port' => $destinationPortTo,
+	        	       'internal' => $internal,
+                       'readOnly' => $readonly);
+           
+}
+
+1;
---
+++ client/services/src/EBox/Services.pm
@@ -0,0 +1,300 @@
+# Copyright (C) 2005 Warp Networks S.L., DBS Servicios Informaticos S.L.
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License, version 2, as
+# published by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+# Class: EBox::Services
+#
+# 	This class is used to abstract services composed of
+# 	protocols and ports.
+#
+# 	
+
+package EBox::Services;
+
+use strict;
+use warnings;
+
+use base qw(EBox::GConfModule EBox::Model::ModelProvider);
+
+
+use EBox::Validate qw( :all );
+use EBox::Global;
+use EBox::Services::Model::ServiceConfigurationTable;
+use EBox::Services::Model::ServiceTable;
+use EBox::Services::Model::ServiceTableFilter;
+use EBox::Gettext;
+
+use EBox::Exceptions::InvalidData;
+use EBox::Exceptions::MissingArgument;
+use EBox::Exceptions::DataExists;
+use EBox::Exceptions::DataMissing;
+use EBox::Exceptions::DataNotFound;
+
+sub _create
+{
+    my $class = shift;
+    my $self = $class->SUPER::_create(name => 'services',
+            title => __n('Services'),
+            domain => 'ebox-objects',
+            @_);
+    $self->{'serviceModel'} = 
+        new EBox::Services::Model::ServiceTable(
+                'gconfmodule' => $self,
+                'directory' => 'serviceTable');
+    $self->{'serviceFilterModel'} = 
+        new EBox::Services::Model::ServiceTableFilter(
+                'gconfmodule' => $self,
+                'directory' => 'serviceTable');
+    $self->{'serviceConfigurationModel'} = 
+        new EBox::Services::Model::ServiceConfigurationTable(
+                'gconfmodule' => $self,
+                'directory' => 'serviceConfigurationTable');
+    bless($self, $class);
+    return $self;
+}
+
+## api functions
+
+# Method: models
+#
+#      Overrides <EBox::ModelImplementator::models>
+#
+sub models {
+    my ($self) = @_;
+
+    return [$self->{'serviceConfigurationModel'}, $self->{'serviceModel'}, 
+            $self->{'serviceFilterModel'}];
+}
+
+# Method: serviceNames
+#
+# 	Fetch all the service identifiers and names
+#
+# Returns:
+#
+# 	Array ref of  hash refs which contain:
+#
+# 	'id' - service identifier
+# 	'name' service name
+#
+#	Example:
+#         [
+#          {
+#            'name' => 'ssh',
+#            'id' => 'serv7999'
+#          },
+#          {
+#            'name' => 'ftp',
+#            'id' => 'serv7867'
+#          }
+#        ];
+sub serviceNames
+{
+    my ($self) = @_;
+
+    my @services;
+    foreach my $service (@{$self->{'serviceModel'}->printableValueRows()}) {
+        push (@services, {'id' => $service->{'id'}, 
+                'name' => $service->{'name'}});
+    }
+
+    return \@services;
+}
+
+# Method: serviceConfiguration
+#
+#	For a given service identifier it returns its service configuration,
+#	that is, the set of protocols and ports.
+#
+# Returns:
+#
+# 	Array ref of  hash refs which contain:
+#	
+#	protocol - it can take one of these: any, tcp, udp, tcp/udp, grep, icmp
+#	source	 - it can take: 
+#   			"any"
+#	    		An integer from 1 to 65536 -> 22
+#		    	Two integers separated by colons -> 22:25 
+#	destination - same as source
+#
+#	Example:
+#
+#	     'protocol' => 'tcp',
+#	     'source' => 'any',
+#       'destination' => '21:22',
+sub serviceConfiguration
+{
+    my ($self, $id) = @_;
+
+    throw EBox::Exceptions::ArgumentMissing("id") unless defined($id);
+
+    my $row = $self->{'serviceModel'}->find('id' => $id);
+
+    unless (defined($row)) {
+        throw EBox::Exceptions::DataNotFound('data' => 'id',
+                'value' => $id);
+    }
+
+    my $model = $self->{'serviceConfigurationModel'};
+    $model->setDirectory($row->{'configuration'}->{'directory'});
+
+    my @conf = map {$_->{'plainValueHash'}} @{$model->rows()};
+
+    return \@conf;
+}              
+
+# Method: addService 
+#
+#   Add a service to the services table	
+#
+# Parameters:
+#
+#   (NAMED)
+#   
+#   name        - service's name
+#   description - service's description
+#	protocol    - it can take one of these: any, tcp, udp, tcp/udp, grep, icmp
+#	sourcePort  - it can take: 
+#                   "any"
+#                    An integer from 1 to 65536 -> 22
+#                   Two integers separated by colons -> 22:25 
+#	destinationPort - same as source
+#   internal - boolean, internal services can't be modified from the UI
+#   readOnly - boolean, set the row unremovable from the UI
+#
+#	Example:
+#
+#       'name' => 'ssh',
+#       'description' => 'secure shell'.
+#	    'protocol' => 'tcp',
+#	    'sourcePort' => 'any',
+#       'destinationPort' => '21:22',
+sub addService 
+{
+    my ($self, %params) = @_;
+
+    $self->{'serviceModel'}->addService(%params);
+}
+
+# Method: removeService 
+#
+#  Remove a service from the  services table	
+#
+# Parameters:
+# 
+#   (NAMED)
+#   
+#   You can select the service using one of the following parameters:
+#       
+#       name - service's name
+#       id - service's id
+sub removeService 
+{
+    my ($self, %params) = @_;
+
+    unless (exists $params{'id'} or exists $params{'name'}) {
+        throw Exceptions::MissingArgument('service');
+    }
+    
+    my $model =  $self->{'serviceModel'};
+    my $id = $params{'id'};
+
+    if (not defined($id)) {
+        my $name = $params{'name'};
+        my $row = $model->findValue('name' => $name);
+        unless (defined($row)) {
+            throw EBox::Exceptions::External("service $name not found");
+        }
+        $id = $row->{'id'};
+    }
+    
+    $model->removeRow($id, 1);
+}
+
+# Method: serviceExists
+#
+#   Check if a given service already exits
+#
+# Paremeters:
+#
+#   (NAMED)
+#   You can select the service using one of the following parameters:
+#       
+#       name - service's name
+#       id - service's id
+sub serviceExists
+{
+    my ($self, %params) = @_;
+
+    unless (exists $params{'id'} or exists $params{'name'}) {
+        throw Exceptions::MissingArgument('service');
+    }
+    
+    my $model =  $self->{'serviceModel'};
+    my $id = $params{'id'};
+
+    my $row;
+    if (not defined($id)) {
+        my $name = $params{'name'};
+        $row = $model->findValue('name' => $name);
+    } else {
+        $row = $model->findValue('id' => $id);
+    }
+
+    return defined($row);
+}    
+
+# Method: serviceId
+#
+#   Given a service's name it returns its id
+#
+# Paremeters:
+#
+#   (POSITIONAL)
+#
+#   name - service's name
+#
+# Returns:
+#
+#   service's id if it exists, otherwise undef
+sub serviceId
+{
+    my ($self, $name) = @_;
+
+    unless (defined($name)) {
+        throw Exceptions::MissingArgument('name');
+    }
+    
+    my $model =  $self->{'serviceModel'};
+    my $row = $model->findValue('name' => $name);
+    
+    return $row->{'id'};
+}
+
+# Method: menu 
+#
+#       Overrides EBox::Module method.
+#   
+#
+sub menu
+{
+    my ($self, $root) = @_;
+    my $item = new EBox::Menu::Item(
+    'url' => 'Services/View/ServiceTable',
+    'text' => __($self->title),
+    'order' => 3);
+    $root->add($item);
+}
+
+1;
