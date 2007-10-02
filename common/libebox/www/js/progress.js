ad12f4a3d17e4058124952c4171d535b11cfea33
Javier Amor García <jamor@zentyal.com>
2007-10-02 08:21:46 +0000

    merged branch 168. js code for progress indicator. refs #168
    git-svn-id: http://svn.zentyal.org/zentyal/trunk@8289 866bc4c1-54da-0310-a958-9d5a9a0e8d04

---
+++ common/libebox/www/js/progress.js
@@ -0,0 +1,62 @@
+// code used by progress.mas
+
+// Update the page  
+function updatePage (xmlHttp) {
+    var rawResponse = xmlHttp.responseText;
+    var rawSections = rawResponse.split(",");
+
+   var response = {};
+   for  (var i=0; i < rawSections.length; i++ ) {
+       var parts = rawSections[i].split(":");
+       var name  = parts[0];
+       var value = parts[1] ;
+       response[name]  = value;
+   }
+   
+
+
+    if (xmlHttp.readyState == 4) {
+        // current item
+        if (('message' in response) && response.message.length > 0 ) {
+   	   $('currentItem').innerHTML = response.message;
+        }
+
+       	if ( ('ticks' in response) && (response.ticks >= 0)) {
+	     $('ticks').innerHTML = response.ticks;
+        }
+
+       	if ( ('totalTicks' in response) && (response.totalTicks > 0)) {
+	     $('totalTicks').innerHTML = response.totalTicks;
+        }         
+    }
+
+    if (response.state == 'done') {
+		Element.hide('progressing');
+    		Element.show('done');
+   }
+}
+
+// Generate an Ajax request to fetch the current package
+function callServer(progressId, url) {
+  // Build the URL to connect to
+  var par = "progress=" + progressId ;
+
+   new Ajax.Request(url, {
+   			  method: 'post',
+			  parameters: par,
+			  asynchronous: true,
+			  onSuccess: function (t) { updatePage(t) }
+			 }
+		    );
+
+}
+
+
+var pe;
+function createPeriodicalExecuter(progressId, currentItemUrl,  reloadInterval)
+{
+  var callServerCurriedBody = 	"callServer(" + progressId + ", '" + currentItemUrl  + "' )";
+  callServerCurried = new Function(callServerCurriedBody );
+
+  pe = new PeriodicalExecuter(callServerCurried, reloadInterval);
+}
