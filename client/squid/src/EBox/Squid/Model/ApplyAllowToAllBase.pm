d8a20682f4a697afdf304cfce5e44d5900179db7
Javier Amor García <jamor@zentyal.com>
2007-10-16 14:24:50 +0000

    list of allowed items are refreshed after setting the same policy for all closes #758
    git-svn-id: http://svn.zentyal.org/zentyal/trunk@8425 866bc4c1-54da-0310-a958-9d5a9a0e8d04

--- client/squid/src/EBox/Squid/Model/ApplyAllowToAllBase.pm
+++ client/squid/src/EBox/Squid/Model/ApplyAllowToAllBase.pm
@@ -38,6 +38,7 @@ sub setRow
   my $listModel = $self->listModel();
 
   my $rows_r = $listModel->rows();
+
   foreach my $row (@{ $rows_r }) {
     my $id       = $row->{id};
     my $allowed = $row->{valueHash}->{allowed};
@@ -46,6 +47,8 @@ sub setRow
 
     $listModel->setTypedRow($id, { allowed => $allowed } );
   }
+;
+
 
   # XXX update listModel
 }
@@ -111,6 +114,59 @@ sub _table
 sub Viewer
 {
   return  '/ajax/squid/applyAllForm.mas';
+
+}
+
+
+
+# custom changeRowJS to update the list
+sub changeRowJS
+{
+  my ($self, $editId, $page) = @_;
+
+  my  $function = 'applyAllChangeRows("%s", "%s", %s, "%s",'.
+			'"%s", %s, %s, %s)';
+
+  
+
+  my $listModel   = $self->listModel();
+  my $changeViewListJS = $listModel->changeViewJS(
+						changeType => 'changeList',
+						editId     => 0,
+						page       => 0,
+						isFilter   => 0,
+
+
+					       );
+
+  my $table = $self->table();
+  my $fields = $self->_paramsWithSetterJS();
+
+  $fields =~ s/'/"/g;
+
+  my $onCompleteJS =  <<END;
+  			 function(t) { 
+			  highlightRow( id, false);
+			  stripe("dataTable", "#ecf5da", "#ffffff");
+			  $changeViewListJS;
+			}
+END
+
+
+  my $JS = sprintf ($function, 
+			 $table->{'actions'}->{'editField'},
+			 $table->{'tableName'},
+			 $fields,
+			 $table->{'gconfdir'},
+			 $editId,
+			 $page,
+		         0, # force
+		         $onCompleteJS
+		   );
+
+
+  EBox::debug("applyToAll js:\n $JS");
+  return $JS;
 
 }
 
