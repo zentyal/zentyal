2159f93ea4c4b12a6d5811e29b4070ad4332fe80
Javier Uruen <juruen@zentyal.com>
2007-11-21 21:04:18 +0000

    add event field to log db
    git-svn-id: http://svn.zentyal.org/zentyal/trunk@8705 866bc4c1-54da-0310-a958-9d5a9a0e8d04

--- client/firewall/tools/sqllog/firewall.sql
+++ client/firewall/tools/sqllog/firewall.sql
@@ -1,2 +1,3 @@
-CREATE TABLE firewall(fw_in VARCHAR(16),  fw_out VARCHAR(16), fw_src INET, fw_dst INET, fw_proto VARCHAR(16), fw_spt INT, fw_dpt INT, timestamp TIMESTAMP);
+CREATE TABLE firewall(fw_in VARCHAR(16),  fw_out VARCHAR(16), fw_src INET,
+fw_dst INET, fw_proto VARCHAR(16), fw_spt INT, fw_dpt INT, event VARCHAR(16), timestamp TIMESTAMP);
 CREATE INDEX firewall_i on firewall(timestamp);
