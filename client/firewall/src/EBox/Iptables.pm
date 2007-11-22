cd7b2494ba4f739bfe961d8592b8017f3279b31a
Enrique J. Hernández Blasco <ejhernandez@zentyal.com>
2007-11-22 10:37:57 +0000

    Fixing eating up from commit [8711]
    git-svn-id: http://svn.zentyal.org/zentyal/trunk@8712 866bc4c1-54da-0310-a958-9d5a9a0e8d04

--- client/firewall/src/EBox/Iptables.pm
+++ client/firewall/src/EBox/Iptables.pm
@@ -28,7 +28,7 @@ use EBox::Sudo qw( :all );
 
 my $new = " -m state --state NEW ";
 
-use constant IPT_MODULES => ('ip_conntrack_ftp', 'ip_nat_ftp');
+use constant IPT_MODULES => ('ip_conntrack_ftp', 'ip_nat_ftp', 'ip_conntrack_tftp');
 use constant SYSLOG_LEVEL => 7;
 
 # Constructor: new
