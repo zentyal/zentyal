f56957d7df8ba1872a297ca8d0037a1a771c44ec
Javier Uruen <juruen@zentyal.com>
2007-11-27 15:29:12 +0000

    indentation sanity :)
    git-svn-id: http://svn.zentyal.org/zentyal/trunk@8772 866bc4c1-54da-0310-a958-9d5a9a0e8d04

--- common/libebox/src/EBox/ColourRange.pm
+++ common/libebox/src/EBox/ColourRange.pm
@@ -6,28 +6,24 @@ use warnings;
 
 
 my @colours = (
-
-	'000000',   #black
-	'00BFFF',  # deep sky blue
-	'5C4033', # dark brown
+        '000000', #black
+        '00BFFF', # deep sky blue
+        '5C4033', # dark brown
         '2F4F2F', # dark green
         'FF8C00', # dark orange
         'FF1493', # deep pink
         '9932CC', # darok orchid
-	'D9D919', # bright gold  
-
+        'D9D919', # bright gold  
         'C0C0C0', # silver grey
         '000080', # navy blue
         'DEB887', # burlywood
-	'ADFF2F', # green yellow
+        'ADFF2F', # green yellow
         'FF2400', # orange red
         'FFB6C1', # light pink
         'DDA0DD', # plum
         'B8860B', # DarkGoldenrod      
-
-         '856363', # green cooper
-
-	      );
+        '856363', # green cooper
+);
 
 sub range
 {
