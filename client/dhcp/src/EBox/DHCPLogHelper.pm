649d6b52aa981df9aaaf85b02e1a02590b61f03f
Enrique J. Hernández Blasco <ejhernandez@zentyal.com>
2008-06-13 12:10:38 +0000

    Bugfix. Logging were not working with hostnames with characters different from alphanumeric plus "_" and correcting indentation
    git-svn-id: http://svn.zentyal.org/zentyal/trunk@10597 866bc4c1-54da-0310-a958-9d5a9a0e8d04

--- client/dhcp/src/EBox/DHCPLogHelper.pm
+++ client/dhcp/src/EBox/DHCPLogHelper.pm
@@ -25,76 +25,76 @@ use EBox::Gettext;
 
 use constant DHCPLOGFILE => '/var/log/syslog';
 
-sub new 
+sub new
 {
-        my $class = shift;
-        my $self = {};
-        bless($self, $class);
-        return $self;
+    my $class = shift;
+    my $self = {};
+    bless($self, $class);
+    return $self;
 }
 
-sub domain { 
-        return 'ebox-dhcp';
+sub domain {
+    return 'ebox-dhcp';
 }
 
 
 # Method: logFiles 
 #
-#	This function must return the file or files to be read from.
+#       This function must return the file or files to be read from.
 #
 # Returns:
 #
-#	array ref - containing the whole paths
+#       array ref - containing the whole paths
 #
 sub logFiles
 {
-	return [DHCPLOGFILE];	
+    return [DHCPLOGFILE];
 }
 
 # Method: processLine 
 #
-#	This fucntion will be run every time a new line is recieved in
-#	the associated file. You must parse the line, and generate
-#	the messages which will be logged to ebox through an object
-#	implementing EBox::AbstractLogger interface.
+#       This fucntion will be run every time a new line is recieved in
+#       the associated file. You must parse the line, and generate
+#       the messages which will be logged to ebox through an object
+#       implementing EBox::AbstractLogger interface.
 #
 # Parameters:
 #
-#	file - file name
-# 	line - string containing the log line
-#	dbengine- An instance of class implemeting AbstractDBEngineinterface
-# 	
+#       file - file name
+#       line - string containing the log line
+#       dbengine- An instance of class implemeting AbstractDBEngineinterface
+#
 sub processLine # (file, line, logger) 
 {
-	my ($self, $file, $line, $dbengine) = @_;
+    my ($self, $file, $line, $dbengine) = @_;
 
-	return unless ($line =~ /^(\w+\s+\d+ \d\d:\d\d:\d\d) \w+ dhcpd:.*/);
-	
-	my $date = $1;
-	my ($ip, $mac, $iface, $event);
-        if ($line =~ /^.*DHCPACK on ([\d.]+) to ([\d:a-f]+) via (\w+)/) {
-		$ip = $1;
-		$mac = $2;
-		$iface =$3;
-		$event = 'leased';
-	} elsif ($line =~ /^.*DHCPRELEASE of ([\d.]+) from ([\d:a-f]+) via (\w+)/) {
-		$ip = $1;
-		$mac = $2;
-		$iface =$3;
-		$event = 'released';
-	} else {
-		return;
-	}
+    return unless ($line =~ /^(\w+\s+\d+ \d\d:\d\d:\d\d) \S+ dhcpd:.*/);
 
-	my $timestamp = $date . ' ' . (${[localtime(time)]}[5] + 1900);
-	my $data = { 
-			'timestamp' => $timestamp, '
-			ip' => $ip, 'mac' => $mac, 
-			'interface' => $iface, 
-			'event' => $event 
-		    };
-	$dbengine->insert('leases', $data);
-	
+    my $date = $1;
+    my ($ip, $mac, $iface, $event);
+    if ($line =~ /^.*DHCPACK on ([\d.]+) to ([\d:a-f]+) via (\w+)/) {
+        $ip = $1;
+        $mac = $2;
+        $iface =$3;
+        $event = 'leased';
+    } elsif ($line =~ /^.*DHCPRELEASE of ([\d.]+) from ([\d:a-f]+) via (\w+)/) {
+        $ip = $1;
+        $mac = $2;
+        $iface =$3;
+        $event = 'released';
+    } else {
+        return;
+    }
+
+    my $timestamp = $date . ' ' . (${[localtime(time)]}[5] + 1900);
+    my $data = {
+        'timestamp' => $timestamp, '
+                        ip' => $ip, 'mac' => $mac, 
+        'interface' => $iface, 
+        'event' => $event 
+       };
+    $dbengine->insert('leases', $data);
+
 }
 
 1;
