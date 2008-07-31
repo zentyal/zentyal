423da70c0ffeef3b1ecf771703c9c9e0ef732359
Javier Amor García <jamor@zentyal.com>
2008-07-31 13:33:49 +0000

    removed deprecated tests
    git-svn-id: http://svn.zentyal.org/zentyal/trunk@11092 866bc4c1-54da-0310-a958-9d5a9a0e8d04

--- client/mail/src/EBox/Mail/Test.pm
+++ client/mail/src/EBox/Mail/Test.pm
@@ -43,21 +43,21 @@ sub setUpConfiguration : Test(setup)
    
 
     my @config = (
-		  '/ebox/modules/mail/active'  => 1,
-		  '/ebox/modules/mail/filter'  => 1,
-		  '/ebox/modules/mail/external_filter_name'  => 'custom',
-		  );
+                  '/ebox/modules/mail/active'  => 1,
+                  '/ebox/modules/mail/filter'  => 1,
+                  '/ebox/modules/mail/external_filter_name'  => 'custom',
+                  );
 
     EBox::GConfModule::TestStub::setConfig(@config);
     EBox::Global::TestStub::setEBoxModule('mail' => 'EBox::Mail');
 
     EBox::TestStubs::fakeEBoxModule(name => 'firewall',
-				    subs => [
-					     availablePort => sub { 
-					       return 1
-					     },
-					     ]
-				   );
+                                    subs => [
+                                             availablePort => sub { 
+                                               return 1
+                                             },
+                                             ]
+                                   );
  }
 
 
@@ -66,8 +66,8 @@ sub setUpConfiguration : Test(setup)
 sub fakeGetIfacesForAddress 
 {
   Test::MockObject->fake_module('EBox::Mail',
-				'_getIfacesForAddress' => sub { return ['eth0']  },
-			       );
+                                '_getIfacesForAddress' => sub { return ['eth0']  },
+                               );
 }
 
 
@@ -121,46 +121,46 @@ sub extendedRestoreTest : Test(7)
   # setup backup dirs
 
   my $beforeBackup = {
-		       $md => {
-			      root => {  "mbox" => 'fake mbox', },
-			     },
+                       $md => {
+                              root => {  "mbox" => 'fake mbox', },
+                             },
 
-		      $vmd => {
-			       'monos.org' => {
-					       macaco => {
-							  tmp => {},
-							  new => { 
-								  '1177498277.V801Id1438.localhost.localdomain,S=690' => 'fake mail',
-								 },
-							  cur => {},
-							  maildirsize => 'fake file',
-							 },
-					      },
+                      $vmd => {
+                               'monos.org' => {
+                                               macaco => {
+                                                          tmp => {},
+                                                          new => { 
+                                                                  '1177498277.V801Id1438.localhost.localdomain,S=690' => 'fake mail',
+                                                                 },
+                                                          cur => {},
+                                                          maildirsize => 'fake file',
+                                                         },
+                                              },
 
-			      },
+                              },
 
-		     };
+                     };
 
   my $afterBackup  = {
-		      $md => {
-			     },
+                      $md => {
+                             },
 
-		      $vmd => {
-			       'monos.org' => {
-					       macaco => {
-							  tmp => {},
-							  new => { 
-								 },
-							  cur => {
-								  '1177498277.V801Id1438.localhost.localdomain,S=690' => 'fake mail',
-								 },
-							  maildirsize => 'fake file',
-							 },
-					      },
+                      $vmd => {
+                               'monos.org' => {
+                                               macaco => {
+                                                          tmp => {},
+                                                          new => { 
+                                                                 },
+                                                          cur => {
+                                                                  '1177498277.V801Id1438.localhost.localdomain,S=690' => 'fake mail',
+                                                                 },
+                                                          maildirsize => 'fake file',
+                                                         },
+                                              },
 
-			      },
+                              },
 
-		    };
+                    };
 
   system "rm -rf @backupDirs";
   spew_tree($varDir => $beforeBackup );
@@ -176,82 +176,8 @@ sub extendedRestoreTest : Test(7)
 }
 
 
-sub setFilterServiceTest : Test(5)
-{
-  my $mail = EBox::Global->modInstance('mail');
-  my @states = (0, 1, 1, 0, 0);
-  foreach my $service ( 'filter') {
-  foreach my $state (@states) {
-    $mail->setService($service, $state);
-    my $newState = $mail->service($service) ? 1 : 0;
-    is $newState, $state, "checking service $service state $state";
-  }
-  }
-
-}
-
-sub filterTest : Test(14)
-{
-
-  my $moduleFilterName  = 'testModuleFilter';
-  my %moduleFilterAttrs = (
-			   address => '127.0.0.1',
-			   port    => 10004,
-			   forwardPort => 10005,
-			   prettyName => 'test filter',
-			  );
 
 
-
-
-  fakeEBoxModule(
-		 name => 'filterModule',
-		 isa  => ['EBox::Mail::FilterProvider'],
-		 subs => [
-			  mailFilter => sub {
-			    return ($moduleFilterName =>  \%moduleFilterAttrs);
-			  },
-			 ]
-		);
-  fakeGetIfacesForAddress();
-
-
-  my $customAddress = '192.168.4.2';
-  my $customPort    = 1001;
-  my $customFwPort =  1009;
-
-  my $mail = EBox::Global->modInstance('mail');
-
-  lives_ok {
-    $mail->setExternalFilter('custom');
-    $mail->setIPFilter($customAddress);
-    $mail->setPortFilter($customPort);
-    $mail->setFWPort($customFwPort);
-  } 'Setting custom port and attributes';
-
-  is $mail->externalFilter, 'custom', 'checking custom filter name';
-  is $mail->ipfilter,   $customAddress, 'checking custom filter address';
-  is $mail->portfilter, $customPort, 'checking custom filter port';
-  is $mail->fwport, $customFwPort, 'checking custom filter forward port';
-
-
-  dies_ok { $mail->setExternalFilter('inexistentMod')  } 
-    'trying to set the external filter to a inexistent filter';
-
-  lives_ok { $mail->setExternalFilter($moduleFilterName)  }
-    'Setting the external filter to a module provided filter';
-  dies_ok { $mail->setIPFilter($customAddress) } 
-    'trying to set the address of a module filter' ;
-  dies_ok { $mail->setPortFilter($customPort)  }
-    'trying to set the port of a module filter';
-  dies_ok { $mail->setFWPort($customFwPort)  }
-    'trying to set the forward port of a module filter';
-
-  is $mail->externalFilter, $moduleFilterName, 'checking module filter name';
-  is $mail->ipfilter, $moduleFilterAttrs{address}, 'checking module filter address';
-  is $mail->portfilter, $moduleFilterAttrs{port}, 'checking module filter port';
-  is $mail->fwport, $moduleFilterAttrs{forwardPort}, 'checking module filter forward port';
-}
 
 
 sub _fakeStorageMailDirs
@@ -259,8 +185,8 @@ sub _fakeStorageMailDirs
   my (@dirs) = @_;
 
   Test::MockObject->fake_module('EBox::Mail',
-				_storageMailDirs => sub { return @dirs }
-			       );
+                                _storageMailDirs => sub { return @dirs }
+                               );
 }
 
 
