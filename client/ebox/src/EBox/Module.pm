4bc81e8a8e34fd4222a60fba42211da0f86fd770
Enrique J. Hernández Blasco <ejhernandez@zentyal.com>
2008-12-22 17:28:20 +0000

    Improve restart debugging
    git-svn-id: http://svn.zentyal.org/zentyal/trunk@11940 866bc4c1-54da-0310-a958-9d5a9a0e8d04

--- client/ebox/src/EBox/Module.pm
+++ client/ebox/src/EBox/Module.pm
@@ -134,8 +134,12 @@ sub restartService
 
 	$log->info("Restarting service for module: " . $self->name);
 	try {
-		$self->_regenConfig('restart' => 1);
-	} finally {
+            $self->_regenConfig('restart' => 1);
+	} otherwise  {
+            my ($ex) = @_;
+            $log->error("Error restarting service: $ex");
+            throw $ex;
+        } finally {
 		$self->_unlock();
 	};
 }
