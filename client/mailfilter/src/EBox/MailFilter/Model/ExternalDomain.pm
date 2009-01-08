aaf78bb63804ad23775edcfea4dd771316a394b4
Javier Uruen <juruen@zentyal.com>
2009-01-08 02:58:33 +0000

    revert change and show head title in external domain
    git-svn-id: http://svn.zentyal.org/zentyal/trunk@12038 866bc4c1-54da-0310-a958-9d5a9a0e8d04

--- client/mailfilter/src/EBox/MailFilter/Model/ExternalDomain.pm
+++ client/mailfilter/src/EBox/MailFilter/Model/ExternalDomain.pm
@@ -140,14 +140,5 @@ sub allowed
     return \@allowed;
 }
 
-# Method: headTitle
-#
-#   Overrides <EBox::Model::Component::headTitle> to not
-#   write a head title within the tabbed composite
-sub headTitle
-{
-    return undef;
-}
-
 1;
 
