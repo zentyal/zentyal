bf04c86aa18a330cd5ca430dd823a175789c6ada
Javier Amor García <jamor@zentyal.com>
2009-02-03 12:02:30 +0000

    fixed bug with prerouting table when transparent proxy is not active, reindented file
    git-svn-id: http://svn.zentyal.org/zentyal/trunk@12360 866bc4c1-54da-0310-a958-9d5a9a0e8d04

--- client/squid/src/EBox/SquidOnlyFirewall.pm
+++ client/squid/src/EBox/SquidOnlyFirewall.pm
@@ -27,69 +27,72 @@ use EBox::Gettext;
 
 sub new 
 {
-        my $class = shift;
-        my %opts = @_;
-        my $self = $class->SUPER::new(@_);
-        bless($self, $class);
-        return $self;
+    my $class = shift;
+    my %opts = @_;
+    my $self = $class->SUPER::new(@_);
+    bless($self, $class);
+    return $self;
 }
 
 sub _trans_prerouting
 {
-	my $self = shift;
-	my $sq = EBox::Global->modInstance('squid');
-	my $net = EBox::Global->modInstance('network');
-	my $sqport = $sq->port();
-	my @rules = ();
+    my ($self) = @_;
+    my $sq = EBox::Global->modInstance('squid');
+    my $net = EBox::Global->modInstance('network');
+    my $sqport = $sq->port();
+    my @rules = ();
+    
+    my @ifaces = @{$net->InternalIfaces()};
+    my $pol = $sq->globalPolicy();
+    foreach my $ifc (@ifaces) {
+        my $addr = $net->ifaceAddress($ifc);
+        (defined($addr) && $addr ne "") or next;
+        
+        my $r = "-i $ifc -d ! $addr -p tcp " . 
+            "--dport 80 -j REDIRECT --to-ports $sqport";
+        push(@rules, $r);
+    }
 
-	my @ifaces = @{$net->InternalIfaces()};
-	my $pol = $sq->globalPolicy();
-	foreach my $ifc (@ifaces) {
-		my $addr = $net->ifaceAddress($ifc);
-		(defined($addr) && $addr ne "") or next;
-
-		my $r = "-i $ifc -d ! $addr -p tcp " . 
-			"--dport 80 -j REDIRECT --to-ports $sqport";
-		push(@rules, $r);
-	}
-	return \@rules;
+    return \@rules;
 }
 
 sub prerouting
 {
-	my $self = shift;
-	my $sq = EBox::Global->modInstance('squid');
-	if ($sq->transproxy()) {
-		return $self->_trans_prerouting();
-	}
+    my ($self) = @_;
+    my $sq = EBox::Global->modInstance('squid');
+    if ($sq->transproxy()) {
+        return $self->_trans_prerouting();
+    }
+
+    return [];
 }
 
 sub input
 {
-	my $self = shift;
-	my $sq = EBox::Global->modInstance('squid');
-	my $net = EBox::Global->modInstance('network');
-	my $sqport = $sq->port();
-	my @rules = ();
-
-	my @ifaces = @{$net->InternalIfaces()};
-	my $pol = $sq->globalPolicy();
-	foreach my $ifc (@ifaces) {
-		my $r = "-m state --state NEW -i $ifc -p tcp --dport $sqport ".
-			"-j ACCEPT";
-		push(@rules, $r);
-	}
-	return \@rules;
+    my ($self) = @_;
+    my $sq = EBox::Global->modInstance('squid');
+    my $net = EBox::Global->modInstance('network');
+    my $sqport = $sq->port();
+    my @rules = ();
+    
+    my @ifaces = @{$net->InternalIfaces()};
+    my $pol = $sq->globalPolicy();
+    foreach my $ifc (@ifaces) {
+        my $r = "-m state --state NEW -i $ifc -p tcp --dport $sqport ".
+            "-j ACCEPT";
+        push(@rules, $r);
+    }
+    return \@rules;
 }
 
 sub output
 {
-	my $self = shift;
-	my $sq = EBox::Global->modInstance('squid');
-	my @rules = ();
-	push(@rules, "-m state --state NEW -p tcp --dport 80 -j ACCEPT");
-	push(@rules, "-m state --state NEW -p tcp --dport 443 -j ACCEPT");
-	return \@rules;
+    my ($self) = @_;
+    my $sq = EBox::Global->modInstance('squid');
+    my @rules = ();
+    push(@rules, "-m state --state NEW -p tcp --dport 80 -j ACCEPT");
+    push(@rules, "-m state --state NEW -p tcp --dport 443 -j ACCEPT");
+    return \@rules;
 }
 
 1;
