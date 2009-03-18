330854ed95a143d363cf36e5ab4d15f93b90a634
José Antonio Calvo <jacalvo@zentyal.com>
2009-03-18 07:36:24 +0000

    Added ebox usplash theme. Updated build script.
    git-svn-id: http://svn.zentyal.org/zentyal/trunk@12894 866bc4c1-54da-0310-a958-9d5a9a0e8d04

--- extra/livecd/buildLiveCD.sh
+++ extra/livecd/buildLiveCD.sh
@@ -2,7 +2,7 @@
 
 FILES_DIR="./files"
 
-PACKAGES_TO_INSTALL="xserver-xorg-base xinit xfonts-base firefox-3 icewm"
+PACKAGES_TO_INSTALL="xserver-xorg-core xinit xfonts-base firefox icewm usplash usplash-ubuntu-theme"
 REMASTERSYS_PACKAGES="remastersys"
 
 echo '' >> /etc/apt/sources.list
@@ -10,14 +10,13 @@ echo 'deb http://www.remastersys.klikit-linux.com/repository remastersys/' >> /e
 echo 'deb http://es.archive.ubuntu.com/ubuntu/ hardy main restricted universe multiverse' >> /etc/apt/sources.list
 echo 'deb http://es.archive.ubuntu.com/ubuntu/ hardy-updates main restricted universe multiverse' >> /etc/apt/sources.list
 apt-get update || exit 1
-apt-get install -y $REMASTERSYS_PACKAGES || exit 1
+apt-get install -y --force-yes $REMASTERSYS_PACKAGES || exit 1
 
 
 
-test -d $FILES_DIR || (echo "Incorrect files directory $FILES_DIR" && exit 1)
+test -d $FILES_DIR || (echo "Incorrect files directory $FILES_DIR"; false) || exit 1
 
-apt-get install -y $PACKAGES_TO_INSTALL || exit 1
-
+apt-get install -y --force-yes $PACKAGES_TO_INSTALL || exit 1
 
 cp -v $FILES_DIR/remastersys.conf /etc/remastersys.conf || exit 1
 
@@ -27,11 +26,19 @@ cp -v $FILES_DIR/eboxlive /etc/init.d/ || exit 1
 chmod a+rx  /etc/init.d/eboxlive       || exit 1
 update-rc.d eboxlive start 99 2 .      || exit 1
 
+echo Setting ebox usplash theme...
+cp -v $FILES_DIR/ebox-theme.so /usr/lib/uspash/ebox-theme.so || exit 1
+update-alternatives --set usplash-artwork.so /usr/lib/usplash/ebox-theme.so
+update-initramfs -u
 
 echo Copyng firefox default profile..
-cp -r $FILES_DIR/firefox_profile/* /etc/firefox-3.0/profile || exit 1
+cp -r $FILES_DIR/firefox_profile/* /etc/firefox/profile || exit 1
 
+#echo Creating samba directories...
+#mkdir /home/samba/{users,groups,profiles,netlogon}
 
+echo Fixing /etc/init.d/postfix...
+sed -i 's/usr\/lib/rofs\/usr\/lib' /etc/init.d/postfix
 
 echo Running "remastersys dist"
 remastersys dist
