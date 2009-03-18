32481e4ee48dd585f1f871be38b29fe460f110e0
José Antonio Calvo <jacalvo@zentyal.com>
2009-03-18 08:32:19 +0000

    Fixed usplash stuff removed commented lines add icewm-themes package
    git-svn-id: http://svn.zentyal.org/zentyal/trunk@12899 866bc4c1-54da-0310-a958-9d5a9a0e8d04

--- extra/livecd/buildLiveCD.sh
+++ extra/livecd/buildLiveCD.sh
@@ -2,7 +2,7 @@
 
 FILES_DIR="./files"
 
-PACKAGES_TO_INSTALL="xserver-xorg-core xinit xfonts-base firefox icewm usplash usplash-theme-ubuntu"
+PACKAGES_TO_INSTALL="xserver-xorg-core xinit xfonts-base firefox icewm icewm-themes usplash usplash-theme-ubuntu"
 REMASTERSYS_PACKAGES="remastersys"
 
 echo '' >> /etc/apt/sources.list
@@ -28,16 +28,12 @@ update-rc.d eboxlive start 99 2 .      || exit 1
 
 echo Setting ebox usplash theme...
 cp -v $FILES_DIR/ebox-theme.so /usr/lib/usplash/ebox-theme.so || exit 1
-update-alternatives --set usplash-artwork.so /usr/lib/usplash/ebox-theme.so
+ln -sf /usr/lib/usplash/ebox-theme.so /usr/lib/usplash/usplash-artwork.so
 update-initramfs -u
 
 echo Copyng firefox default profile..
 (test -d /etc/firefox) && (cp -r $FILES_DIR/firefox_profile/* /etc/firefox/profile || exit 1)
 (test -d /etc/firefox-3.0) && (cp -r $FILES_DIR/firefox_profile/* /etc/firefox-3.0/profile || exit 1)
-
-
-#echo Creating samba directories...
-#mkdir /home/samba/{users,groups,profiles,netlogon}
 
 echo Fixing /etc/init.d/postfix...
 sed -i 's/usr\/lib/rofs\/usr\/lib' /etc/init.d/postfix
