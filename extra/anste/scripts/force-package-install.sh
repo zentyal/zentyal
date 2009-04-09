cdaff3e2d410f8e26863b925e7e1ff6cd817fc83
José Antonio Calvo <jacalvo@zentyal.com>
2009-04-09 17:55:48 +0000

    remove redirection to another log file
    git-svn-id: http://svn.zentyal.org/zentyal/trunk@13158 866bc4c1-54da-0310-a958-9d5a9a0e8d04

--- extra/anste/scripts/force-package-install.sh
+++ extra/anste/scripts/force-package-install.sh
@@ -4,6 +4,4 @@ export DEBIAN_FRONTEND=noninteractive
 
 APT_OPTIONS='-o Dpkg::Options::=--force-confnew -o Dpkg::Options::=--force-confdef';
 
-LOG=/var/log/anste-force-install-`date +%y%m%d%H%M%S`.log
-
-apt-get install -f -y --force-yes $APT_OPTIONS &> $LOG 
+apt-get install -f -y --force-yes $APT_OPTIONS
