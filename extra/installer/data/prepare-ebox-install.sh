763e7f87bbf2b0859979e7c417395ca7a6eea009
José Antonio Calvo <jacalvo@zentyal.com>
2009-05-05 16:59:13 +0000

    fixed internationalization issue
    git-svn-id: http://svn.zentyal.org/zentyal/trunk@13487 866bc4c1-54da-0310-a958-9d5a9a0e8d04

--- extra/installer/data/prepare-ebox-install.sh
+++ extra/installer/data/prepare-ebox-install.sh
@@ -9,6 +9,8 @@ update-rc.d $INSTALLER start  41 S .
 
 # Copy locale
 cp /tmp/locale.gen /var/tmp
+# Copy .mo files
+cp -r /tmp/locale /usr/share/
 
 cp /tmp/enable-all-modules.pl /var/tmp
 cp -r /tmp/package-installer/* /var/tmp/
