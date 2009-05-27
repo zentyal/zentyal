1f235a01044671cb725d63d55e841f9572cb0265
Jorge Salamero Sanz <jsalamero@zentyal.com>
2009-05-27 10:44:29 +0000

    Adds semi-automatic install entry in the boot menu.
    git-svn-id: http://svn.zentyal.org/zentyal/trunk@13719 866bc4c1-54da-0310-a958-9d5a9a0e8d04

--- extra/installer/scripts/30_put_ebox_stuff.sh
+++ extra/installer/scripts/30_put_ebox_stuff.sh
@@ -6,6 +6,8 @@ test -d $CD_BUILD_DIR || (echo "cd build directory not found."; false) || exit 1
 test -d $DATA_DIR  || (echo "data directory not found."; false) || exit 1
 
 cp $DATA_DIR/ubuntu-ebox.seed $CD_BUILD_DIR/preseed/ubuntu-server.seed
+cp $DATA_DIR/ubuntu-ebox-auto.seed $CD_BUILD_DIR/preseed/ubuntu-server-auto.seed
+sed -e s:VERSION:$EBOX_VERSION$EBOX_APPEND: < $DATA_DIR/isolinux-ebox.cfg.template > $CD_BUILD_DIR/isolinux/isolinux.cfg
 
 test -d $CD_EBOX_DIR || mkdir -p $CD_EBOX_DIR
 
