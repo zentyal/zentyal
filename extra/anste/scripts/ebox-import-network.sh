f4ad670e36efc1a487652306095b0e9ffe342184
Javier Uruen <juruen@zentyal.com>
2009-06-22 10:04:30 +0000

    wipe out network configuration before importing
    git-svn-id: http://svn.zentyal.org/zentyal/trunk@14017 866bc4c1-54da-0310-a958-9d5a9a0e8d04

--- extra/anste/scripts/ebox-import-network.sh
+++ extra/anste/scripts/ebox-import-network.sh
@@ -3,4 +3,9 @@
 # Wait for ebox start
 sleep 10
 
+# wipe out current network configuration
+rm -rf /var/lib/ebox/gconf/ebox/modules/network
+pkill gconf
+
+# import configuration
 /usr/share/ebox-network/ebox-netcfg-import
