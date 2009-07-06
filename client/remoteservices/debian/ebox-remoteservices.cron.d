086928b04ef77ee597046749975befd58d5c0ce6
Isaac Clerencia <iclerencia@zentyal.com>
2009-07-06 11:39:48 +0000

    Remove debian dirs from source
    git-svn-id: http://svn.zentyal.org/zentyal/trunk@14183 866bc4c1-54da-0310-a958-9d5a9a0e8d04

--- client/remoteservices/debian/ebox-remoteservices.cron.d
+++
@@ -1,7 +0,0 @@
-# /etc/cron.d/ebox-remoteservices: crontab entries for the ebox-remoteservices package
-
-SHELL=/bin/sh
-PATH=/usr/bin:/bin
-
-# Get the clients and their eBox assigned to our VPN server each minute
-0-59/15 * * * * root /usr/share/ebox/ebox-notify-mon-stats >> /var/log/ebox/ebox.log 2>&1
--- client/usersandgroups/debian/ebox-usersandgroups.cron.d
+++
@@ -1,7 +0,0 @@
-# /etc/cron.d/ebox-usersandgroups
-
-SHELL=/bin/sh
-PATH=/usr/bin:/bin
-
-# Sync the slaves every 5 minutes if there are missing changes
-*/5 * * * * root /usr/share/ebox-usersandgroups/slave-sync
