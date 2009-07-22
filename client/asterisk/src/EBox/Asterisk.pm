d8ee5f4889479a94eda59137063a37cd901ca400
Jorge Salamero Sanz <jsalamero@zentyal.com>
2009-07-22 08:41:33 +0000

    Bugfix: Makes Asterisk depend on Network. Bugfix: Adds s parameter to MeetMe so users and admins can enter the menu.
    git-svn-id: http://svn.zentyal.org/zentyal/trunk@14422 866bc4c1-54da-0310-a958-9d5a9a0e8d04

--- client/asterisk/src/EBox/Asterisk.pm
+++ client/asterisk/src/EBox/Asterisk.pm
@@ -390,12 +390,14 @@ sub _setMeetings
         #my $alias = $row->valueByName('alias'); FIXME not implemented yet
         my $pin = $row->valueByName('pin');
         my $adminpin = $row->valueByName('adminpin');
+        my $options = ",s";
+        my $data = $exten . $options;
         push (@meetings, { exten => $exten,
                            pin => $pin,
                            adminpin => $adminpin,
                          });
         #$extns->addExtension($alias, 1, 'GoTo', $exten); #FIXME when we delete these extensions? XXX
-        $extns->addExtension($exten, 1, 'MeetMe', $exten);
+        $extns->addExtension($exten, 1, 'MeetMe', $data);
     }
 
     my @params = ( meetings => \@meetings );
