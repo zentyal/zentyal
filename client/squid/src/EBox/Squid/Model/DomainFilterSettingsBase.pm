6123e7647d704571f86d50fc5214863c0abc8a2f
Javier Amor García <jamor@zentyal.com>
2009-07-31 11:09:56 +0000

    fixed help message
    git-svn-id: http://svn.zentyal.org/zentyal/trunk@14520 866bc4c1-54da-0310-a958-9d5a9a0e8d04

--- client/squid/src/EBox/Squid/Model/DomainFilterSettingsBase.pm
+++ client/squid/src/EBox/Squid/Model/DomainFilterSettingsBase.pm
@@ -42,7 +42,7 @@ sub _tableHeader
             defaultValue     => 0,
             editable         => 1,
             help         => __('If this is enabled, ' .
-                                   'any domain which is neither present either in the ' .
+                                   'any domain which is neither present neither in the ' .
                                 '<i>Domains rules</i> nor in the <i>Domain list files</i> sections below will be ' .
                                 'forbidden.'),
 
