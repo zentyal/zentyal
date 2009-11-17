d1eaa9f9595f181336faeda057e196a4e75e1a92
José Antonio Calvo <jacalvo@zentyal.com>
2009-11-17 15:47:57 +0000

    fix url in generate_extras script, also include metapackages instead of only ebox module packages
    git-svn-id: http://svn.zentyal.org/zentyal/trunk@15236 866bc4c1-54da-0310-a958-9d5a9a0e8d04

--- extra/installer/generate_extras.sh
+++ extra/installer/generate_extras.sh
@@ -11,7 +11,7 @@ sudo debootstrap --arch=$ARCH $VERSION $CHROOT
 echo "deb http://archive.ubuntu.com/ubuntu hardy main restricted universe multiverse" > sources.list
 echo "deb http://archive.ubuntu.com/ubuntu hardy-updates main restricted universe multiverse" >> sources.list
 echo "deb http://security.ubuntu.com/ubuntu hardy-security main restricted universe" >> sources.list
-echo "deb http://ppa.launchpad.net/ebox-unstable/ppa/ubuntu hardy main" >> sources.list
+echo "deb http://ppa.launchpad.net/ebox/1.2/ubuntu hardy main" >> sources.list
 sudo mv sources.list $CHROOT/etc/apt/sources.list
 
 sudo chroot $CHROOT apt-get update
