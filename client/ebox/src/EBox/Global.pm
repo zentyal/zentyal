3d355ac57dfa4ec1fc520f7f8f86fdb3f7dafe3c
Enrique J. Hernández Blasco <ejhernandez@zentyal.com>
2009-12-15 18:56:04 +0000

    NN:Passing the right parameter to `lastModificationTime` method refs #1597
    git-svn-id: http://svn.zentyal.org/zentyal/trunk@15532 866bc4c1-54da-0310-a958-9d5a9a0e8d04

--- client/ebox/src/EBox/Global.pm
+++ client/ebox/src/EBox/Global.pm
@@ -867,7 +867,11 @@ sub lastModificationTime
     if ( $self->modExists('users') ) {
         my $usersMod = $self->modInstance('users');
         if ( $usersMod->configured() ) {
-            my $ldapStamp = $usersMod->ldap()->lastModificationTime($lastStamp);
+            my ($sec, $min, $hour, $mday, $mon, $year) = localtime($lastStamp);
+            my $lastStampStr = sprintf('%04d%02d%02d%02d%02d%02dZ',
+                                       ($year + 1900, $mon + 1, $mday + 1, $hour,
+                                        $min, $sec));
+            my $ldapStamp = $usersMod->ldap()->lastModificationTime($lastStampStr);
             if ( $ldapStamp > $lastStamp ) {
                 $lastStamp = $ldapStamp;
             }
