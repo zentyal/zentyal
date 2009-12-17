5abdc0d9baafb209fa09a188178a8b894f7d6340
Isaac Clerencia <iclerencia@zentyal.com>
2009-12-17 18:08:05 +0000

    NN: add migration to rename mail to mail_message
    git-svn-id: http://svn.zentyal.org/zentyal/trunk@15592 866bc4c1-54da-0310-a958-9d5a9a0e8d04

---
+++ client/mail/migration/0007_rename_tables.pl
@@ -0,0 +1,32 @@
+#!/usr/bin/perl
+
+#	Migration between gconf data version 6 to 7
+#
+#
+#   This migration script renames the access table to squid_access if it exists
+#
+package EBox::Migration;
+use base 'EBox::Migration::Base';
+
+use strict;
+use warnings;
+
+use EBox;
+use EBox::Config;
+use EBox::Global;
+use EBox::MigrationHelpers;
+
+sub runGConf
+{
+    EBox::MigrationHelpers::renameTable('mail', 'mail_message');
+}
+
+EBox::init();
+
+my $mod = EBox::Global->modInstance('mail');
+my $migration = new EBox::Migration(
+				    'gconfmodule' => $mod,
+				    'version' => 7,
+				   );
+
+$migration->execute();
