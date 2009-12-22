497c973a3932adc98a93ddff1f3c51e7bb61108b
José Antonio Calvo <jacalvo@zentyal.com>
2009-12-22 15:04:55 +0000

    NN: added leela autobuilder sources, remove deprecated test
    git-svn-id: http://svn.zentyal.org/zentyal/trunk@15659 866bc4c1-54da-0310-a958-9d5a9a0e8d04

--- extra/anste/scripts/write-hardy-ebox-sources.sh
+++ extra/anste/scripts/write-hardy-ebox-sources.sh
@@ -8,6 +8,11 @@ then
     echo "deb http://ppa.launchpad.net/ebox/1.3/ubuntu hardy main " >> $SOURCES
 fi
 
+if ! grep -q "leela" $SOURCES
+then
+    echo "deb http://leela/ebox/trunk ./" >> $SOURCES
+fi
+
 if ! grep -q "10.6.7.1" $SOURCES
 then
     echo "deb http://10.6.7.1/ebox ./" >> $SOURCES
