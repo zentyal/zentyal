569984de4980d228a8ea227d7be17d636865a059
José Antonio Calvo <jacalvo@zentyal.com>
2009-12-29 22:36:16 +0000

    NN: merged r15740 from 1.2
    git-svn-id: http://svn.zentyal.org/zentyal/trunk@15741 866bc4c1-54da-0310-a958-9d5a9a0e8d04

--- extra/installer/list-not-installed.sh
+++ extra/installer/list-not-installed.sh
@@ -1,28 +1,55 @@
 #!/bin/sh
 
+# If we do a normal installation, without using LVM, cryptsetup or RAID, we should
+# avoid deleting the following packages
+MANDATORY_PACKAGES="
+lvm
+dmsetup
+devmapper
+crypt
+mdadm
+kpartx
+multipath
+lilo
+xfs
+jfs"
+
 cd /cdrom/pool/main
 
 for j in *
-  do
+do
     cd $j
     for k in *
-      do
+    do
         cd $k
         for l in *.deb
-          do
+        do
             if [ $l != "*.deb" ]
-              then
+            then
                 n=$(dpkg -l $(echo $l | cut -f1 -d"_") 2> /dev/null| grep "^ii")
                 if [ -z "$n" ]
-                  then
+                then
                     pkgfile=/cdrom/pool/main/$j/$k/$l
-                    size=`du $pkgfile`
-                    echo "$size $pkgfile"
+                    skip=0
+                    for i in $MANDATORY_PACKAGES
+                    do
+                        if echo $pkgfile | grep -q $i
+                        then
+                            skip=1
+                            break
+                        fi
+                    done
+                    if [ $skip -eq 0 ]
+                    then
+                        #size=`du $pkgfile`
+                        #echo "$size $pkgfile"
+                        echo $pkgfile
+                    fi
                 fi
             fi
-          done
+        done
         cd ..
-      done
+    done
     cd ..
-  done
+done
 find -depth -type d -empty -exec echo {} \;
