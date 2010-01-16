88241d12bce46df027388108bec8ee7c2d505085
Javier Uruen <juruen@zentyal.com>
2010-01-16 21:55:06 +0000

    NN:   + Add an allow rule by default from internal       networks to internet
    git-svn-id: http://svn.zentyal.org/zentyal/trunk@16038 866bc4c1-54da-0310-a958-9d5a9a0e8d04

--- client/firewall/migration/0004_allow_output_by_default.pl
+++ client/firewall/migration/0004_allow_output_by_default.pl
@@ -1,10 +1,10 @@
 #!/usr/bin/perl
 
-#	Migration between gconf data version 3 to 4
+#   Migration between gconf data version 3 to 4
 #
 #
 #   This migration script just tries to add a rule to allow outcoming
-#   connections by eBox.
+#   connections by eBox and also connections from internal to external.
 #
 #   There's no migration from previous data as we only need to add a rule.
 #
@@ -30,7 +30,18 @@ use Error qw(:try);
 sub runGConf
 {
     my ($self) = @_;
-    my $fw = EBox::Model::ModelManager::instance()->model('EBoxOutputRuleTable');
+
+    $self->_allowOutput();
+    $self->_allowInternal();
+
+}
+
+sub _allowOutput
+{
+    my ($self) = @_;
+
+    my $fw =
+        EBox::Model::ModelManager::instance()->model('EBoxOutputRuleTable');
     my $service = EBox::Model::ModelManager::instance()->model('ServiceTable');
     my $anyRow = $service->findValue(name => 'any');
     unless (defined($anyRow)) {
@@ -39,6 +50,27 @@ sub runGConf
     }
     $fw->add(
             decision => 'accept',
+            destination => { destination_any => undef },
+            description => '',
+            service => $anyRow->id()
+            );
+}
+
+sub _allowInternal
+{
+    my ($self) = @_;
+
+    my $fw =
+        EBox::Model::ModelManager::instance()->model('ToInternetRuleTable');
+    my $service = EBox::Model::ModelManager::instance()->model('ServiceTable');
+    my $anyRow = $service->findValue(name => 'any');
+    unless (defined($anyRow)) {
+        EBox::warn('There must be something broken as I could not find "any" service');
+        return;
+    }
+    $fw->add(
+            decision => 'accept',
+            source => { source_any => undef },
             destination => { destination_any => undef },
             description => '',
             service => $anyRow->id()
