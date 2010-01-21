b9c567440e926dcd0c2d708a0ee1ec918e7dd4d1
Javier Uruen <juruen@zentyal.com>
2010-01-21 09:54:29 +0000

    NN: + Bug fix: do not warn about disabled services which return false from       showModuleStatus()
    git-svn-id: http://svn.zentyal.org/zentyal/trunk@16139 866bc4c1-54da-0310-a958-9d5a9a0e8d04

--- client/ebox/src/EBox/CGI/Finish.pm
+++ client/ebox/src/EBox/CGI/Finish.pm
@@ -121,6 +121,7 @@ sub _disabledModules
         my $modInstance = $global->modInstance($modName);
         next unless ($modInstance->isa('EBox::Module::Service'));
         next if ($modInstance->isEnabled());
+        next if ($modInstance->showModuleStatus());
         push (@modules, $modInstance->printableName());
     }
     return \@modules;
