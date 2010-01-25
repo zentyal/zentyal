bf60f4999d829f46e56103ff64ad2c45de8de9ab
José Antonio Calvo <jacalvo@zentyal.com>
2010-01-25 14:58:34 +0000

    NN: fixed typo
    git-svn-id: http://svn.zentyal.org/zentyal/trunk@16205 866bc4c1-54da-0310-a958-9d5a9a0e8d04

--- client/squid/src/EBox/Squid/Model/ObjectPolicy.pm
+++ client/squid/src/EBox/Squid/Model/ObjectPolicy.pm
@@ -433,13 +433,13 @@ sub precondition
     my ($self) = @_;
     my $objects = EBox::Global->modInstance('objects');
 
-    return @{ $objects->objects() } > 0; 
+    return @{ $objects->objects() } > 0;
 }
 
 sub preconditionFailMsg
 {
     return __x(
-'There are not network objects in the system. {open}Create{close} at least one object  if you want to set a object policy',
+'There are no network objects in the system. {open}Create{close} at least one object if you want to set a object policy',
 open => q{<a href='/ebox/Objects/View/ObjectTable'>},
 close => q{</a>},
 );
