9b1c07c4287de4ab873b03faaf9ee0a15707142d
Enrique J. Hernández Blasco <ejhernandez@zentyal.com>
2010-01-28 13:40:32 +0000

    NN: Fix typo in English msg
    git-svn-id: http://svn.zentyal.org/zentyal/trunk@16339 866bc4c1-54da-0310-a958-9d5a9a0e8d04

--- client/squid/src/EBox/Squid/Model/GlobalGroupPolicy.pm
+++ client/squid/src/EBox/Squid/Model/GlobalGroupPolicy.pm
@@ -91,7 +91,7 @@ sub _table
       rowUnique          => 1,
       automaticRemove    => 1,
       printableRowName   => __("group's policy"),
-      help               => __("Here you can globaly block or allow access by user group. Filter options will be by global policy or by object policy"),
+      help               => __("Here you can globally block or allow access by user group. Filter options will be by global policy or by object policy"),
       messages           => {
           add => __(q{Added group's policy}),
           del =>  __(q{Removed group's policy}),
