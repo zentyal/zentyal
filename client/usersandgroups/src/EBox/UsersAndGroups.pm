3fb4791e3428cd1df99ee6edabf1ccef2b3d96a6
Javier Uruen <juruen@zentyal.com>
2010-02-07 16:02:40 +0000

    BP:  + Bug fix: do not allow \w with localized characters as LDAP schema does not       allow them for home directory attribute. (Closes #1713)
    git-svn-id: http://svn.zentyal.org/zentyal/trunk@16598 866bc4c1-54da-0310-a958-9d5a9a0e8d04

--- client/usersandgroups/src/EBox/UsersAndGroups.pm
+++ client/usersandgroups/src/EBox/UsersAndGroups.pm
@@ -2061,7 +2061,7 @@ sub _checkName
 {
     my ($name) = @_;
 
-    if ($name =~ /^[\w\d\s_-]+\.?[\w\d\s_-]+$/) {
+    if ($name =~ /^[a-zA-Z\d\s_-]+\.?[a-zA-Z\d\s_-]+$/) {
         return 1;
     } else {
         return undef;
