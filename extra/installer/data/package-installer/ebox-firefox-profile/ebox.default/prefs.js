03da74564f23654a38d9ca7057ccd8ba60060eb9
José Antonio Calvo <jacalvo@zentyal.com>
2010-03-12 01:55:12 +0000

    NN: X11 UI tweaks (thanks to Kamilion)
    git-svn-id: http://svn.zentyal.org/zentyal/trunk@17043 866bc4c1-54da-0310-a958-9d5a9a0e8d04

---
+++ extra/installer/data/package-installer/ebox-firefox-profile/ebox.default/prefs.js
@@ -0,0 +1,3 @@
+user_pref("browser.sessionstore.resume_from_crash", false);
+user_pref("browser.startup.homepage", "https://localhost");
+user_pref("browser.startup.homepage_override.mstone", "rv:1.9.0.18");
