55d09eb6e04db8d3ccb4c34b86d9aa1007e08e5e
José Antonio Calvo <jacalvo@zentyal.com>
2010-04-05 00:03:49 +0000

    added lucid support
    git-svn-id: http://svn.zentyal.org/zentyal/trunk@17120 866bc4c1-54da-0310-a958-9d5a9a0e8d04

---
+++ extra/anste/scripts/write-lucid-ebox-sources.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+SOURCES="/etc/apt/sources.list"
+
+
+if ! grep -q "1.5" $SOURCES
+then
+    echo "deb http://ppa.launchpad.net/ebox/1.5/ubuntu hardy main " >> $SOURCES
+fi
+
+if ! grep -q "trunk" $SOURCES
+then
+    echo "deb http://leela/ebox/trunk ./" >> $SOURCES
+fi
+
+if ! grep -q "10.6.7.1" $SOURCES
+then
+    echo "deb http://10.6.7.1/ebox ./" >> $SOURCES
+fi
