24ef0dc6eb97b2381259c8d5d0f955e20d660818
Enrique J. Hernández Blasco <ejhernandez@zentyal.com>
2010-04-15 13:50:55 +0000

    BP: DHCP log works now if the DHCP client sends a host name as suggested
    git-svn-id: http://svn.zentyal.org/zentyal/trunk@17184 866bc4c1-54da-0310-a958-9d5a9a0e8d04

--- client/dhcp/src/EBox/DHCPLogHelper.pm
+++ client/dhcp/src/EBox/DHCPLogHelper.pm
@@ -72,12 +72,12 @@ sub processLine # (file, line, logger)
 
     my $date = $1;
     my ($ip, $mac, $iface, $event);
-    if ($line =~ /^.*DHCPACK on ([\d.]+) to ([\d:a-f]+) via (\w+)/) {
+    if ($line =~ /^.*DHCPACK on ([\d.]+) to ([\d:a-f]+).*?via (\w+)/) {
         $ip = $1;
         $mac = $2;
         $iface =$3;
         $event = 'leased';
-    } elsif ($line =~ /^.*DHCPRELEASE of ([\d.]+) from ([\d:a-f]+) via (\w+)/) {
+    } elsif ($line =~ /^.*DHCPRELEASE of ([\d.]+) from ([\d:a-f]+).*?via (\w+)/) {
         $ip = $1;
         $mac = $2;
         $iface =$3;
