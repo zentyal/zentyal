22bb3c34588f57e5d52ec7b74bd16a8398b95108
José Antonio Calvo <jacalvo@zentyal.com>
2010-06-08 23:44:40 +0000

    NN: don't show firefox tab if there is only one page open
    git-svn-id: http://svn.zentyal.org/zentyal/trunk@17584 866bc4c1-54da-0310-a958-9d5a9a0e8d04

--- extra/installer/data/package-installer/ebox-firefox-profile/ebox.default/prefs.js
+++ extra/installer/data/package-installer/ebox-firefox-profile/ebox.default/prefs.js
@@ -1,3 +1,4 @@
 user_pref("browser.sessionstore.resume_from_crash", false);
 user_pref("browser.startup.homepage", "https://localhost");
 user_pref("browser.startup.homepage_override.mstone", "rv:1.9.0.18");
+user_pref("browser.tabs.autoHide", true);
