3188dc0de41840efcb6b9b9cdf9dddb433255280
José Antonio Calvo <jacalvo@zentyal.com>
2010-07-16 21:41:05 +0000

    NN: unified ebox-installer and prepare-ebox-install.sh as the first no longer makes sense
    git-svn-id: http://svn.zentyal.org/zentyal/trunk@18031 866bc4c1-54da-0310-a958-9d5a9a0e8d04

--- extra/installer/data/prepare-ebox-install.sh
+++ extra/installer/data/prepare-ebox-install.sh
@@ -1,22 +1,62 @@
 #!/bin/bash
 
-INSTALLER=ebox-installer
+export LOG=/tmp/ebox-installer.log
+SOURCES_LIST=/etc/apt/sources.list
+PPA_URL="http://ppa.launchpad.net/ebox/1.5/ubuntu"
+EBOX_SOURCES="deb $PPA_URL lucid main"
+PKG_DIR=/var/tmp/ebox-packages
+LOCAL_SOURCES="deb file:$PKG_DIR ./"
+
+create_repository() {
+    cd $PKG_DIR
+    apt-ftparchive packages . | gzip > Packages.gz 2>>$LOG
+    cd - > /dev/null
+    # Update the package database with only the local repository
+    # just in case we are installing without internet connection
+    mv ${SOURCES_LIST} /tmp/ebox
+    echo ${LOCAL_SOURCES} > ${SOURCES_LIST}
+    apt-get update >> $LOG 2>&1
+    # Restore the original sources.list
+    mv /tmp/ebox/sources.list ${SOURCES_LIST}
+    # Move packages to the cache
+    mv $PKG_DIR/*.deb /var/cache/apt/archives/
+}
+
+update_if_network() {
+    # Import PPA key to avoid warning
+    apt-key add /var/tmp/ebox-ppa.asc >> $LOG 2>&1
+    # Check if we can connect to the PPA url
+    if $(wget -T 10 -t 1 $PPA_URL >> $LOG 2>&1); then
+        echo "Updating package database from the network..." >> $LOG
+        apt-get update >> $LOG 2>&1
+    else
+        echo "Warning: Can't connect to $PPA_URL. Updates won't be installed." >> $LOG
+    fi
+}
+
+gen_locales() {
+    # load LANG variable with default locale
+    . /etc/default/locale
+
+    # Append eBox support languages to generate to current supported
+    # locales
+    LOCALES_FILE=/var/lib/locales/supported.d/local
+    TMP=/tmp/local.tmp
+    cat /tmp/ebox/locale.gen $LOCALES_FILE > $TMP
+    sort $TMP | uniq > $LOCALES_FILE
+    rm -f $TMP
+
+    # Regenerate locales to update the new messages from eBox
+    /usr/sbin/locale-gen
+
+    /usr/share/ebox/ebox-set-locale $LANG > /dev/null 2>&1
+}
+
 
 # replace motd
-cp /tmp/motd /etc/motd.tail
-
-# put in place the installer
-cp /etc/rc.local /var/tmp/
-cp /tmp/$INSTALLER /etc/rc.local
-
-# copy locale.gen
-cp /tmp/locale.gen /var/tmp/
+cp /tmp/ebox/motd /etc/motd.tail
 # copy .mo files
-cp -r /tmp/locale /usr/share/
-
-# copy installer files
-cp -r /tmp/package-installer/* /var/tmp/
-cp /tmp/ebox-installer.auto /var/tmp
+cp -r /tmp/ebox/locale /usr/share/
 
 # copy *.deb files from CD to hard disk
 PKG_DIR=/var/tmp/ebox-packages
@@ -30,7 +70,18 @@ do
     cp /cdrom/pool/extras/${p}_*.deb $PKG_DIR 2> /dev/null
 done
 
-/var/tmp/ebox-installer
+create_repository # Set up local package repository
+
+echo ${LOCAL_SOURCES} >> ${SOURCES_LIST} # add local sources
+echo ${EBOX_SOURCES} >> ${SOURCES_LIST} # add ppa sources
+
+update_if_network # apt-get update if we are connected to the internet
+
+gen_locales
+
+/tmp/ebox/ebox-x11-setup
+
+rm -rf /tmp/ebox
 
 sync
 
