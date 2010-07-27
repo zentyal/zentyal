93357160da9ec6df0aa4ab33cb6ee6796d4edf06
Jaime Soriano <jsoriano@zentyal.com>
2010-07-27 11:59:00 +0000

    NN: anste configuration scripts for lucid
    git-svn-id: http://svn.zentyal.org/zentyal/trunk@18174 866bc4c1-54da-0310-a958-9d5a9a0e8d04

--- extra/anste/scripts/write-lucid-ebox-sources.sh
+++ extra/anste/scripts/write-lucid-ebox-sources.sh
@@ -5,7 +5,7 @@ SOURCES="/etc/apt/sources.list"
 
 if ! grep -q "1.5" $SOURCES
 then
-    echo "deb http://ppa.launchpad.net/ebox/1.5/ubuntu hardy main " >> $SOURCES
+    echo "deb http://ppa.launchpad.net/ebox/1.5/ubuntu lucid main " >> $SOURCES
 fi
 
 if ! grep -q "trunk" $SOURCES
@@ -16,4 +16,9 @@ fi
 if ! grep -q "10.6.7.1" $SOURCES
 then
     echo "deb http://10.6.7.1/ebox ./" >> $SOURCES
+fi
+
+if ! grep -q "multiverse" $SOURCES
+then
+    echo "deb http://archive.ubuntu.com/ubuntu/ lucid multiverse" >> $SOURCES
 fi
