d80dd81c03c5aa35c82a0c26f7af0bab070832b8
Javier Amor García <jamor@zentyal.com>
2010-07-29 12:52:10 +0000

    BP: (added forgoten copyright) bugfix soruce column in event db logs tables is wider to accomodate all posssible sources names
    git-svn-id: http://svn.zentyal.org/zentyal/trunk@18225 866bc4c1-54da-0310-a958-9d5a9a0e8d04

--- client/ebox/migration/events/0001_migrate_logs_db_fields.pl
+++ client/ebox/migration/events/0001_migrate_logs_db_fields.pl
@@ -1,4 +1,22 @@
 #!/usr/bin/perl
+# Copyright (C) 2010 eBox Technologies S.L.
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License, version 2, as
+# published by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+#  Migration between gconf data version 0 and 1
+#
+#   db changes: source field is wider to accomadate longer source names
 
 
 package EBox::Migration;
