1e27ea98753e234563a463dab07f5dbabc0c62d5
José Antonio Calvo <jacalvo@zentyal.com>
2010-10-01 01:33:26 +0000

    NN: update from vs2005 to vs2008
    git-svn-id: http://svn.zentyal.org/zentyal/trunk@18990 866bc4c1-54da-0310-a958-9d5a9a0e8d04

--- extra/ebox-ad-passwd-sync-win/ebox_adsync_config/StdAfx.h
+++ extra/ebox-ad-passwd-sync-win/ebox_adsync_config/StdAfx.h
@@ -1,31 +1,45 @@
 // stdafx.h : include file for standard system include files,
-//  or project specific include files that are used frequently, but
-//      are changed infrequently
-//
-#ifndef _WIN32_WINNT	// Permit use of features specific to Windows NT 4.0 or later.
-#define _WIN32_WINNT 0x0400	// Change this to the appropriate value to target
-#endif		         // Windows 98 and Windows 2000 or later.
+// or project specific include files that are used frequently,
+// but are changed infrequently
 
-
-#if !defined(AFX_STDAFX_H__17D034B1_4682_4AD6_ACAB_7C364AA64832__INCLUDED_)
-#define AFX_STDAFX_H__17D034B1_4682_4AD6_ACAB_7C364AA64832__INCLUDED_
-
-#if _MSC_VER > 1000
 #pragma once
-#endif // _MSC_VER > 1000
 
-#define VC_EXTRALEAN		// Exclude rarely-used stuff from Windows headers
+#ifndef _SECURE_ATL
+#define _SECURE_ATL 1
+#endif
+
+#ifndef VC_EXTRALEAN
+#define VC_EXTRALEAN            // Exclude rarely-used stuff from Windows headers
+#endif
+
+#include "targetver.h"
+
+#define _ATL_CSTRING_EXPLICIT_CONSTRUCTORS      // some CString constructors will be explicit
+
+// turns off MFC's hiding of some common and often safely ignored warning messages
+#define _AFX_ALL_WARNINGS
 
 #include <afxwin.h>         // MFC core and standard components
 #include <afxext.h>         // MFC extensions
-#include <afxdisp.h>        // MFC Automation classes
-#include <afxdtctl.h>		// MFC support for Internet Explorer 4 Common Controls
+
+
+
+
+
+#ifndef _AFX_NO_OLE_SUPPORT
+#include <afxdtctl.h>           // MFC support for Internet Explorer 4 Common Controls
+#endif
 #ifndef _AFX_NO_AFXCMN_SUPPORT
-#include <afxcmn.h>			// MFC support for Windows Common Controls
+#include <afxcmn.h>                     // MFC support for Windows Common Controls
 #endif // _AFX_NO_AFXCMN_SUPPORT
 
 
-//{{AFX_INSERT_LOCATION}}
-// Microsoft Visual C++ will insert additional declarations immediately before the previous line.
 
-#endif // !defined(AFX_STDAFX_H__17D034B1_4682_4AD6_ACAB_7C364AA64832__INCLUDED_)
+
+
+
+
+
+
+
+
