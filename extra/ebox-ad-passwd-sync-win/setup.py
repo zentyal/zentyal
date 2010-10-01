0f306b4da0fe48c6927af13d091d89ebf25f5c81
José Antonio Calvo <jacalvo@zentyal.com>
2010-10-01 16:52:13 +0000

    NN: adding new python program to the installer
    git-svn-id: http://svn.zentyal.org/zentyal/trunk@18997 866bc4c1-54da-0310-a958-9d5a9a0e8d04

--- extra/ebox-ad-passwd-sync-win/setup.py
+++ extra/ebox-ad-passwd-sync-win/setup.py
@@ -1,4 +1,4 @@
 from distutils.core import setup
 import py2exe
 
-setup(console=['ebox-pwdsync-hook','ebox-pwdsync-service'])
+setup(console=['ebox-pwdsync-hook','ebox-pwdsync-service','zentyal-enable-hook'])
