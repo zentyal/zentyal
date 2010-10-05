7ab1361071875bec119bea319dad5d9398d5de65
José Antonio Calvo <jacalvo@zentyal.com>
2010-10-05 23:03:13 +0000

    NN: fixed indentation and style
    git-svn-id: http://svn.zentyal.org/zentyal/trunk@19034 866bc4c1-54da-0310-a958-9d5a9a0e8d04

--- client/samba/src/EBox/CGI/ActivePrinting.pm
+++ client/samba/src/EBox/CGI/ActivePrinting.pm
@@ -27,55 +27,53 @@ use EBox::Gettext;
 use EBox::Exceptions::External;
 
 sub new {
-        my $class = shift;
-        my $self = $class->SUPER::new( @_);
-	$self->{domain} = "ebox-samba";
+    my $class = shift;
+    my $self = $class->SUPER::new( @_);
+    $self->{domain} = "ebox-samba";
 
-        bless($self, $class);
-        return $self;
+    bless($self, $class);
+    return $self;
 }
 
-sub _process($) {
+sub _process
+{
+    my ($self) = @_;
 
-        my $self = shift;
+    my ($user, $group);
+    if ($self->param('user')) {
+        $self->_requireParam('user', __('user name'));
+        $user = $self->param('user');
 
-	my ($user, $group);
- 	if ($self->param('user')) {
-		$self->_requireParam('user', __('user name'));
-         	$user = $self->param('user');
+    } else {
+        $self->_requireParam('group', __('group name'));
+        $group = $self->param('group');
+    }
 
+    my $samba = EBox::Global->modInstance('samba');
+    my @newconf;
+
+    for my $printer (@{$samba->printers()}) {
+        if (not $self->param('allbox') ) {
+            push (@newconf, {
+                    'name' => $printer,
+                    'allowed' => $self->param($printer) ? 1 : undef
+                    });
         } else {
-		$self->_requireParam('group', __('group name'));
-         	$group = $self->param('group');
+            # If all selected, done!
+            push (@newconf, {
+                    'name'    => $printer,
+                    'allowed' => 1
+                    });
         }
+    }
 
-
-	my $samba = EBox::Global->modInstance('samba');
-	my @newconf;
-
-	for my $printer (@{$samba->printers()}) {
-	  if (not $self->param('allbox') ) {
-	    push (@newconf, {
-			     'name' => $printer,
-			     'allowed' => $self->param($printer) ? 1 : undef
-			    });
-	  } else {
-	    # If all selected, done!
-	    push (@newconf, {
-			     'name'    => $printer,
-			     'allowed' => 1
-			    });
-	  }
-	}
-
-	if ($user) {
-		$samba->setPrintersForUser($user, \@newconf);
-        	$self->{redirect} = "UsersAndGroups/User?username=$user";
-	} else {
-		$samba->setPrintersForGroup($group, \@newconf);
-        	$self->{redirect} = "UsersAndGroups/Group?group=$group";
-	}
-
+    if ($user) {
+        $samba->setPrintersForUser($user, \@newconf);
+        $self->{redirect} = "UsersAndGroups/User?username=$user";
+    } else {
+        $samba->setPrintersForGroup($group, \@newconf);
+        $self->{redirect} = "UsersAndGroups/Group?group=$group";
+    }
 }
 
 1;
