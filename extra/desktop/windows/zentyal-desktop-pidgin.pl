c68e314a163af0cb139ab8c3de3c9e8dce57930c
José Antonio Calvo <jacalvo@zentyal.com>
2010-10-26 17:24:42 +0000

    NN: fixed style and added changelog
    git-svn-id: http://svn.zentyal.org/zentyal/trunk@19183 866bc4c1-54da-0310-a958-9d5a9a0e8d04

--- extra/desktop/windows/zentyal-desktop-pidgin.pl
+++ extra/desktop/windows/zentyal-desktop-pidgin.pl
@@ -1,32 +1,56 @@
 #!perl
+#
+# Copyright (C) 2010 eBox Technologies S.L.
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License, version 2, as
+# published by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
 use strict;
 use warnings;
+
 use Win32::TieRegistry(Delimiter=>"#", ArrayValues=>0);
 
-$Registry->Delimiter("/");      
-my $APPDATA= $Registry->{"CUser/Volatile Environment/APPDATA"} 
+$Registry->Delimiter("/");
+my $APPDATA= $Registry->{"CUser/Volatile Environment/APPDATA"}
     or die "Error: $^E\n";
 
 my $USER = $ENV{USERNAME};
-my $SERVER = 'zentyal.com';
+my $SERVER = '192.168.1.135'; # FIXME: unhardcode this
 
-open (T_ACCOUNT, '<.\templates\pidgin\accounts.xml')
+open (my $T_ACCOUNT, '<.\templates\pidgin\accounts.xml')
     or die "Error: $^E\n";
-my $account = join ("",<T_ACCOUNT>);
+
+my $account = join ('', <$T_ACCOUNT>);
+
 $account =~ s/USERNAME/$USER/g;
-$account =~ s/EBOXDOMAIN/zentyal/g;
-$account =~ s/EBOXSERVER/$SERVER/g;
+$account =~ s/ZENTYALDOMAIN/zentyal/g;
+$account =~ s/ZENTYALSERVER/$SERVER/g;
 
-open (T_BLIST, '<.\templates\pidgin\blist.xml')
+open (my $T_BLIST, '<./templates/pidgin/blist.xml')
     or die "Error: $^E\n";
-my $blist = join ("", <T_BLIST>);
+
+my $blist = join ('', <$T_BLIST>);
 $blist =~ s/USERNAME/$USER/g;
-$blist =~ s/EBOXDOMAIN/ebox/g;
+$blist =~ s/ZENTYALDOMAIN/ebox/g;
 
 print $account;
 
-open (ACCOUNT, '>'. $APPDATA . '\.purple\accounts.xml') or die "Error: $^E\n";
-print ACCOUNT $account;
+open (my $ACCOUNT, ">$APPDATA/.purple/accounts.xml") or
+    die "Error: $^E\n";
 
-open (BLIST, '>' .$APPDATA . '\.purple\blist.xml') or die "Error: $^E\n";
-print BLIST $blist;
+print $ACCOUNT $account;
+
+open (my $BLIST, ">$APPDATA/.purple/blist.xml") or
+    die "Error: $^E\n";
+
+print $BLIST $blist;
