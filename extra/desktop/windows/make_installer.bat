ffd9b716217bcebe8d6dacc9a8c3e3c6c5ba61ef
José Antonio Calvo <jacalvo@zentyal.com>
2010-10-29 15:33:45 +0000

    NN: fixed typo
    git-svn-id: http://svn.zentyal.org/zentyal/trunk@19218 866bc4c1-54da-0310-a958-9d5a9a0e8d04

--- extra/desktop/windows/make_installer.bat
+++ extra/desktop/windows/make_installer.bat
@@ -1,6 +1,6 @@
 cd res
 "c:\Archivos de programa\NSIS\makensis.exe" config.nsi
-"c:\Archivos de programa\NSIS\makensis.exe" install.nsii
+"c:\Archivos de programa\NSIS\makensis.exe" install.nsi
 move Config.exe ../bin/
 move Install.exe ../bin/
 cd ..
