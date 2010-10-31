e173148fea3eb29f86440b689e994d2acba5029e
José Antonio Calvo <jacalvo@zentyal.com>
2010-10-31 00:16:47 +0000

    NN: repalce downloads.txt with a script and update build doc
    git-svn-id: http://svn.zentyal.org/zentyal/trunk@19256 866bc4c1-54da-0310-a958-9d5a9a0e8d04

---
+++ extra/desktop/windows/res/download_extras.bat
@@ -0,0 +1,9 @@
+set WGET="%programfiles%\GnuWin32\Bin\wget.exe"
+
+%WGET% "http://ekiga.net/yannick/win32/ekiga-setup-3.2.7.exe" -O ekiga-setup.exe
+
+%WGET% "http://download.mozilla.org/?product=firefox-3.6.12&os=win&lang=en-GB" -O firefox-setup.exe
+
+%WGET% "http://download.mozilla.org/?product=thunderbird-3.1.6&os=win&lang=en-GB" -O thunderbird-setup.exe
+
+%WGET% "http://downloads.sourceforge.net/project/pidgin/Pidgin/2.7.4/pidgin-2.7.4.exe" -O pidgin-setup.exe
