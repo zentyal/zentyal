4df4d44045032d819ad25f7a6b952dc663f1a465
José Antonio Calvo <jacalvo@zentyal.com>
2010-10-31 02:34:32 +0000

    NN: include dynamic-load modules that are not automatically included
    git-svn-id: http://svn.zentyal.org/zentyal/trunk@19261 866bc4c1-54da-0310-a958-9d5a9a0e8d04

--- extra/desktop/windows/make_installer.bat
+++ extra/desktop/windows/make_installer.bat
@@ -2,25 +2,25 @@
 
 set MAKENSIS_EXE="%programfiles%\NSIS\makensis.exe"
 set LIBPERL=C:\strawberry\perl\lib\auto
+set EXTRAMODULES=ZentyalDesktop/Services/UserCorner.pm ZentyalDesktop/Services/Zarafa.pm ZentyalDesktop/Services/VoIP.pm ZentyalDesktop/Services/Samba.pm ZentyalDesktop/Services/Mail.pm ZentyalDesktop/Services/Jabber.pm
 
-mkdir build
-mkdir dist
+mkdir build dist
 
 copy zentyal-setup-user.pl build
 xcopy /S /I /Y ZentyalDesktop build\ZentyalDesktop
-xcopy /S /I /Y ..\common\ZentyalDesktop build\ZentyalDesktop
+xcopy /S /I /Y ..\common\ZentyalDesktop\*.* build\ZentyalDesktop
 
 cd build
 
 :: Comment this and uncomment the following to build in release mode
-call pp -o zentyal-setup-user.exe zentyal-setup-user.pl --link %LIBPERL%\Socket\Socket.dll
+call pp -o zentyal-setup-user.exe zentyal-setup-user.pl --link %LIBPERL%\Socket\Socket.dll -M %EXTRAMODULES%
 :: call pp --gui --icon ..\res\zentyal.ico -o zentyal-setup-user.exe zentyal-setup-user.pl --link %LIBPERL%\Socket\Socket.dll
 
 move zentyal-setup-user.exe ..\dist
 cd ..
 
 xcopy /S /I /Y templates dist\templates
-xcopy /S /I /Y ..\common\templates dist\templates
+xcopy /S /I /Y ..\common\templates\*.* dist\templates
 
 xcopy /S /I /Y res dist\res
 copy LICENSE.txt dist
