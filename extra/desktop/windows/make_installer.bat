99fcc7bb51372d62e642ee150aa480129a334f1b
Javier Alcubierre <jalcubierre@zentyal.com>
2010-11-09 23:06:31 +0000

    NN: Corrected error in make_installer.bat
    git-svn-id: http://svn.zentyal.org/zentyal/trunk@19380 866bc4c1-54da-0310-a958-9d5a9a0e8d04

--- extra/desktop/windows/make_installer.bat
+++ extra/desktop/windows/make_installer.bat
@@ -15,8 +15,8 @@ xcopy /S /I /Y ZentyalDesktop build\ZentyalDesktop
 cd build
 
 :: The two following lines are for debug and release build, uncomment only one of them
-call pp -o zentyal-setup-user.exe zentyal-setup-user.pl --link %LIBPERL%\Socket\Socket.dll 
-::call pp --gui --icon ..\res\zentyal.ico -o zentyal-setup-user.exe zentyal-setup-user.pl --link %LIBPERL%\Socket\Socket.dll -M %EXTRAMODULES%
+call pp -o zentyal-setup-user.exe zentyal-setup-user.pl --link %LIBPERL%\Socket\Socket.dll
+::call pp --gui --icon ..\res\zentyal.ico -o zentyal-setup-user.exe zentyal-setup-user.pl --link %LIBPERL%\Socket\Socket.dll
 
 move zentyal-setup-user.exe ..\dist
 cd ..
