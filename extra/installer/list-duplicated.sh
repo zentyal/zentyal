06115ca5f101690e89bdfbfcb079296f445fb9da
José Antonio Calvo <jacalvo@zentyal.com>
2010-12-20 16:40:15 +0000

    NN: use different files per arch in list-duplicated, added snort preseed
    git-svn-id: http://svn.zentyal.org/zentyal/trunk@19882 866bc4c1-54da-0310-a958-9d5a9a0e8d04

--- extra/installer/list-duplicated.sh
+++ extra/installer/list-duplicated.sh
@@ -15,52 +15,52 @@ cd $CD_IMAGE
 
 for dir in main extras
 do
-	echo -n > $BASE/${dir}_WITH_VERSIONS
-	echo -n > $BASE/${dir}_WITHOUT_VERSIONS
-	echo -n > $BASE/REMOVE_${dir}
+	echo -n > $BASE/${dir}_WITH_VERSIONS_$ARCH
+	echo -n > $BASE/${dir}_WITHOUT_VERSIONS_$ARCH
+	echo -n > $BASE/REMOVE_${dir}_$ARCH
 
 	for i in `find $dir -name "*.deb"`
 	do
 		NAME=`echo $i | sed 's/.*\///g' | cut -d'_' -f1`
 		VERSION=`dpkg-deb --info $i | grep ^" Version:" | cut -d' ' -f3`
-		echo "$NAME $VERSION" >> $BASE/${dir}_WITH_VERSIONS
-		echo $NAME >> $BASE/${dir}_WITHOUT_VERSIONS
+		echo "$NAME $VERSION" >> $BASE/${dir}_WITH_VERSIONS_$ARCH
+		echo $NAME >> $BASE/${dir}_WITHOUT_VERSIONS_$ARCH
 	done
 done
 
 cd -
 
-cat main_WITHOUT_VERSIONS extras_WITHOUT_VERSIONS | sort | uniq -c | cut -c7- | grep -v ^1 | cut -d' ' -f2 > DUPLICATED_PACKAGES
+cat main_WITHOUT_VERSIONS_$ARCH extras_WITHOUT_VERSIONS_$ARCH | sort | uniq -c | cut -c7- | grep -v ^1 | cut -d' ' -f2 > DUPLICATED_PACKAGES_$ARCH
 
-for i in `cat DUPLICATED_PACKAGES`
+for i in `cat DUPLICATED_PACKAGES_$ARCH`
 do
-	MAIN_VERSION=`grep $i main_WITH_VERSIONS | cut -d' ' -f2| head -1`
-	EXTRA_VERSION=`grep $i extras_WITH_VERSIONS | cut -d' ' -f2 | head -1`
+	MAIN_VERSION=`grep $i main_WITH_VERSIONS_$ARCH | cut -d' ' -f2| head -1`
+	EXTRA_VERSION=`grep $i extras_WITH_VERSIONS_$ARCH | cut -d' ' -f2 | head -1`
 	# FIXME: Probably there is a bug here (false positive with samba)
 	if [ $EXTRA_VERSION \> $MAIN_VERSION ]
 	then
 		# extras version newer than main one but may be unsafe to remove
         # the main one if it is a package belonging to the base system
-		echo $i >> REMOVE_main_unsafe
+		echo $i >> REMOVE_main_unsafe_$ARCH
 	else
-		echo $i >> REMOVE_extras
+		echo $i >> REMOVE_extras_$ARCH
 	fi
 done
 
 # be careful with duplicated packages in extras
 # the older version has to be removed manually by now
-ls extras-$ARCH | cut -d'_' -f1 | uniq -c | grep -v "    1" | cut -d' ' -f8- > NO_REMOVE
+ls extras-$ARCH | cut -d'_' -f1 | uniq -c | grep -v "    1" | cut -d' ' -f8- > NO_REMOVE_$ARCH
 
-cp REMOVE_extras FINAL_REMOVE
+cp REMOVE_extras_$ARCH FINAL_REMOVE_$ARCH
 
-for i in `cat NO_REMOVE`
+for i in `cat NO_REMOVE_$ARCH`
 do
-    sed -i "/^$i$/d" FINAL_REMOVE
+    sed -i "/^$i$/d" FINAL_REMOVE_$ARCH
 done
 
 for dir in main extras
 do
-	rm ${dir}_WITH_VERSIONS
-	rm ${dir}_WITHOUT_VERSIONS
+	rm ${dir}_WITH_VERSIONS_$ARCH
+	rm ${dir}_WITHOUT_VERSIONS_$ARCH
 done
-rm DUPLICATED_PACKAGES
+rm DUPLICATED_PACKAGES_$ARCH
