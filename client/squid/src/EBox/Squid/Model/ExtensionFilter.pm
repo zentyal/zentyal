7ba44278695704cbc2043a8db83b3040afaa1d8e
José Antonio Calvo <jacalvo@zentyal.com>
2011-01-11 14:27:23 +0000

    NN: fixed indentation
    git-svn-id: http://svn.zentyal.org/zentyal/trunk@20114 866bc4c1-54da-0310-a958-9d5a9a0e8d04

--- client/squid/src/EBox/Squid/Model/ExtensionFilter.pm
+++ client/squid/src/EBox/Squid/Model/ExtensionFilter.pm
@@ -43,34 +43,30 @@ use EBox::Types::Text;
 #       created model
 #
 sub new
-  {
+{
+    my $class = shift;
 
-      my $class = shift;
+    my $self = $class->SUPER::new(@_);
 
-      my $self = $class->SUPER::new(@_);
-
-      bless $self, $class;
-      return $self;
-
-  }
+    bless $self, $class;
+    return $self;
+}
 
 sub validateTypedRow
 {
-  my ($self, $action, $params_r) = @_;
+    my ($self, $action, $params_r) = @_;
 
-  if (exists $params_r->{extension} ) {
-    my $extension = $params_r->{extension}->value();
-    if ($extension =~ m{\.}) {
-        throw EBox::Exceptions::InvalidData(
-                data  => __('File extension'),
-                value => $extension,
-                advice => ('Dots (".") are not allowed in file extensions')
-                )
+    if (exists $params_r->{extension} ) {
+        my $extension = $params_r->{extension}->value();
+        if ($extension =~ m{\.}) {
+            throw EBox::Exceptions::InvalidData(
+                    data  => __('File extension'),
+                    value => $extension,
+                    advice => ('Dots (".") are not allowed in file extensions')
+                    )
+        }
     }
-  }
-
 }
-
 
 # Function: bannedExtensions
 #
@@ -78,20 +74,20 @@ sub validateTypedRow
 #
 # Returns:
 #
-# 	Array ref - containing the extensions
+#	Array ref - containing the extensions
 sub banned
 {
-  my ($self) = @_;
+    my ($self) = @_;
 
-  my @bannedExtensions;
+    my @bannedExtensions;
 
-  for my $id (@{$self->ids()}) {
-    my $row = $self->row($id);
-    if (not $row->valueByName('allowed')) {
-        push (@bannedExtensions, $row->valueByName('extension'));
+    for my $id (@{$self->ids()}) {
+        my $row = $self->row($id);
+        if (not $row->valueByName('allowed')) {
+            push (@bannedExtensions, $row->valueByName('extension'));
+        }
     }
-  }
-  return \@bannedExtensions;
+    return \@bannedExtensions;
 }
 
 # Group: Protected methods
@@ -102,36 +98,26 @@ sub _table
     my ($self) = @_;
     my $warnMsg = q{The extension filter needs a 'filter' policy to take effect};
 
-
-
-  my $dataTable =
+    my $dataTable =
     {
-     tableName          => 'ExtensionFilter',
-     printableTableName => __('Configure allowed file extensions'),
-     modelDomain        => 'Squid',
-     defaultController  => '/ebox/Squid/Controller/ExtensionFilter',
-     defaultActions     =>
-     [
-      'add', 'del',
-      'editField',
-      'changeView'
-     ],
-     tableDescription   => $self->_tableHeader(),
-     class              => 'dataTable',
-     order              => 0,
-     rowUnique          => 1,
-     printableRowName   => __('extension'),
-     help               => __("Allow/Deny the HTTP traffic of the files which the given extensions.\nExtensions not listed here are allowed.\nThe extension filter needs a 'filter' policy to be in effect"),
-
-     messages           => {
-			    add    => __('Extension added'),
-			    del    => __('Extension removed'),
-			    update => __('Extension updated'),
-			   },
-     sortedBy           => 'extension',
+        tableName          => 'ExtensionFilter',
+        printableTableName => __('Configure allowed file extensions'),
+        modelDomain        => 'Squid',
+        defaultController  => '/ebox/Squid/Controller/ExtensionFilter',
+        defaultActions     => [ 'add', 'del', 'editField', 'changeView' ],
+        tableDescription   => $self->_tableHeader(),
+        class              => 'dataTable',
+        order              => 0,
+        rowUnique          => 1,
+        printableRowName   => __('extension'),
+        help               => __("Allow/Deny the HTTP traffic of the files which the given extensions.\nExtensions not listed here are allowed.\nThe extension filter needs a 'filter' policy to be in effect"),
+        messages           => {
+            add    => __('Extension added'),
+            del    => __('Extension removed'),
+            update => __('Extension updated'),
+        },
+        sortedBy           => 'extension',
     };
-
 }
 
 1;
-
