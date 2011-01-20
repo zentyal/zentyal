2ae032a7796c30e58415efaaa954802a5070b448
José Antonio Calvo <jacalvo@zentyal.com>
2011-01-20 13:13:44 +0000

    NN: merge r20245 from branches/2.0
    git-svn-id: http://svn.zentyal.org/zentyal/trunk@20247 866bc4c1-54da-0310-a958-9d5a9a0e8d04

--- main/software/src/EBox/Software.pm
+++ main/software/src/EBox/Software.pm
@@ -720,24 +720,26 @@ sub _getUpgradablePkgs
     my @list;
     for my $pack (keys %$cache) {
         my $pkgCache = $cache->packages()->lookup($pack) or next;
-        my %data;
 
         my $currentVerObj = $cache->{$pack}{CurrentVer};
 
         if ($currentVerObj) {
             if ($currentVerObj->{VerStr} eq $pkgCache->{VerStr}) {
+                # Nothing new available
                 next;
             }
         } else {
             next;
         }
 
+        my %data;
         $data{'name'} = $pkgCache->{Name};
         $data{'description'} = $pkgCache->{ShortDesc};
         my $candidateVerInfo = $self->_candidateVersion($cache->{$pack});
         $data{'security'}    = $candidateVerInfo->{security};
         $data{'ebox-qa'}     = $candidateVerInfo->{qa};
         $data{'version'}     = $candidateVerInfo->{version};
+        next if ($data{'version'} eq $currentVerObj->{VerStr});
 
         push(@list, \%data);
     }
