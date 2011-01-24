7d1be944933044218460245873f813a0ff1809a6
José Antonio Calvo <jacalvo@zentyal.com>
2011-01-24 00:37:06 +0000

    NN: removed methods that are now part of firewall and rename redisvi in Makefile.am
    git-svn-id: http://svn.zentyal.org/zentyal/trunk@20304 866bc4c1-54da-0310-a958-9d5a9a0e8d04

--- main/core/src/EBox/Migration/Base.pm
+++ main/core/src/EBox/Migration/Base.pm
@@ -37,7 +37,6 @@ sub _checkCurrentGConfVersion
 
 	my $currentVer = $self->{'gconfmodule'}->get_int("data_version");
 
-
 	if (not defined($currentVer)) {
 		$currentVer = 0;
 	}
@@ -94,87 +93,6 @@ sub execute
 sub runGConf
 {
 
-}
-
-
-# Method: addInternalService
-#
-#  Helper method to add new internal services to the service module and related
-#  firewall rules
-#
-#
-#  Named Parameters:
-#    name - name of the service
-#    protocol - protocol used by the service
-#    sourcePort - source port used by the service (default : any)
-#    destinationPort - destination port used by the service (default : any)
-#    target - target for the firewall rule (default: allow)
-sub addInternalService
-{
-    my ($self, %params) = @_;
-    exists $params{name} or
-        throw EBox::Exceptions::MissingArgument('name');
-
-    $self->_addService(%params);
-
-    my @fwRuleParams = ($params{name});
-    push @fwRuleParams, $params{target} if exists $params{target};
-    $self->fwRuleForInternalService(@fwRuleParams);
-}
-
-sub fwRuleForInternalService
-{
-    my ($self, $service, $target) = @_;
-    $service or
-        throw EBox::Exceptions::MissingArgument('service');
-    $target or
-        $target = 'accept';
-
-    my $fw = EBox::Global->modInstance('firewall');
-    $fw->setInternalService($service, $target);
-    $fw->saveConfigRecursive();
-}
-
-sub _addService
-{
-    my ($self, %params) = @_;
-    exists $params{name} or
-        throw EBox::Exceptions::MissingArgument('name');
-    exists $params{protocol} or
-        throw EBox::Exceptions::MissingArgument('protocol');
-    exists $params{protocol} or
-        throw EBox::Exceptions::MissingArgument('translationDomain');
-    exists $params{sourcePort} or
-        $params{sourcePort} = 'any';
-    exists $params{destinationPort} or
-        $params{destinationPort} = 'any';
-
-    my $serviceMod = EBox::Global->modInstance('services');
-
-    if (not $serviceMod->serviceExists('name' => $params{name})) {
-        $serviceMod->addService('name' => $params{name},
-                'protocol' => $params{protocol},
-                'sourcePort' => $params{sourcePort},
-                'destinationPort' => $params{destinationPort},
-                'translationDomain' => $params{translationDomain},
-                'internal' => 1,
-                'readOnly' => 1
-                );
-
-    } else {
-        $serviceMod->setService('name' => $params{name},
-                'protocol' => $params{protocol},
-                'sourcePort' => $params{sourcePort},
-                'destinationPort' => $params{destinationPort},
-                'translationDomain' => $params{translationDomain},
-                'internal' => 1,
-                'readOnly' => 1);
-
-        EBox::info(
-            "Not adding $params{name} service as it already exists instead");
-    }
-
-    $serviceMod->saveConfig();
 }
 
 1;
