f5a59760c7f29761ec2d3dcf5f9dd53852e7d13a
Enrique J. Hernández Blasco <ejhernandez@zentyal.com>
2011-02-01 14:32:21 +0000

    NN: Add migration script to delete old backup configuration from ebox crontab
    git-svn-id: http://svn.zentyal.org/zentyal/branches/2.0@20412 866bc4c1-54da-0310-a958-9d5a9a0e8d04

---
+++ client/ebackup/migration/0003_delete_ebox_crontab.pl
@@ -0,0 +1,49 @@
+#!/usr/bin/perl
+
+# Copyright (C) 2011 eBox Technologies S.L.
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License, version 2, as
+# published by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+#
+#   This migration script removes any content from ebox crontab
+#
+
+package EBox::Migration;
+use base 'EBox::Migration::Base';
+
+use strict;
+use warnings;
+
+use EBox;
+use EBox::Global;
+use EBox::Sudo;
+
+sub runGConf
+{
+    my ($self) = @_;
+
+    EBox::Sudo::command('crontab -r');
+
+}
+
+EBox::init();
+
+my $ebackup = EBox::Global->modInstance('ebackup');
+my $migration = new EBox::Migration(
+    'gconfmodule' => $ebackup,
+    'version' => 3,
+);
+$migration->execute();
+
+1;
