c904d0482b885d8cf6a2849a6fbeaec00e1df96e
Jorge Salamero Sanz <jsalamero@zentyal.com>
2011-02-28 09:31:40 +0000

    NN: fix copyright note on Clients.pm file.
    git-svn-id: http://svn.zentyal.org/zentyal/branches/2.0@20668 866bc4c1-54da-0310-a958-9d5a9a0e8d04

--- client/radius/src/EBox/Model/Clients.pm
+++ client/radius/src/EBox/Model/Clients.pm
@@ -1,4 +1,4 @@
-# Copyright
+# Copyright (C) 2009-2010 eBox Technologies S.L.
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License, version 2, as
