e4600def4a8264b090043707ce726156ad1a3c2c
José Antonio Calvo <jacalvo@zentyal.com>
2011-03-14 22:34:21 +0000

    NN: merge r20822 from branches/jag/2.0-misc/client
    git-svn-id: http://svn.zentyal.org/zentyal/trunk@20832 866bc4c1-54da-0310-a958-9d5a9a0e8d04

--- main/mail/src/EBox/Mail/Model/Report/TrafficDetails.pm
+++ main/mail/src/EBox/Mail/Model/Report/TrafficDetails.pm
@@ -90,8 +90,7 @@ sub _table
             'tableName' =>__PACKAGE__->tableName(),
             'printableTableName' => __('Mail traffic details'),
             'defaultActions' => [ 'changeView', 'editField' ],
-        'defaultController' =>
-         '/zentyal/Mail/Controller/MailReport',
+            'defaultController' => '/zentyal/Mail/Controller/TrafficDetails',
             'tableDescription' => $tableHead,
             'class' => 'dataTable',
             'order' => 0,
