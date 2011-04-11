d7d99611546a48fe5308cdb74fd9e1e35f7c0de8
Jorge Salamero Sanz <jsalamero@zentyal.com>
2011-04-11 11:26:34 +0000

    NN: updates packaging stubs, deletes migration and new version on changelog.
    git-svn-id: http://svn.zentyal.org/zentyal/trunk@21076 866bc4c1-54da-0310-a958-9d5a9a0e8d04

--- extra/moddev/stubs/debian/cdbs/ebox.mk
+++ extra/moddev/stubs/debian/cdbs/ebox.mk
@@ -3,14 +3,13 @@ DEB_CONFIGURE_SCRIPT_ENV += CONFPATH="/var/lib/zentyal/conf"
 DEB_CONFIGURE_SCRIPT_ENV += STUBSPATH="/usr/share/zentyal/stubs"
 DEB_CONFIGURE_SCRIPT_ENV += CGIPATH="/usr/share/zentyal/cgi/"
 DEB_CONFIGURE_SCRIPT_ENV += TEMPLATESPATH="/usr/share/zentyal/templates"
-DEB_CONFIGURE_SCRIPT_ENV += SCHEMASPATH="/usr/share/zentyal/schemas"
 DEB_CONFIGURE_SCRIPT_ENV += WWWPATH="/usr/share/zentyal/www/"
 DEB_CONFIGURE_SCRIPT_ENV += CSSPATH="/usr/share/zentyal/www/css"
 DEB_CONFIGURE_SCRIPT_ENV += IMAGESPATH="/usr/share/zentyal/www/images"
 DEB_CONFIGURE_SCRIPT_ENV += VARPATH="/var"
 DEB_CONFIGURE_SCRIPT_ENV += ETCPATH="/etc/zentyal"
+
 DEB_CONFIGURE_EXTRA_FLAGS := --disable-runtime-tests
-DEB_MAKE_FLAGS += schemadir=usr/share/gconf/schemas
 DEB_MAKE_INVOKE = $(MAKE) $(DEB_MAKE_FLAGS) -C $(DEB_BUILDDIR)
 
 $(patsubst %,binary-install/%,$(DEB_PACKAGES)) :: binary-install/%:
@@ -20,3 +19,4 @@ $(patsubst %,binary-install/%,$(DEB_PACKAGES)) :: binary-install/%:
 		DESTFILE=$$(basename $$(echo $$event | sed 's/\.upstart//g')); \
 		install -m 644 "$$event" debian/$(cdbs_curpkg)/etc/event.d/$$DESTFILE; \
 	done;
+
