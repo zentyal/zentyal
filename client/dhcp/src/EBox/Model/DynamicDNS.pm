6792e39579c41781de479381dffdbe48ac6b2430
Enrique J. Hernández Blasco <ejhernandez@zentyal.com>
2011-05-02 14:19:08 +0000

    NN: Do not fail if the DNS is not installed
    git-svn-id: http://svn.zentyal.org/zentyal/branches/2.0@21246 866bc4c1-54da-0310-a958-9d5a9a0e8d04

--- client/dhcp/src/EBox/Model/DynamicDNS.pm
+++ client/dhcp/src/EBox/Model/DynamicDNS.pm
@@ -1,4 +1,4 @@
-# Copyright (C) 2009-2010 eBox Technologies S.L.
+# Copyright (C) 2009-2011 eBox Technologies S.L.
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License, version 2, as
@@ -386,7 +386,11 @@ sub _manageZone
 # Get the domain model
 sub _domainModel
 {
-    return EBox::Global->modInstance('dns')->model('DomainTable');
+    if ( EBox::Global->modExists('dns') ) {
+        return EBox::Global->modInstance('dns')->model('DomainTable');
+    } else {
+        return undef;
+    }
 }
 
 1;
