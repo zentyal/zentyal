eab6f157365593bf70eba070ff911017cb48ea07
Carlos Pérez-Aradros Herce <cperez@zentyal.com>
2011-06-28 10:02:12 +0000

    NN: added protocol info on monitor output
    git-svn-id: http://svn.zentyal.org/zentyal/trunk@21961 866bc4c1-54da-0310-a958-9d5a9a0e8d04

--- main/captiveportal/src/bwmonitor/dumpers/console.cpp
+++ main/captiveportal/src/bwmonitor/dumpers/console.cpp
@@ -30,9 +30,15 @@ void ConsoleBWStatsDumper::dumpHost(HostStats *host) {
     cout << "IP=" << ip;
     cout << " INT_SENT=" << internal->totalSent/1024;
     cout << " INT_RECV=" << internal->totalRecv/1024;
+    cout << " INT_TCP="  << internal->TCP/1024;
+    cout << " INT_UDP="  << internal->UDP/1024;
+    cout << " INT_ICMP=" << internal->ICMP/1024;
 
     cout << " EXT_SENT=" << external->totalSent/1024;
     cout << " EXT_RECV=" << external->totalRecv/1024;
+    cout << " EXT_TCP="  << external->TCP/1024;
+    cout << " EXT_UDP="  << external->UDP/1024;
+    cout << " EXT_ICMP=" << external->ICMP/1024;
     cout << endl;
 }
 
