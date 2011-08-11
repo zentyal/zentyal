69cc912bc17d663d9b6fd78bd37cec67be85a959
Enrique J. Hernández Blasco <ejhernandez@zentyal.com>
2011-08-11 16:13:11 +0000

    NN: Merging bwmonitor-report branch
    git-svn-id: http://svn.zentyal.org/zentyal/trunk@22680 866bc4c1-54da-0310-a958-9d5a9a0e8d04

---
+++ main/bwmonitor/schemas/sql/bwmonitor_usage_report.sql
@@ -0,0 +1,24 @@
+CREATE TABLE bwmonitor_usage_report (
+    client INET,
+    username VARCHAR(255),
+
+    /* internal traffic */
+    intTotalRecv  BIGINT,
+    intTotalSent  BIGINT,
+    intTCP        BIGINT,
+    intUDP        BIGINT,
+    intICMP       BIGINT,
+
+    /* external traffic */
+    extTotalRecv  BIGINT,
+    extTotalSent  BIGINT,
+    extTCP        BIGINT,
+    extUDP        BIGINT,
+    extICMP       BIGINT,
+
+    date DATE);
+
+CREATE INDEX bwmonitor_usage_report_date_i on bwmonitor_usage_report(date);
+CREATE INDEX bwmonitor_usage_report_client_i on bwmonitor_usage_report(client);
+CREATE INDEX bwmonitor_usage_report_username_i on bwmonitor_usage_report(username);
+
