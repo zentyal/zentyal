1a28b785fd5c84b1b2eeb276e0c48c2da611a77d
José Antonio Calvo <jacalvo@zentyal.com>
2011-08-29 00:46:07 +0000

    NN: fixed indentation
    git-svn-id: http://svn.zentyal.org/zentyal/trunk@22968 866bc4c1-54da-0310-a958-9d5a9a0e8d04

--- main/core/src/EBox/Menu/Item.pm
+++ main/core/src/EBox/Menu/Item.pm
@@ -25,40 +25,40 @@ use EBox::Gettext;
 
 sub new
 {
-	my $class = shift;
-	my %opts = @_;
-	my $url = delete $opts{url};
-	unless (defined($url) and ($url ne '')) {
-		throw EBox::Exceptions::MissingArgument('url');
-	}
-	my $self = $class->SUPER::new(@_);
-	bless($self, $class);
-	$self->{url} = $url;
-	return $self;
+    my $class = shift;
+    my %opts = @_;
+    my $url = delete $opts{url};
+    unless (defined($url) and ($url ne '')) {
+        throw EBox::Exceptions::MissingArgument('url');
+    }
+    my $self = $class->SUPER::new(@_);
+    bless($self, $class);
+    $self->{url} = $url;
+    return $self;
 }
 
 sub add # (item)
 {
-	my $self = shift;
-	throw EBox::Exceptions::Internal(
-		"EBox::Menu::Item cannot have children");
+    my $self = shift;
+    throw EBox::Exceptions::Internal(
+        "EBox::Menu::Item cannot have children");
 }
 
 sub html
 {
-	my ($self, $current) = @_;
-	my $text = $self->{text};
-	my $url = $self->{url};
-	my $html = '';
+    my ($self, $current) = @_;
+    my $text = $self->{text};
+    my $url = $self->{url};
+    my $html = '';
 
-	(length($text) == 0) and return $html;
+    (length($text) == 0) and return $html;
 
-	my $class = "";
-	if (defined($current) and ($current eq $url)) {
-		$class = "current ";
-	}
-	if (defined($self->{style})) {
-		$class .= $self->{style};
+    my $class = "";
+    if (defined($current) and ($current eq $url)) {
+        $class = "current ";
+    }
+    if (defined($self->{style})) {
+        $class .= $self->{style};
     }
     if($class) {
         $class = "class='$class'";
@@ -69,14 +69,14 @@ sub html
        $style = qq/style='display:inline;'/;
     }
 
-	$html .= "<li id='" . $self->{id} . "' $style $class>\n";
+    $html .= "<li id='" . $self->{id} . "' $style $class>\n";
 
-	$html .= qq{<a title="$text" href="/$url" class="navc" }
-		 . qq{ target="_parent">$text</a>\n};
+    $html .= qq{<a title="$text" href="/$url" class="navc" }
+         . qq{ target="_parent">$text</a>\n};
 
-	$html .= "</li>\n";
+    $html .= "</li>\n";
 
-	return $html;
+    return $html;
 }
 
 1;
