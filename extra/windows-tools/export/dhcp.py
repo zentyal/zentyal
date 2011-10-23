270ad3781033c6bc6c5d092a6f659c72509452ba
José Antonio Calvo <jacalvo@zentyal.com>
2011-10-23 20:54:02 +0000

    added IPy 0.75, unify dhcp.py deleting duplicated code, now the module can be also used for debugging if executed in commandline
    git-svn-id: http://svn.zentyal.org/zentyal/trunk@23499 866bc4c1-54da-0310-a958-9d5a9a0e8d04

--- extra/windows-tools/export/dhcp.py
+++ extra/windows-tools/export/dhcp.py
@@ -17,55 +17,69 @@
 
 # Parse a dhcp dump from windows server to extract configuration values
 
-import subprocess, re, yaml
+import subprocess, re, yaml, socket
+from IPy import IP
 
 ipre = '\\d+\\.\\d+\\.\\d+\\.\\d+'
 cmd = 'netsh dhcp server dump'
 
 def export(filepath):
     # Compile reg exps
-    server_def = re.compile('Dhcp Server ('+ipre+') add scope ('+ipre+') ('+ipre+') "([^"]*)"')
-    range_def = re.compile('Dhcp Server ('+ipre+') Scope ('+ipre+') Add iprange ('+ipre+') ('+ipre+')')
-    reserved_def = re.compile('Dhcp Server ('+ipre+') Scope ('+ipre+') Add reservedip ('+ipre+') ([0-9abcdef]+) "(.*)" ".*" ".*"')
+    server_def = re.compile('Dhcp Server '+ipre+' add scope ('+ipre+') ('+ipre+') "([^"]*)"')
+    range_def = re.compile('Dhcp Server '+ipre+' Scope ('+ipre+') Add iprange ('+ipre+') ('+ipre+')')
+    reserved_def = re.compile('Dhcp Server '+ipre+' Scope ('+ipre+') Add reservedip ('+ipre+') ([0-9abcdef]+) "(.*)" ".*" ".*"')
 
     # result
     dhcp_servers = {}
+
+    # local IPs for this machine
+    local_ips = [IP(x) for x in socket.gethostbyname_ex('')[2]]
 
     p = subprocess.Popen(cmd, shell=True, stdin=subprocess.PIPE, stdout=subprocess.PIPE)
     for line in p.stdout:
         match = server_def.match(line)
         if match:
-            (server_ip, network, netmask, name) = match.groups()
+            (network, netmask, name) = match.groups()
             if network not in dhcp_servers:
                 dhcp_servers[network] = {}
+
+            Network = IP(network + '/' + netmask)
+            for ip in local_ips:
+                if ip in Network:
+                    dhcp_servers[network]['ip'] = str(ip)
+
             dhcp_servers[network]['network'] = network
             dhcp_servers[network]['netmask'] = netmask
             dhcp_servers[network]['name'] = name
-            print 'DEBUG: DHCP server listening on ' + server_ip + ' for ' + network + '-' + netmask + ' ('+name+')'
 
         match = range_def.match(line)
         if match:
-            (server_ip, network, range_start, range_end) = match.groups()
+            (network, range_start, range_end) = match.groups()
             if network not in dhcp_servers:
                 dhcp_servers[network] = {}
-                dhcp_servers[network]['ip'] = network
             if 'ranges' not in dhcp_servers[network]:
                 dhcp_servers[network]['ranges'] = []
             dhcp_servers[network]['ranges'].append({'from':range_start, 'to':range_end})
-            print 'DEBUG: DHCP range for ' + server_ip + ': ' + range_start + '-' + range_end
+            print 'DEBUG: DHCP range for ' + network + ': ' + range_start + '-' + range_end
 
         match = reserved_def.match(line)
         if match:
-            (server_ip, network, ip, mac, name) = match.groups()
+            (network, ip, mac, name) = match.groups()
             mac = ":".join(mac[i:i+2] for i in xrange(0, len(mac), 2))
             if network not in dhcp_servers:
                 dhcp_servers[network] = {}
-                dhcp_servers[network]['ip'] = network
             if 'fixed_addrs' not in dhcp_servers[network]:
                 dhcp_servers[network]['fixed_addrs'] = []
             dhcp_servers[network]['fixed_addrs'].append({'ip':ip, 'mac':mac, 'name':name})
             print 'DEBUG: DHCP fixed address for ' + ip + ', mac ' + mac + ': ' + name
 
-    f = open(filepath, 'w')
-    f.write(yaml.dump(dhcp_servers.values()))
-    f.close()
+    dump = yaml.dump(dhcp_servers.values())
+    if filepath:
+        f = open(filepath, 'w')
+        f.write(dump)
+        f.close()
+    else:
+        print dump
+
+if __name__ == "__main__":
+    export(None)
