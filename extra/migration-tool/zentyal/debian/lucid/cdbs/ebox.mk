caf57c3cb8e7c522933b78dd07ef12836c96708b
José Antonio Calvo <jacalvo@zentyal.com>
2011-10-24 01:07:30 +0000

    new zentyal-migration-tool package, with the dhcp and dns import scripts moved there
    git-svn-id: http://svn.zentyal.org/zentyal/trunk@23509 866bc4c1-54da-0310-a958-9d5a9a0e8d04

---
+++ extra/migration-tool/zentyal/debian/lucid/cdbs/ebox.mk
@@ -0,0 +1,21 @@
+DEB_CONFIGURE_SCRIPT_ENV += LOGPATH="/var/log/zentyal"
+DEB_CONFIGURE_SCRIPT_ENV += CONFPATH="/var/lib/zentyal/conf"
+DEB_CONFIGURE_SCRIPT_ENV += STUBSPATH="/usr/share/zentyal/stubs"
+DEB_CONFIGURE_SCRIPT_ENV += CGIPATH="/usr/share/zentyal/cgi/"
+DEB_CONFIGURE_SCRIPT_ENV += TEMPLATESPATH="/usr/share/zentyal/templates"
+DEB_CONFIGURE_SCRIPT_ENV += WWWPATH="/usr/share/zentyal/www/"
+DEB_CONFIGURE_SCRIPT_ENV += CSSPATH="/usr/share/zentyal/www/css"
+DEB_CONFIGURE_SCRIPT_ENV += IMAGESPATH="/usr/share/zentyal/www/images"
+DEB_CONFIGURE_SCRIPT_ENV += VARPATH="/var"
+DEB_CONFIGURE_SCRIPT_ENV += ETCPATH="/etc/zentyal"
+
+DEB_CONFIGURE_EXTRA_FLAGS := --disable-runtime-tests
+DEB_MAKE_INVOKE = $(MAKE) $(DEB_MAKE_FLAGS) -C $(DEB_BUILDDIR)
+
+$(patsubst %,binary-install/%,$(DEB_PACKAGES)) :: binary-install/%:
+	for event in debian/*.upstart ; do \
+		[ -f $$event ] || continue; \
+		install -d -m 755 debian/$(cdbs_curpkg)/etc/init; \
+		DESTFILE=$$(basename $$(echo $$event | sed 's/\.upstart/.conf/g')); \
+		install -m 644 "$$event" debian/$(cdbs_curpkg)/etc/init/$$DESTFILE; \
+	done;
---
+++ extra/migration-tool/zentyal/src/EBox/MigrationTool.pm
@@ -0,0 +1,53 @@
+# Copyright (C) 2011 eBox Technologies S.L.
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License, version 2, as
+# published by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+package EBox::MigrationTool;
+
+use strict;
+use warnings;
+
+use base qw(EBox::GConfModule);
+
+use EBox::Gettext;
+use EBox::Global;
+
+sub _create
+{
+    my $class = shift;
+    my $self =$class->SUPER::_create(name => 'migration-tool',
+                                     printableName => __('Migration Tool'),
+                                     @_);
+
+    bless($self, $class);
+    return $self;
+}
+
+# Method: menu
+#
+#       Overrides EBox::Module method.
+#
+sub menu
+{
+    my ($self, $root) = @_;
+
+    my $item = new EBox::Menu::Item('name' => 'MigrationTool',
+                                    'text' => $self->printableName(),
+                                    'url' => 'MigrationTool/Help',
+                                    'separator' => 'Core',
+                                    'order' => 105);
+    $root->add($item);
+}
+
+1;
