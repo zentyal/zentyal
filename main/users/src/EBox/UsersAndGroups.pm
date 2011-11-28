8d4b0af63f91bbde902b7031133c6d10c47a650c
José Antonio Calvo <jacalvo@zentyal.com>
2011-11-28 16:41:01 +0000

    NN: fixed previous commit
    git-svn-id: http://svn.zentyal.org/zentyal/trunk@23715 866bc4c1-54da-0310-a958-9d5a9a0e8d04

--- main/users/src/EBox/UsersAndGroups.pm
+++ main/users/src/EBox/UsersAndGroups.pm
@@ -309,9 +309,9 @@ sub enableActions
     # make it ready for any user/group query
     $self->restartService();
 
-    # mark apache as changed to avoid problems with getpwent calls
-    # it needs to be restarted to be aware of the new nsswitch conf
-    EBox::Global->modChange('apache');
+    # restart apache to avoid problems with getpwent calls, it needs
+    # to be restarted to be aware of the new nsswitch conf
+    EBox::Global->modInstance('apache')->restartService();
 }
 
 
