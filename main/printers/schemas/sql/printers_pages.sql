6b07120a3389ea1d68e63ba7dc80d32bbb140f83
José Antonio Calvo <jacalvo@zentyal.com>
2012-01-21 21:56:58 +0100

    fixed tables creation in printers

--- main/printers/schemas/sql/printers_pages.sql
+++ main/printers/schemas/sql/printers_pages.sql
@@ -1,8 +1,8 @@
 CREATE TABLE printers_pages(
-         timestamp TIMESTAMP, 
-         job INT, 
-         printer VARCHAR(255) NOT NULL,     
+         timestamp TIMESTAMP,
+         job INT,
+         printer VARCHAR(255) NOT NULL,
          pages INT
-        );
-CREATE INDEX printers_pages_job_i on printers_pages(timestamp);
+);
 
+CREATE INDEX printers_pages_job_i on printers_pages(timestamp);
