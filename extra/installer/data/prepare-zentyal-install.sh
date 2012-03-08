97bea65aa78bd5bc34c6dd737278640ea6d10d92
José Antonio Calvo <jacalvo@zentyal.com>
2012-03-08 09:55:54 +0100

    Workaround for broken /etc/hosts when there is no domain

--- extra/installer/data/prepare-zentyal-install.sh
+++ extra/installer/data/prepare-zentyal-install.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# Workaround for broken /etc/hosts when there is no domain
+sed -i 's/127.0.1.1.*(null)/127.0.1.1/' /etc/hosts
+
 # copy *.deb files from CD to hard disk
 PKG_DIR=/var/tmp/zentyal-packages
 mkdir $PKG_DIR
