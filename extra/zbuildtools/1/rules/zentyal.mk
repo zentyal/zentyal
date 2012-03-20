135cfac0919a72b7afb6af9250bf8f50cd7e9c4d
Jorge Salamero Sanz <jsalamero@zentyal.com>
2012-03-20 21:54:25 +0100

    Fixes zbuildtools for precise.

--- extra/zbuildtools/1/rules/zentyal.mk
+++ extra/zbuildtools/1/rules/zentyal.mk
@@ -3,9 +3,9 @@ include /usr/share/cdbs/1/rules/debhelper.mk
 $(patsubst %,binary-install/%,$(DEB_PACKAGES)) :: binary-install/%:
 	for event in debian/*.upstart ; do \
 		[ -f $$event ] || continue; \
-		install -d -m 755 debian/$(cdbs_curpkg)/etc/init; \
+		install -d -m 755 debian/$(DEB_SOURCE_PACKAGE)/etc/init; \
 		DESTFILE=$$(basename $$(echo $$event | sed 's/\.upstart/.conf/g')); \
-		install -m 644 "$$event" debian/$(cdbs_curpkg)/etc/init/$$DESTFILE; \
+		install -m 644 "$$event" debian/$(DEB_SOURCE_PACKAGE)/etc/init/$$DESTFILE; \
 	done;
 
 common-install-indep::
