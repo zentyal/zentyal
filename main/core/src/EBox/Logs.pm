2f6d95661935ac7c5e579cd18a4827e6715bad14
Javier Amor García <jamor@zentyal.com>
2012-04-09 11:01:55 +0200

    migration for regexes in user queries

--- main/core/src/EBox/Logs.pm
+++ main/core/src/EBox/Logs.pm
@@ -694,7 +694,7 @@ sub _sqlStmnt
 
     if ($sql->{'regexp'}) {
         foreach my $field (keys %{$sql->{'regexp'}}) {
-            $stmt .= "$and CAST($field as text) ~ ? ";
+            $stmt .= "$and CAST($field as CHAR CHARACTER SET utf8) LIKE ? ";
             $and = 'AND';
             push @params, $sql->{'regexp'}->{$field};
         }
