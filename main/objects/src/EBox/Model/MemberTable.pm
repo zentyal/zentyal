f10fe8848e78593e2fd29d45db725e526f4c759e
José Antonio Calvo <jacalvo@zentyal.com>
2012-05-28 05:52:23 +0200

    some minor 'if' refactoring
    and fixed string

--- main/objects/src/EBox/Model/MemberTable.pm
+++ main/objects/src/EBox/Model/MemberTable.pm
@@ -127,16 +127,16 @@ sub validateTypedRow
         my $ip = $ipaddr->ip();
         my $mask = $ipaddr->mask();
 
-        if ($ip =~ /\.0+$/) {
-            if ($mask eq '32') {
+        if ($mask eq '32') {
+            if ($ip =~ /\.0+$/) {
                 throw EBox::Exceptions::External(
-                    __('Only network addresses can end in a zero'));
+                        __('Only network addresses can end with a zero'));
             }
-        }
-
-        if (defined($mac) and $mask ne '32') {
-            throw EBox::Exceptions::External(
-            __('You can only use MAC addresses with hosts'));
+        } else {
+            if (defined ($mac)) {
+                throw EBox::Exceptions::External(
+                        __('You can only use MAC addresses with hosts'));
+            }
         }
 
         $printableValue = $ipaddr->printableValue();
