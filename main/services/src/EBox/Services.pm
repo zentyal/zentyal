6d90699531d15e4b73b40c8afaf43ed5834df1a2
Javier Amor García <jamor@zentyal.com>
2012-06-27 11:28:01 +0200

    Added serviceIptablesArgs method

--- main/services/src/EBox/Services.pm
+++ main/services/src/EBox/Services.pm
@@ -206,15 +206,15 @@ sub serviceConfiguration
     my $row = $self->model('ServiceTable')->row($id);
 
     unless (defined($row)) {
-        throw EBox::Exceptions::DataNotFound('data' => 'id',
+        throw EBox::Exceptions::DataNotFound('data' => 'service by id',
                 'value' => $id);
     }
 
     my $model = $row->subModel('configuration');
 
     my @conf;
-    for my $id (@{$model->ids()}) {
-	my $subRow = $model->row($id);
+    foreach my $id (@{$model->ids()}) {
+        my $subRow = $model->row($id);
         push (@conf, {
                         'protocol' => $subRow->valueByName('protocol'),
                         'source' => $subRow->valueByName('source'),
@@ -223,6 +223,36 @@ sub serviceConfiguration
     }
 
     return \@conf;
+}
+
+# Method: serviceIptablesArgs
+#
+#  get a list with the iptables arguments required to match each of the
+#  configurations of the service (see serviceConfiguration)
+#
+#  Warning:
+#    for any/any/any configuration a empty string is the correct iptables argument
+sub serviceIptablesArgs
+{
+    my ($self, $id) = @_;
+    my @args;
+    my @conf =  @{ $self->serviceConfiguration($id) };
+    foreach my $conf (@conf) {
+        my $args = '';
+        if ($conf->{protocol} ne 'any') {
+            $args .= '--protocol ' . $conf->{protocol};
+        }
+        if ($conf->{source} ne 'any') {
+            $args .= ' --sport ' . $conf->{source};
+        }
+        if ($conf->{destination} ne 'any') {
+            $args .= ' --dport ' . $conf->{destination};
+        }
+
+        push @args, $args;
+    }
+
+    return \@args;
 }
 
 # Method: addService
