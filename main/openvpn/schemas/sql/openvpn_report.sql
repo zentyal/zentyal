6d62ac65bb53df406b4ad475e5255bc9387a78cd
Enrique J. Hernández Blasco <ejhernandez@zentyal.com>
2012-07-02 16:46:27 +0200

    Move report code to remoteservices

--- main/openvpn/schemas/sql/openvpn_report.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE IF NOT EXISTS openvpn_report (
-       `date` DATE,
-       daemon_name VARCHAR(20),
-       daemon_type VARCHAR(10),
-       ip INT UNSIGNED,
-       certificate VARCHAR(100),
-       connections INT
-) ENGINE = MyISAM;
