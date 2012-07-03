083ea13b5b1e814c5bb0148364390630966a4278
Enrique J. Hernández Blasco <ejhernandez@zentyal.com>
2012-07-03 10:26:59 +0200

    Added Squid reporter

---
+++ main/remoteservices/src/EBox/RemoteServices/Reporter/Squid.pm
@@ -0,0 +1,73 @@
+# Copyright (C) 2012 eBox Technologies S.L.
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License, version 2, as
+# published by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+package EBox::RemoteServices::Reporter::Squid;
+
+# Class: EBox::RemoteServices::Reporter::Squid
+#
+#      Perform the squid consolidation
+#
+
+use warnings;
+use strict;
+
+use base 'EBox::RemoteServices::Reporter::Base';
+
+# Method: module
+#
+# Overrides:
+#
+#      <EBox::RemoteServices::Reporter::Base::module>
+#
+sub module
+{
+    return 'squid';
+}
+
+# Method: name
+#
+# Overrides:
+#
+#      <EBox::RemoteServices::Reporter::Base::name>
+#
+sub name
+{
+    return 'squid_access';
+}
+
+# Group: Protected methods
+
+# Method: _consolidate
+#
+# Overrides:
+#
+#     <EBox::Exceptions::Reporter::Base::_consolidate>
+#
+sub _consolidate
+{
+    my ($self, $begin, $end) = @_;
+
+    my $res = $self->{db}->query_hash(
+        { select => $self->_hourSQLStr() . ','
+                    . q{rfc931 AS username, remotehost AS ip, domain, event, code,
+                        SUM(bytes) AS bytes, COUNT(event) AS hits},
+          from   => $self->name(),
+          where  => $self->_rangeSQLStr($begin, $end),
+          group  => $self->_groupSQLStr() . ', username, ip, domain, event, code'
+         });
+    return $res;
+}
+
+1;
