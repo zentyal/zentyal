a09fae108acd99bd367736b1965299198ceacb9e
Enrique J. Hernández Blasco <ejhernandez@zentyal.com>
2012-07-03 13:35:13 +0200

    Added printer reporter

---
+++ main/remoteservices/src/EBox/RemoteServices/Reporter/PrintersUsers.pm
@@ -0,0 +1,75 @@
+# Copyright (C) 2012 eBox Technologies S.L.
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License, version 2, as
+# published by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+package EBox::RemoteServices::Reporter::PrintersJobs;
+
+# Class: EBox::RemoteServices::Reporter::PrintersJobs
+#
+#      Perform the consolidation of printer jobs by printer
+#
+
+use warnings;
+use strict;
+
+use base 'EBox::RemoteServices::Reporter::Base';
+
+# TODO: Disabled until tested with samba4
+sub enabled { return 0; }
+
+# Method: module
+#
+# Overrides:
+#
+#      <EBox::RemoteServices::Reporter::Base::module>
+#
+sub module
+{
+    return 'printers';
+}
+
+# Method: name
+#
+# Overrides:
+#
+#      <EBox::RemoteServices::Reporter::Base::name>
+#
+sub name
+{
+    return 'printers_users';
+}
+
+# Group: Protected methods
+
+# Method: _consolidate
+#
+# Overrides:
+#
+#     <EBox::Exceptions::Reporter::Base::_consolidate>
+#
+sub _consolidate
+{
+    my ($self, $begin, $end) = @_;
+
+    my $res = $self->{db}->query_hash(
+        { select => $self->_hourSQLStr() . ','
+                    . q{username, event, COUNT(*) AS jobs},
+          from   => 'printers_jobs',
+          where  => $self->_rangeSQLStr($begin, $end),
+          group  => $self->_groupSQLStr() . ', username, event',
+         });
+    return $res;
+}
+
+1;
