0653337f247c1fe9873f8d1379f0b051ac1f59ce
Javier Amor García <jamor@zentyal.com>
2012-07-19 18:44:06 +0200

    increased next step timeout to avoid flood pache

--- main/software/src/EBox/CGI/InstallPkgs.pm
+++ main/software/src/EBox/CGI/InstallPkgs.pm
@@ -190,7 +190,7 @@ sub showInstallProgress
         reloadInterval  => 2,
         nextStepUrl => '/Wizard',
         nextStepText => __('Go to save changes'),
-        nextStepTimeout => 1
+        nextStepTimeout => 5
     );
 }
 
@@ -213,7 +213,7 @@ sub showRemoveProgress
             . '. More information on the logs in {dir}',
                          dir => EBox::Config->log()),
         reloadInterval  => 2,
-        nextStepTimeout => 1
+        nextStepTimeout => 5
     );
 }
 
