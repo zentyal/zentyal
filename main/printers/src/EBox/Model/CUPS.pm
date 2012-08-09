9cd61694aec21870f385b594d5c2c6febde8c0db
Samuel Cabrero <scabrero@zentyal.com>
2012-08-09 11:55:31 +0200

    Add new model to manage printer ACLs

--- main/printers/src/EBox/Model/CUPS.pm
+++ main/printers/src/EBox/Model/CUPS.pm
@@ -46,26 +46,6 @@ sub new
     return $self;
 }
 
-# Method: viewCustomizer
-#
-#      Return a custom view customizer to set a permanent message if
-#      the VPN is not enabled or configured
-#
-# Overrides:
-#
-#      <EBox::Model::DataTable::viewCustomizer>
-#
-sub viewCustomizer
-{
-    my ($self) = @_;
-
-    my $customizer = new EBox::View::Customizer();
-    $customizer->setModel($self);
-    $customizer->setPermanentMessage($self->_configureMessage());
-
-    return $customizer;
-}
-
 # Method: syncRows
 #
 #   Overrides <EBox::Model::DataTable::syncRows>
@@ -163,20 +143,6 @@ sub _table
         help               => __('Select in which interfaces the CUPS webserver will listen on. Take into account that is not probably a good idea to listen on external interfaces. If none are selected, it will be listening only at localhost'),
     };
     return $dataForm;
-}
-
-sub _configureMessage
-{
-    my ($self) = @_;
-
-    my $CUPS_PORT = 631;
-    my $URL = "https://localhost:$CUPS_PORT/admin";
-    my $message = __x('To add or manage printers you have to use the {open_href}CUPS Web Interface{close_href}',
-                      open_href => "<a href='$URL' target='_blank' id='cups_url'>",
-                      close_href => '</a>');
-    $message .= "<script>document.getElementById('cups_url').href='https://' + document.domain + ':$CUPS_PORT/admin';</script>";
-
-    return $message;
 }
 
 1;
