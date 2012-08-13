992d389518e21be5adbcccdfbc169bfb064ac168
Enrique J. Hernández Blasco <ejhernandez@zentyal.com>
2012-08-13 17:45:04 +0200

    Remove no longer used backup from proxy feature

--- main/remoteservices/src/EBox/CGI/Backup/Confirm.pm
+++ main/remoteservices/src/EBox/CGI/Backup/Confirm.pm
@@ -14,13 +14,12 @@
 # Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
 package EBox::CGI::RemoteServices::Backup::Confirm;
-use base qw(EBox::CGI::RemoteServices::Backup::ProxyBase);
+use base qw(EBox::CGI::ClientBase);
 
 use strict;
 use warnings;
 
 use EBox::RemoteServices::Backup;
-use EBox::RemoteServices::ProxyBackup;
 use EBox::Gettext;
 use EBox::Exceptions::Internal;
 use EBox::Exceptions::External;
@@ -44,14 +43,7 @@ sub _setErrorChain
 {
     my ($self) = @_;
 
-    my $errorchain;
-    if ($self->param('action') eq 'restoreProxy') {
-        $errorchain = "RemoteServices/Backup/Proxy";
-    }
-    else {
-        $errorchain = "RemoteServices/Backup/Index";
-    }
-
+    my $errorchain = "RemoteServices/Backup/Index";
     $self->{errorchain} = $errorchain;
 }
 
@@ -72,7 +64,6 @@ sub optionalParameters
 my %cgiByAction = (
            delete  => 'DeleteRemoteBackup',
            restore => 'RestoreRemoteBackup',
-           restoreProxy =>  'RestoreProxyRemoteBackup',
            overwrite   => 'OverwriteRemoteBackup',
 );
 
@@ -94,18 +85,6 @@ sub restoreOkText
     return __('Restore');
 }
 
-
-sub restoreProxyText
-{
-    my ($self) = @_;
-    return __('Please confirm that you want to restore the configuraction using the following remote backup:')
-}
-
-sub restoreProxyOkText
-{
-    my ($self) = @_;
-    return __('Restore');
-}
 
 sub deleteText
 {
@@ -135,32 +114,10 @@ sub _backup
 {
     my ($self, $action) = @_;
 
-    if ($action eq 'restoreProxy') {
-        return $self->_remoteProxyBackup();
-    }
-
-    return $self->_remoteBackup();
-}
-
-sub _remoteBackup
-{
-    my ($self) = @_;
-    my $name   = $self->param('name');
-
-    my $backupService =  new EBox::RemoteServices::Backup;
-    return $backupService->remoteBackupInfo($name);
-}
-
-sub _remoteProxyBackup
-{
-    my ($self) = @_;
-
-    my $backupService =  $self->backupService();
-
-    my $cn   = $self->param('cn');
     my $name = $self->param('name');
 
-    return $backupService->remoteBackupInfo($cn, $name);
+    my $backupService =  new EBox::RemoteServices::Backup();
+    return $backupService->remoteBackupInfo($name);
 }
 
 sub masonParameters
