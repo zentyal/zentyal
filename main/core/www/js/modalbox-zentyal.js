0511ee8213ada05fd864a1cd2fff0f0ef4919694
Javier Amor García <jamor@zentyal.com>
2012-08-21 13:15:27 +0200

    Dialog windows are centered again after new look and feel changes

--- main/core/www/js/modalbox-zentyal.js
+++ main/core/www/js/modalbox-zentyal.js
@@ -5,6 +5,7 @@
 Modalbox.show = Modalbox.show.wrap(function (origFunc) {
                                      var args = Array.prototype.slice.call(arguments, 1);
                                      origFunc.apply(Modalbox, args);
+                                     Modalbox.MBwindow.setStyle({'left' : 'auto'});
                                      Modalbox.MBwindow.addClassName('MB_dialog');
                                    });
 
