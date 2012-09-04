64b5c93652d69eb998db86829254c4cec5ba54ee
Samuel Cabrero <scabrero@zentyal.com>
2012-09-04 16:47:28 +0200

    Set the managed domain as read only as well as records

--- main/users/src/EBox/UsersAndGroups.pm
+++ main/users/src/EBox/UsersAndGroups.pm
@@ -18,7 +18,10 @@ package EBox::UsersAndGroups;
 use strict;
 use warnings;
 
-use base qw(EBox::Module::Service EBox::LdapModule EBox::UserCorner::Provider EBox::UsersAndGroups::SyncProvider);
+use base qw( EBox::Module::Service
+             EBox::LdapModule
+             EBox::UserCorner::Provider
+             EBox::UsersAndGroups::SyncProvider );
 
 use EBox::Global;
 use EBox::Util::Random;
@@ -255,25 +258,29 @@ sub setupDNS
 
     # Get the host domain
     my $sysinfo = EBox::Global->modInstance('sysinfo');
+    my $ownDomain = $sysinfo->hostDomain();
     my $hostName = $sysinfo->hostName();
-    my $hostDomain = $sysinfo->hostDomain();
 
     # Create the domain in the DNS module if it does not exists
     my $dnsMod = EBox::Global->modInstance('dns');
-    my $domain = { domain_name => $hostDomain,
-                   hostnames   => [] };
-    my $domains = $dnsMod->domains();
-    my %domains = map {$_->{name} => $_} @{$domains};
-    if (not exists $domains{$hostDomain}) {
-        $dnsMod->addDomain($domain);
+    my $domainModel = $dnsMod->model('DomainTable');
+    my $row = $domainModel->find(domain => $ownDomain);
+    if (defined $row) {
+        # Set the domain as managed and readonly
+        $row->setReadOnly(1);
+        $row->elementByName('managed')->setValue(1);
+        $row->store();
+    } else {
+        $domainModel->addRow(domain => $ownDomain, managed => 1, readOnly => 1);
     }
 
     EBox::debug("Adding DNS records for kerberos");
 
     # Add the TXT record with the realm name
     my $txtRR = { name => '_kerberos',
-                  data => $hostDomain };
-    $dnsMod->addText($hostDomain, $txtRR);
+                  data => $ownDomain,
+                  readOnly => 1 };
+    $dnsMod->addText($ownDomain, $txtRR);
 
     # Add the SRV records to the domain
     my $service = { service => 'kerberos',
@@ -282,10 +289,11 @@ sub setupDNS
                     priority => 100,
                     weight => 100,
                     target_type => 'domainHost',
-                    target => $hostName };
-    $dnsMod->addService($hostDomain, $service);
+                    target => $hostName,
+                    readOnly => 1 };
+    $dnsMod->addService($ownDomain, $service);
     $service->{protocol} = 'udp';
-    $dnsMod->addService($hostDomain, $service);
+    $dnsMod->addService($ownDomain, $service);
 
     ## TODO Check if the server is a master or slave and adjust the target
     ##      to the master server
@@ -295,10 +303,11 @@ sub setupDNS
                  priority => 100,
                  weight => 100,
                  target_type => 'domainHost',
-                 target => $hostName };
-    $dnsMod->addService($hostDomain, $service);
+                 target => $hostName,
+                 readOnly => 1 };
+    $dnsMod->addService($ownDomain, $service);
     $service->{protocol} = 'udp';
-    $dnsMod->addService($hostDomain, $service);
+    $dnsMod->addService($ownDomain, $service);
 
     $service = { service => 'kpasswd',
                  protocol => 'tcp',
@@ -306,10 +315,11 @@ sub setupDNS
                  priority => 100,
                  weight => 100,
                  target_type => 'domainHost',
-                 target => $hostName };
-    $dnsMod->addService($hostDomain, $service);
+                 target => $hostName,
+                 readOnly => 1 };
+    $dnsMod->addService($ownDomain, $service);
     $service->{protocol} = 'udp';
-    $dnsMod->addService($hostDomain, $service);
+    $dnsMod->addService($ownDomain, $service);
 }
 
 # Method: enableActions
