161561943b6200612252e7286d6e0b8f421d97c1
Javier Amor García <jamor@zentyal.com>
2012-09-12 15:12:28 +0200

    untab and indent file

--- main/core/src/EBox/CGI/View/DataTable.pm
+++ main/core/src/EBox/CGI/View/DataTable.pm
@@ -26,14 +26,14 @@ use EBox::Global;
 
 sub new # (cgi=?)
 {
-	my $class = shift;
-	my %params = @_;
-	my $tableModel = delete $params{'tableModel'};
-	my $self = $class->SUPER::new('template' => $tableModel->Viewer(),
-                                      @_);
-	$self->{'tableModel'} = $tableModel;
-	bless($self, $class);
-	return $self;
+    my $class = shift;
+    my %params = @_;
+    my $tableModel = delete $params{'tableModel'};
+    my $self = $class->SUPER::new('template' => $tableModel->Viewer(),
+                                  @_);
+    $self->{'tableModel'} = $tableModel;
+    bless($self, $class);
+    return $self;
 }
 
 # Group: Protected methods
@@ -63,30 +63,30 @@ sub _header
 
 sub _process
 {
-	my $self = shift;
+    my $self = shift;
 
-	my $global = EBox::Global->getInstance();
+    my $global = EBox::Global->getInstance();
 
-        my $model = $self->{'tableModel'};
-        $self->setMenuFolder($model->menuFolder());
-        my $directory = $self->param('directory');
-        if ($directory) {
-            $model->setDirectory($directory);
-        }
+    my $model = $self->{'tableModel'};
+    $self->setMenuFolder($model->menuFolder());
+    my $directory = $self->param('directory');
+    if ($directory) {
+        $model->setDirectory($directory);
+    }
 
-        if ( $self->param('action') eq 'presetUpdate' ) {
-            $self->_presetUpdate();
-        } else {
-            my @params;
-            push(@params, 'data' => undef );
-            push(@params, 'dataTable' => $model->table());
-            push(@params, 'model'      => $model);
-            push(@params, 'hasChanged' => $global->unsaved());
-            push(@params, 'tpages' => 0);
-            push(@params, 'page' => 0);
+    if ( $self->param('action') eq 'presetUpdate' ) {
+        $self->_presetUpdate();
+    } else {
+        my @params;
+        push(@params, 'data' => undef );
+        push(@params, 'dataTable' => $model->table());
+        push(@params, 'model'      => $model);
+        push(@params, 'hasChanged' => $global->unsaved());
+        push(@params, 'tpages' => 0);
+        push(@params, 'page' => 0);
 
-            $self->{'params'} = \@params;
-        }
+        $self->{'params'} = \@params;
+    }
 }
 
 # Group: Private methods
@@ -95,57 +95,55 @@ sub _process
 # not and then add
 sub _presetUpdate
 {
-        my ($self) = @_;
+    my ($self) = @_;
 
-        my $model = $self->{'tableModel'};
-        my $presetParams = $self->_fillTypes();
+    my $model = $self->{'tableModel'};
+    my $presetParams = $self->_fillTypes();
 
-        my ($editid, $action) = ( '', $self->param('action'));
-        if ( $model->rowUnique() ) {
-            # Then, search for the element in order to edit instead of
-            # adding a new one
-            my $foundId = $self->_findEqualRow($model, $presetParams);
-            if ( $foundId ) {
-                $editid = $foundId;
-            }
+    my ($editid, $action) = ( '', $self->param('action'));
+    if ( $model->rowUnique() ) {
+        # Then, search for the element in order to edit instead of
+        # adding a new one
+        my $foundId = $self->_findEqualRow($model, $presetParams);
+        if ( $foundId ) {
+            $editid = $foundId;
         }
-        # Not unique or just adding a new unique row
-        my $gl = EBox::Global->getInstance();
-        my @params;
-        push(@params, 'model'        => $model);
-        push(@params, 'action'       => $action);
-        push(@params, 'hasChanged'   => $gl->unsaved());
-        push(@params, 'presetParams' => $presetParams);
-        push(@params, 'editid'       => $editid);
-        push(@params, 'page'         => 0);
+    }
+    # Not unique or just adding a new unique row
+    my $gl = EBox::Global->getInstance();
+    my @params;
+    push(@params, 'model'        => $model);
+    push(@params, 'action'       => $action);
+    push(@params, 'hasChanged'   => $gl->unsaved());
+    push(@params, 'presetParams' => $presetParams);
+    push(@params, 'editid'       => $editid);
+    push(@params, 'page'         => 0);
 
-        $self->{'params'} = \@params;
-
+    $self->{'params'} = \@params;
 }
 
 # Method to fill a hash with instanced types from the array of CGI
 # params
 sub _fillTypes
 {
-	my $self = shift;
+    my $self = shift;
 
-	my $tableDesc = $self->{'tableModel'}->table()->{'tableDescription'};
+    my $tableDesc = $self->{'tableModel'}->table()->{'tableDescription'};
 
-	my %params;
-        my $cgiParams = $self->paramsAsHash();
-	foreach my $field (@{$tableDesc}) {
-            my $instancedType = $field->clone();
-            $instancedType->setValue($cgiParams->{$instancedType->fieldName()});
-            $params{$field->fieldName()} = $instancedType;
-	}
+    my %params;
+    my $cgiParams = $self->paramsAsHash();
+    foreach my $field (@{$tableDesc}) {
+        my $instancedType = $field->clone();
+        $instancedType->setValue($cgiParams->{$instancedType->fieldName()});
+        $params{$field->fieldName()} = $instancedType;
+    }
 
-	return \%params;
+    return \%params;
 }
 
 # Method to search for the same row
 sub _findEqualRow
 {
-
     my ($self, $model, $presetParams) = @_;
 
     my $foundId = 0;
