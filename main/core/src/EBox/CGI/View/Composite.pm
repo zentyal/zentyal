4ce1d70ab30a55663a6ac202b8f674f14f99b581
Javier Amor García <jamor@zentyal.com>
2012-09-12 15:20:14 +0200

    reformat uses

--- main/core/src/EBox/CGI/View/Composite.pm
+++ main/core/src/EBox/CGI/View/Composite.pm
@@ -12,6 +12,8 @@
 # You should have received a copy of the GNU General Public License
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+use strict;
+use warnings;
 
 # Class: EBox::CGI::View::Composite
 #
@@ -24,10 +26,6 @@
 #
 
 package EBox::CGI::View::Composite;
-
-use strict;
-use warnings;
-
 use base 'EBox::CGI::ClientBase';
 
 use EBox::Global;
