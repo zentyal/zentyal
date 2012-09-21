55153d94a03d63f1b64fd614a1702f6a332b8d08
Javier Amor García <jamor@zentyal.com>
2012-09-21 18:04:19 +0200

    added original parameter to revert to the orginal behavior of modalbox

--- main/core/www/js/modalbox-zentyal.js
+++ main/core/www/js/modalbox-zentyal.js
@@ -1,8 +1,11 @@
 // This files modifies Modalbox to force zentyal styles
 Modalbox.show = Modalbox.show.wrap(function (origFunc) {
+//                                     var original = arguments[2].original;
                                      var args = Array.prototype.slice.call(arguments, 1);
                                      origFunc.apply(Modalbox, args);
-                                     Modalbox.MBwindow.addClassName('MB_dialog');
+                                     if (!this.options.original) {
+                                           Modalbox.MBwindow.addClassName('MB_dialog');
+                                     }
                                    });
 Modalbox._setPosition = function() {
                                      Modalbox.MBwindow.setStyle({'left' : 'auto'});
@@ -10,15 +13,21 @@ Modalbox._setPosition = function() {
 Modalbox.resize = Modalbox.resize.wrap(function (origFunc) {
                                      var args = Array.prototype.slice.call(arguments, 1);
                                      origFunc.apply(Modalbox, args);
-                                     Modalbox.MBwindow.setStyle({'height' : 'auto'});
+                                     if (!this.options.original) {
+                                        Modalbox.MBwindow.setStyle({'height' : 'auto'});
+                                      }
                                    });
 Modalbox._putContent = Modalbox._putContent.wrap(function (origFunc) {
                                      var args = Array.prototype.slice.call(arguments, 1);
                                      origFunc.apply(Modalbox, args);
-                                     Modalbox.MBwindow.setStyle({'height' : 'auto'});
+                                     if (!this.options.original) {
+                                          Modalbox.MBwindow.setStyle({'height' : 'auto'});
+                                     }
                                    });
 Modalbox._setWidth = Modalbox._setWidth.wrap(function (origFunc) {
                                      var args = Array.prototype.slice.call(arguments, 1);
                                      origFunc.apply(Modalbox, args);
-                                     Modalbox.MBwindow.setStyle({'height' : 'auto'});
+                                     if (!this.options.original) {
+                                         Modalbox.MBwindow.setStyle({'height' : 'auto'});
+                                     }
                                    });
