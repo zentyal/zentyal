7083d4e3a59afabbadd0c4d01abfc09b795e72e5
Javier Amor García <jamor@zentyal.com>
2012-10-15 16:21:21 +0200

    typo fix to restore present status properly

--- main/squid/src/EBox/Squid/Model/DomainFilterCategories.pm
+++ main/squid/src/EBox/Squid/Model/DomainFilterCategories.pm
@@ -62,14 +62,11 @@ sub syncRows
 
     my $lists;
     my $removeDirPrefix = EBox::Squid::Types::ListArchive::unpackPath() . '/' .  EBox::Squid::Types::ListArchive::toRemovePrefix();
-    EBox::debug("removeDirPRefix $removeDirPrefix");
     my $removeDirRe = qr/^$removeDirPrefix/;
     foreach my $dir (@dirs) {
         if ($dir =~ m/$removeDirRe/) {
-            EBox::debug("$dir matched as removal dir");
             next;
         }
-            EBox::debug("$dir NOT removal dir");
         if ($self->{seenListDirectories}->{$modelConfDir}->{$dir}) {
             next;
         } else {
@@ -105,7 +102,8 @@ sub syncRows
             next if $row->valueByName('list') ne $list;
             my $rowCategory = $row->valueByName('category');
             my $present = $row->valueByName('present');
-            # remove if not file and not present
+            # remove if not file and present == true (present ==false we assume
+            # thre is not file)
             if ($present and (not exists $categories{$rowCategory}) ) {
                $self->removeRow($id);
                $modified = 1;
@@ -122,7 +120,7 @@ sub syncRows
             } else {
                 my $noPresentRow = $current{$category};
                 if ($noPresentRow) {
-                    $noPresentRow->elementByName('present')->setValue(0);
+                    $noPresentRow->elementByName('present')->setValue(1);
                     $noPresentRow->store();
                 }
                 $modified = 1;
