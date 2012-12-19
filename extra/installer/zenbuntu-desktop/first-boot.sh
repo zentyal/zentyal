08b2799882133084cc240e6a3b57f1c54f049719
José Antonio Calvo <jacalvo@zentyal.com>
2012-12-19 04:02:05 +0100

    improved installer debug

--- extra/installer/zenbuntu-desktop/first-boot.sh
+++ extra/installer/zenbuntu-desktop/first-boot.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
+LOG=/var/tmp/zentyal-installer.log
+
 plymouth message --text="Installing Zentyal core packages... Please wait."
 
-/usr/share/zenbuntu-desktop/core-install
+/usr/share/zenbuntu-desktop/core-install >> $LOG 2>&1
 if [ $? -ne 0 ]
 then
     plymouth message --text="Installation failed. Press <ESC> to see details."
@@ -11,7 +13,7 @@ fi
 
 plymouth message --text="Core packages installed. Continuing first boot..."
 
-/usr/share/zenbuntu-desktop/x11-setup
+/usr/share/zenbuntu-desktop/x11-setup >> $LOG 2>&1
 
 mv /usr/share/zenbuntu-desktop/second-boot.sh /etc/rc.local
 
