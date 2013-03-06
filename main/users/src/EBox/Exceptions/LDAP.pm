55e0278493d0271f7dc152c5718bc04d1790cd44
Javier Amor García <jamor@zentyal.com>
2013-03-06 16:13:42 +0100

    Added EBox::Exceptions::LDAP. Dont loop through the group members when adding/removing members

---
+++ main/users/src/EBox/Exceptions/LDAP.pm
@@ -0,0 +1,54 @@
+# Copyright (C) 2013 Zentyal S.L.
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License, version 2, as
+# published by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+use strict;
+use warnings;
+
+package EBox::Exceptions::LDAP;
+use base 'EBox::Exceptions::Internal';
+
+sub new
+{
+    my ($class, %args) = @_;
+    my $message = $args{message};
+    my $result = $args{result};
+    if (not $result) {
+        throw EBox::Exceptions::MissingArgument('result');
+    }
+
+    my $exText;
+    if ($message) {
+        $exText = $message . ' ';
+    }
+    $exText .= $result->error_text;
+
+    local $Error::Depth = $Error::Depth + 1;
+    local $Error::Debug = 1;
+
+    $Log::Log4perl::caller_depth++;
+    my $self = $class->SUPER::new($exText);
+    $Log::Log4perl::caller_depth--;
+
+    $self->{result} = $result;
+    bless $self, $class;
+    return $self;
+}
+
+sub errorName
+{
+    my ($self) = @_;
+    return $self->{result}->error_name();
+}
+
+1;
