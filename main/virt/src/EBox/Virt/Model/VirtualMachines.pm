7ba15a3dd0c7969ceb3aa4ad3b3627253b7f5459
Rubén Durán Balda <rduran@zentyal.com>
2013-03-15 17:30:54 +0100

    Reload the page after saving changes if a VM has been modified

--- main/virt/src/EBox/Virt/Model/VirtualMachines.pm
+++ main/virt/src/EBox/Virt/Model/VirtualMachines.pm
@@ -1,4 +1,4 @@
-# Copyright (C) 2011-2012 eBox Technologies S.L.
+# Copyright (C) 2011-2013 Zentyal S.L.
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License, version 2, as
@@ -436,5 +436,11 @@ sub vncPorts
     return \@ports;
 }
 
+sub updatedRowNotify
+{
+    my ($self) = @_;
+    my $sysinfo = EBox::Global->getInstance(1)->modInstance('sysinfo');
+    $sysinfo->setReloadPageAfterSavingChanges(1);
+}
 
 1;
