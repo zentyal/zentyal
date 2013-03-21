a89141a310a4565378223702315f94adf32c27ed
Javier Amor García <jamor@zentyal.com>
2013-03-21 18:24:12 +0100

    reformatted logout index cgi

--- main/core/src/EBox/CGI/Logout/Index.pm
+++ main/core/src/EBox/CGI/Logout/Index.pm
@@ -12,42 +12,39 @@
 # You should have received a copy of the GNU General Public License
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
-
-package EBox::CGI::Logout::Index;
-
 use strict;
 use warnings;
 
+package EBox::CGI::Logout::Index;
 use base 'EBox::CGI::ClientBase';
+
 use EBox::Global;
 use EBox::Gettext;
 
-
-sub new # (error=?, msg=?, cgi=?)
+sub new
 {
-	my $class = shift;
-	my $self = $class->SUPER::new('title' => __('Logout'),
-				      'template' => '/logout/index.mas',
-				      @_);
-	bless($self, $class);
-	return $self;
+    my $class = shift;
+    my $self = $class->SUPER::new('title' => __('Logout'),
+                                  'template' => '/logout/index.mas',
+                                  @_);
+    bless($self, $class);
+    return $self;
 }
 
 sub _process
 {
-	my $self = shift;
-	my $global = EBox::Global->getInstance();
-	if ($global->unsaved) {
-		my @array = ();
-		push(@array, 'unsaved' => 'yes');
-		$self->{params} = \@array;
-	}
+    my ($self) = @_;
+    my $global = EBox::Global->getInstance();
+    if ($global->unsaved) {
+        my @array = ();
+        push(@array, 'unsaved' => 'yes');
+        $self->{params} = \@array;
+    }
 }
 
 sub _loggedIn
 {
-	return 1;
+    return 1;
 }
-
 
 1;
