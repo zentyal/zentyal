16206c0178b0b0aa81ec875602431386a3a08a7b
Javier Amor García <jamor@zentyal.com>
2013-04-16 15:56:30 +0200

    removed innecesary rm command

--- main/mail/src/EBox/Mail.pm
+++ main/mail/src/EBox/Mail.pm
@@ -283,9 +283,6 @@ sub enableActions
 
     $self->performLDAPActions();
 
-    # Remove old keytab file
-    EBox::Sudo::root('rm -f ' . KEYTAB_FILE);
-
     # Create the kerberos service principal in kerberos,
     # export the keytab and set the permissions
     $self->kerberosCreatePrincipals();
@@ -1848,13 +1845,17 @@ sub preSlaveSetup
     $self->model('VDomains')->removeAll(1);
 }
 
+# Method: reprovisionLDAP
+#
+# Overrides:
+#
+#      <EBox::LdapModule::reprovisionLDAP>
 sub reprovisionLDAP
 {
     my ($self) = @_;
     $self->SUPER::reprovisionLDAP();
 
     # Create new kerberos keytab
-    EBox::Sudo::root('rm -f ' . KEYTAB_FILE);
     $self->kerberosCreatePrincipals();
 
     # regenerate mail ldap tree
