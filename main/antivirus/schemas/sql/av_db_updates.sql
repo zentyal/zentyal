aa4a5e50672b71f25f3eaad06c1e8aa9c38bcaf4
Enrique J. Hernández Blasco <ejhernandez@zentyal.com>
2013-04-22 00:03:49 +0200

    Set proper length to source
    * Use proper mason tag for doc

--- main/antivirus/schemas/sql/av_db_updates.sql
+++ main/antivirus/schemas/sql/av_db_updates.sql
@@ -1,6 +1,6 @@
 CREATE TABLE IF NOT EXISTS av_db_updates (
     timestamp TIMESTAMP,
-    source VARCHAR(20),
+    source VARCHAR(24),
     event VARCHAR(20),
     INDEX(timestamp)
 ) ENGINE = MyISAM;
