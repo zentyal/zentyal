c14cb6f1c512e51fa9b97424318be99a29d1e746
Javier Amor García <jamor@zentyal.com>
2013-04-29 18:16:48 +0200

    small format fixes

--- main/openvpn/src/EBox/OpenVPN/Server/ClientBundleGenerator.pm
+++ main/openvpn/src/EBox/OpenVPN/Server/ClientBundleGenerator.pm
@@ -12,12 +12,10 @@
 # You should have received a copy of the GNU General Public License
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
-
-package EBox::OpenVPN::Server::ClientBundleGenerator;
-
-# package:
 use strict;
 use warnings;
+
+package EBox::OpenVPN::Server::ClientBundleGenerator;
 
 use EBox::Global;
 use EBox::Config;
@@ -118,7 +116,6 @@ sub confFileExtraParameters
 {
     return ();
 }
-
 
 sub _copyCertFilesToDir
 {
--- main/openvpn/src/EBox/OpenVPN/Server/ClientBundleGenerator/EBoxToEBox.pm
+++ main/openvpn/src/EBox/OpenVPN/Server/ClientBundleGenerator/EBoxToEBox.pm
@@ -12,12 +12,11 @@
 # You should have received a copy of the GNU General Public License
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+use strict;
+use warnings;
 
 package EBox::OpenVPN::Server::ClientBundleGenerator::EBoxToEBox;
 use base 'EBox::OpenVPN::Server::ClientBundleGenerator';
-
-use strict;
-use warnings;
 
 use EBox::Config;
 use EBox::Gettext;
