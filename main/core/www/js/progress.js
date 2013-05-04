805e2920853554656cc26c0cde314bbd9059fc92
Javier Amor García <jamor@zentyal.com>
2013-05-04 19:23:01 +0200

    begin work in progress.js

--- main/core/www/js/progress.js
+++ main/core/www/js/progress.js
@@ -1,23 +1,24 @@
 // Copyright (C) 2004-2012 eBox Technologies S.L. licensed under the GPLv2
-
 // code used by progress.mas
-var percent = 0;
-var time = 0;
 
-var ticks = 0;
-var totalTicks = 0;
-
+"use strict";
+jQuery.noConflict();
 
 function porcentH(i){
-  this.value = 0;
-  this.setValue = function(v){
-    if(v > 100)
-      v = 100;
-    if(v < 0)
-      v = 0;
-    this.value = v;
-    $('progressValue').morph('width: ' + v + '%', { duration: 0.5 });
-    document.getElementById('percentValue').innerHTML= v+"%";
+    this.value = 0;
+    this.ticks = 0;
+    this.totalTicks = 0;
+    this.setValue = function(ticks, totalTicks){
+        this.ticks = ticks;
+        this.totalTicks = totalTicks;
+        var v = Math.ceil((ticks/totalTicks)*100);
+        if(v > 100)
+            v = 100;
+        if(v < 0)
+            v = 0;
+        this.value = v;
+        $('progressValue').morph('width: ' + v + '%', { duration: 0.5 });
+        document.getElementById('percentValue').innerHTML= v+"%";
   };
 
   this.upValue = function(v){
@@ -30,17 +31,21 @@ function porcentH(i){
     this.setValue(v);
   };
 
-};
+}
 
 var ph = new porcentH('progress');
 
 // Update the page
-function updatePage (xmlHttp, nextStepTimeout, nextStepUrl, showNotesOnFinish) {
-    var rawResponse = xmlHttp.responseText.replace(/\n/g, "<br />");
-    var response = eval("(" + rawResponse + ")");
+function updatePage (xmlHttp, timerId, nextStepTimeout, nextStepUrl, showNotesOnFinish) {
+//    var rawResponse = xmlHttp.responseText.replace(/\n/g, "<br />");
+//    var response = eval("(" + rawResponse + ")");
+    var response = jQuery.parseJSON(xmlHttp.responseText);
+
 
     if (xmlHttp.readyState == 4) {
         if (response.state == 'running') {
+            var ticks = 0;
+            var totalTicks = 0;
             // current item
             if (('message' in response) && response.message.length > 0 ) {
                 $('currentItem').innerHTML = response.message;
@@ -55,12 +60,11 @@ function updatePage (xmlHttp, nextStepTimeout, nextStepUrl, showNotesOnFinish) {
             }
 
             if ( totalTicks > 0 ) {
-                percent = Math.ceil((ticks/totalTicks)*100);
-                ph.setValue(percent);
+                ph.setValue(ticks, totalTicks);
             }
         }
         else if (response.state == 'done') {
-            pe.stop();
+            clearInterval(timerId);
             if ( nextStepTimeout > 0 ) {
 //                setTimeout ( "location.href='" + nextStepUrl + "';", nextStepTimeout*1000 );
               loadWhenAvailable(nextStepUrl, nextStepTimeout);
@@ -86,7 +90,7 @@ function updatePage (xmlHttp, nextStepTimeout, nextStepUrl, showNotesOnFinish) {
             $('ajax_request_cookie').value = 1337;
         }
         else if (response.state == 'error') {
-            pe.stop();
+            clearInterval(timerId);
             if (showNotesOnFinish) {
                Element.hide('progressing');
             }
@@ -100,43 +104,31 @@ function updatePage (xmlHttp, nextStepTimeout, nextStepUrl, showNotesOnFinish) {
     }
 }
 
-// Generate an Ajax request to fetch the current package
-function callServer(progressId, url, nextStepTimeout, nextStepUrl, showNotesOnFinish) {
 
-    // Build the URL to connect to
-    var par = "progress=" + progressId ;
-
-    new Ajax.Request(url, {
-        method: 'post',
-        parameters: par,
-        asynchronous: true,
-                       onSuccess: function (t) { updatePage(t, nextStepTimeout, nextStepUrl, showNotesOnFinish); }
-        }
-    );
-    time++;
-    if (time >= 10) {
-        time = 0;
-      if (window.showAds) {
-        showAds(1);
-      }
-    }
-
-
-}
-
-
-var pe;
 function createPeriodicalExecuter(progressId, currentItemUrl,  reloadInterval, nextStepTimeout, nextStepUrl, showNotesOnFinish)
 {
-    var callServerCurriedBody = "callServer(" + progressId + ", '"
-                                                + currentItemUrl  + "', "
-                                                + nextStepTimeout + ", '"
-                                                + nextStepUrl + "', " +
-                                                + showNotesOnFinish +  ")";
+    var time = 0;
+    var requestParams = "progress=" + progressId ;
+    var callServer = function() {
+        jQuery.ajax({
+            url: currentItemUrl,
+            data: requestParams,
+            type : 'POST',
+            complete: function (xhr) {
+                // TODO check ofr success
+                updatePage(xhr, timerId, nextStepTimeout, nextStepUrl, showNotesOnFinish);
+            }
+        });
+        time++;
+        if (time >= 10) {
+            time = 0;
+            if (window.showAds) {
+                showAds(1);
+            }
+        }
+    };
 
-    var callServerCurried = new Function(callServerCurriedBody );
-
-    pe = new PeriodicalExecuter(callServerCurried, reloadInterval);
+    var timerId = setInterval(callServer, reloadInterval*1000);
 }
 
 
