824129cfae6957e46e7473e69a4a8a2c4bb7d03d
José Antonio Calvo <jacalvo@zentyal.com>
2013-05-05 15:01:08 +0200

    fixed regression with rc.local

--- extra/installer/zenbuntu-core/second-boot.sh
+++ extra/installer/zenbuntu-core/second-boot.sh
@@ -15,8 +15,10 @@ then
     rm -rf /var/lib/zinstaller-remote
 
     # Restore default rc.local
-    if [ -f /usr/share/zenbuntu-core/rc.local ]
+    if [ -f /usr/share/zenbuntu-desktop/rc.local ]
     then
+        cp /usr/share/zenbuntu-desktop/rc.local /etc/rc.local
+    else
         cp /usr/share/zenbuntu-core/rc.local /etc/rc.local
     fi
 fi
