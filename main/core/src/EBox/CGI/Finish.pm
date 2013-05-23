d4929cda9c646e6bfec21aa6145f2a31174dbc52
Javier Amor García <jamor@zentyal.com>
2013-05-23 09:12:56 +0200

    removed deprecated code which showed the accept fiel changes dialog

--- main/core/src/EBox/CGI/Finish.pm
+++ main/core/src/EBox/CGI/Finish.pm
@@ -43,11 +43,7 @@ sub _process
     my @array = ();
     my $global = EBox::Global->getInstance();
     if ($global->unsaved) {
-        my $manager = new EBox::ServiceManager();
-        #my $askPermission = defined @{$manager->checkFiles()};
-
         push(@array, 'unsaved' => 'yes');
-        push(@array, 'askPermission' => 0);
         push(@array, 'disabledModules' => _disabledModules());
         push(@array, 'actions' => _pendingActions());
     } else {
