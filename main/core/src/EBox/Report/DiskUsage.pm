1aad600518ad345de8364e13e162c426d2a6ee5c
Javier Amor García <jamor@zentyal.com>
2013-05-30 15:54:18 +0200

    first version of new disk usage graph

--- main/core/src/EBox/Report/DiskUsage.pm
+++ main/core/src/EBox/Report/DiskUsage.pm
@@ -19,12 +19,9 @@ use warnings;
 package EBox::Report::DiskUsage;
 
 use EBox::Gettext;
-use EBox::CGI::Temp;
 use EBox::Backup;
 use EBox::FileSystem;
 
-use Chart::Pie;
-use GD;
 use Filesys::Df;
 use Perl6::Junction qw(all);
 
@@ -32,165 +29,20 @@ use constant PERCENTAGE_TO_APPEAR => 0.1; # percentage of disk size must reach a
                                           # facilty to be worthwile of display
                                           # in the chart
 
-use constant PIE_RADIUS => 50;
-
-use constant MIN_GRAPH_HEIGHT => 400;
-use constant MIN_GRAPH_WIDTH  => 650;
-
-# Function: charts
-#
-#   make a disk usage chart formatted as a pie chart for each file system
-#
-# Returns: a hash ref with the file system as key and the url needed to embed
-# the chart in a web page as value
-sub charts
+sub fsDataset
 {
-  my %charts;
-  my %usageData = %{ usage() };
+    my ($partition) = @_;
+    my $usage = usage( fileSystem => $partition);
 
-  while (my ($fsys, $usage) = each %usageData) {
-    my $datasets   = _chartDatasets($usage);
-    $charts{$fsys} =_chart($datasets);
-  }
+    exists $usage->{$partition} or
+        throw EBox::Exceptions::External(
+            __x('No usage data for {d}. Are you sure is a valid disk?', d => $partition)
+           );
 
-  return \%charts;
+    my $datasets = _chartDatasets($usage->{$partition});
+    return $datasets;
 }
 
-# Function: chart
-#
-#   make a disk usage chart formatted as a pie chart for the specified partition
-#
-#  Parametes:
-#     partition - path to the partition device file
-#
-# Returns:
-#  the url needed to embed the chart in a web page as value
-sub chart
-{
-  my ($partition) = @_;
-  my $usage = usage( fileSystem => $partition);
-
-  exists $usage->{$partition} or
-    throw EBox::Exceptions::External(
-      __x('No usage data for {d}. Are you sure is a valid disk?', d => $partition)
-    );
-
-  my $datasets = _chartDatasets($usage->{$partition});
-
-  # this is to avoid errors with labels too large
-  my $maxLabelSize = 30;
-  my $labels = $datasets->[0];
-  foreach my $label (@{ $labels }) {
-      if (length($label) > $maxLabelSize) {
-          $label = substr($label, 0, ($maxLabelSize - 4));
-          $label .= ' ...';
-      }
-  }
-
-  return _chart($datasets);
-}
-
-sub _chart
-{
-  my ($datasets) = @_;
-
-  my $imageLocation = EBox::CGI::Temp::newImage();
-
-  my $labelFont  = GD::Font->Large;
-  my $legendFont = GD::Font->Small;
-  my $textSpace  = 2;
-
-  my %colors = (
-                dataset0 => [18, 130, 76],
-               );
-
-  my $chartParams = {
-                     transparent => 'true',
-                     grey_background => 'false',
-
-                     precision       => 2,
-
-                     legend          => 'bottom',
-                     colors => \%colors,
-                     label_font => $labelFont,
-                     legend_font => $legendFont,
-                     text_space  => $textSpace,
-                    };
-
-  my $chart = new Chart::Pie( _calcGraphSize($datasets, $chartParams)  );
-
-  $chart->set (
-               %{ $chartParams  }
-            );
-
-  foreach my $ds_r (@{ $datasets }) {
-    $chart->add_dataset(  @{ $ds_r  });
-  }
-
-  $chart->png($imageLocation->{file});
-
-  return $imageLocation->{url};
-}
-
-# the calcualtion is derivated of the one found in Graph::Pie::_draw_data
-# this assumes that we use percents to show the values in the pie chart
-#  and that the legend is at the bottom
-sub _calcGraphSize
-{
-  my ($datasets, $params) = @_;
-  my $graphWidth = 1;
-  my $graphHeight = 1;
-
-  $params->{'legend_space'} = 4; # extracted from Chart::Base::_init
-
-  my $max_label_len = 1;
-
-  my %labelsByUsage = @{ $datasets };
-
-  while (my($label, $value) = each %labelsByUsage) {
-    my $text =  sprintf("%s %4.2f%%", $label, $value );
-    my $length = length $text;
-    $length += 6; # space took by percent
-    if ($length > $max_label_len) {
-      $max_label_len = $length;
-    }
-  }
-
-  my $fWidth = $params->{label_font}->width;
-  my $fHeight = $params->{label_font}->height;
-
-  $max_label_len *= $fWidth;
-
-  my $labeldistance = 2*($fWidth > $fHeight ? $fWidth : $fHeight);
-  my $pieLabelsSize =    2*$max_label_len + $labeldistance;
-
-  # graph width
-  $graphWidth = $pieLabelsSize + PIE_RADIUS;
-
-  $graphWidth += $params->{text_space} *2;
-
-  if ($graphWidth < MIN_GRAPH_WIDTH) {
-    $graphWidth = MIN_GRAPH_WIDTH;
-  }
-
-  # graph height
-  $graphHeight= $pieLabelsSize + PIE_RADIUS;
-  # calculate the height used by the legend and add it to the height
-
-  # we take a row for datapoint to simplify to don't have to follow all the
-  # calculations scattered in Chart code
-  my $rows = values %labelsByUsage;
-  my $legend_row_height = $params->{legend_font}->height + $params->{text_space};
-
-  $graphHeight += ($rows * $legend_row_height) + $params->{text_space}
-                              + (2 * $params->{'legend_space'});
-
-  if ($graphHeight < MIN_GRAPH_HEIGHT) {
-    $graphHeight = MIN_GRAPH_HEIGHT;
-  }
-
-  return ($graphWidth, $graphHeight);
-}
 
 #  Function: usage
 #
@@ -292,8 +144,9 @@ sub _chartDatasets
   my ($usageByFacility_r) = @_;
   my %usageByFacility = %{  $usageByFacility_r };
 
-  my @labels;
-  my @diskUsage;
+  my %labels;
+  my @data;
+  my %dataByLabel;
 
   # we calculate the minimal size needed to appear in the chart
   my $totalSpace = 0;
@@ -321,25 +174,44 @@ sub _chartDatasets
 
   # we don't translate the strings: 'Free space' and 'System' to avoid
   # problems with special characters in some lenguages
-  push @labels, 'Free space';
-  push @diskUsage, _sizeLabelWithUnit($freeSpace, $unit);
+  my $freeSpaceLabel = __('Free space');
+  push @data, {label => $freeSpaceLabel, data => _sizeForUnit($freeSpace, $unit)};
+  $labels{$freeSpaceLabel} = _sizeLabelWithUnit($freeSpace, $unit);
 
-  push @labels,    'System';
-  push @diskUsage, _sizeLabelWithUnit($systemUsage, $unit);
+  my $systemLabel = __('System');
+  push @data, {label => $systemLabel, data => _sizeForUnit($systemUsage, $unit)};
+  $labels{$systemLabel} = _sizeLabelWithUnit($systemUsage, $unit);
+
 
   while (my ($facilityName, $facilityUsage) = each %usageByFacility ) {
       ($facilityUsage >= $minSizeToAppear) or
           next;
 
-      push @labels, $facilityName;
-      push @diskUsage, _sizeLabelWithUnit($facilityUsage, $unit);
+      push @data, {label => $facilityName, data => _sizeForUnit($facilityUsage, $unit)};
+      $labels{$facilityName} = _sizeLabelWithUnit($facilityUsage, $unit);
   }
 
-  return [
-          \@labels,
-          \@diskUsage,
-         ];
+
+
+  return {
+          data   => \@data,
+          usageByLabel => \%labels,
+         };
 }
+
+sub _sizeForUnit
+{
+    my ($size, $unit) = @_;
+
+    if ($unit eq 'GB') {
+        return $size / 1024;
+    } elsif ($unit eq 'MB') {
+        return $size;
+    } else {
+        throw EBox::Exceptions::Internal("Unknown unit: $unit");
+    }
+}
+
 
 sub _sizeLabelWithUnit
 {
