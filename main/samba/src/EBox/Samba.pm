dd3069d551f0ca8c2795dd7af24ce7df20af124f
Samuel Cabrero <scabrero@zentyal.com>
2013-06-12 11:43:15 +0200

    Fix wrong condition when setting the netbios name

--- main/samba/src/EBox/Samba.pm
+++ main/samba/src/EBox/Samba.pm
@@ -1960,7 +1960,7 @@ sub hostNameChangedDone
 {
     my ($self, $oldHostName, $newHostName) = @_;
 
-    unless ($self->configured()) {
+    if ($self->configured()) {
         my $settings = $self->model('GeneralSettings');
         $settings->setValue('netbiosName', $newHostName);
     }
