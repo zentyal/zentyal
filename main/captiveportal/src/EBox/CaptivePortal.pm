cba8315095bdd41cd36f2825008e219c26b452b5
Carlos Perelló Marín <cperello@zentyal.com>
2013-06-25 00:52:38 +0200

    s/username/uid/g

--- main/captiveportal/src/EBox/CaptivePortal.pm
+++ main/captiveportal/src/EBox/CaptivePortal.pm
@@ -375,7 +375,7 @@ sub quotaExceeded
         return 0;
     }
 
-    my $user = EBox::Global->modInstance('users')->userByUsername($username);
+    my $user = EBox::Global->modInstance('users')->userByUID($username);
     my $quota = $self->{cpldap}->getQuota($user);
 
     # No limit
--- main/users/src/EBox/CloudSync/Slave.pm
+++ main/users/src/EBox/CloudSync/Slave.pm
@@ -42,7 +42,7 @@ sub _addUser
     return if ($user->baseDn() ne $users->usersDn());
 
     # refresh user info to avoid cache problems with passwords:
-    $user = $users->userByUsername($user->name());
+    $user = $users->userByUID($user->name());
 
     my @passwords = map { encode_base64($_) } @{$user->passwordHashes()};
     my $userinfo = {
@@ -69,7 +69,7 @@ sub _modifyUser
     return if ($user->baseDn() ne $users->usersDn());
 
     # refresh user info to avoid cache problems with passwords:
-    $user = $users->userByUsername($user->name());
+    $user = $users->userByUID($user->name());
 
     my @passwords = map { encode_base64($_) } @{$user->passwordHashes()};
     my $userinfo = {
