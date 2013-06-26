0fd24006ee5afb9a7cf692ebb29ddf3d293716d7
Javier Bahillo <jbahillo@zentyal.com>
2013-06-26 13:23:07 +0200

    Removed "" for the warning, as __ does not parse " o ' and lead to a wrong URL

--- main/network/src/EBox/Network/Composite/MultiGw.pm
+++ main/network/src/EBox/Network/Composite/MultiGw.pm
@@ -67,7 +67,7 @@ sub precondition
 
 sub preconditionFailMsg
 {
-    return __('To be able to use this feature you need at least two enabled gateways. You can add them <a href="/Network/View/GatewayTable">here</a> first.');
+    return __('To be able to use this feature you need at least two enabled gateways. You can add them <a href=/Network/View/GatewayTable>here</a> first.');
 }
 
 1;
