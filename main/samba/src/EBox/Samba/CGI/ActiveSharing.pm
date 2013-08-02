b7e8c810f1b9db3d335c04745409ef0eacfee857
Javier Amor García <jamor@zentyal.com>
2013-08-02 19:54:50 +0200

    samba user-addons half fixed

--- main/samba/src/EBox/Samba/CGI/ActiveSharing.pm
+++ main/samba/src/EBox/Samba/CGI/ActiveSharing.pm
@@ -42,22 +42,28 @@ sub _group
     my $smbldap = new EBox::SambaLdapUser;
 
     $self->_requireParam('group', __('group'));
-    my $group = $self->unsafeParam('group');
-    $self->{redirect} = "Users/Group?group=$group";
-    $self->{errorchain} =  "Users/Group";
-
-    $self->keepParam('group');
-
-    $group = new EBox::Users::Group(dn => $group);
+    my $groupDN = $self->unsafeParam('group');
+    my $group = new EBox::Users::Group(dn => $groupDN);
 
     $self->_requireParamAllowEmpty('sharename', __('share name'));
     my $name =  $self->param('sharename');
 
-    if ($self->param('namechange') or $self->param('add')) {
+    my $namechange = $self->param('namechange');
+    if ($namechange or $self->param('add')) {
         $smbldap->setGroupShare($group, $name);
+        if ($namechange) {
+            $self->{json}->{msg} = __('Group share renamed');
+        } else {
+            $self->{json}->{msg} = __('Group share added');
+        }
     } elsif ($self->param('remove')) {
         $smbldap->removeGroupShare($group);
+        $self->{json}->{msg} = __('Group share removed');
+    } else {
+        $self->{json}->{msg} = __('Group share set');
     }
+
+    $self->{json}->{success} = 1;
 }
 
 sub _user
@@ -67,25 +73,25 @@ sub _user
     my $smbldap = new EBox::SambaLdapUser;
 
     $self->_requireParam('user', __('user'));
-    my $user = $self->unsafeParam('user');
-    $self->{redirect} = "Users/User?user=$user";
-    $self->{errorchain} = "Users/User";
+    my $userDN = $self->unsafeParam('user');
 
-    $self->keepParam('user');
-
-    my $zentyalUser = new EBox::Users::User(dn => $user);
+    my $zentyalUser = new EBox::Users::User(dn => $userDN);
     my $sambaUser = new EBox::Samba::User(samAccountName => $zentyalUser->get('uid'));
     my $accountEnabled = $self->param('accountEnabled');
     if ($accountEnabled eq 'yes') {
         $sambaUser->setAccountEnabled(1);
+        $self->{json}->{msg} = __('Samba account enabled');
     } else {
         $sambaUser->setAccountEnabled(0);
+        $self->{json}->{msg} = __('Samba account disabled');
     }
+    $self->{json}->{success} = 1;
 }
 
 sub _process
 {
     my ($self) = @_;
+    $self->{json}->{success} = 0;
 
     if ($self->unsafeParam('user')) {
         $self->_user();
