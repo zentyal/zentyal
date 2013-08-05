2ef1454c1d1f4f629342986dbb7ab1133636bc55
José Antonio Calvo <jacalvo@zentyal.com>
2013-08-05 10:54:25 +0200

    hide Administrator user

--- main/users/src/EBox/Users/Model/Manage.pm
+++ main/users/src/EBox/Users/Model/Manage.pm
@@ -85,6 +85,9 @@ sub childNodes
             $type = 'user';
             $printableName = $child->name();
 
+            # Hide samba admin user
+            next if ($printableName eq 'Administrator');
+
             # FIXME: temporary workaround until the regression is fixed properly
             use Sys::Hostname;
             my $hostname = Sys::Hostname::hostname();
