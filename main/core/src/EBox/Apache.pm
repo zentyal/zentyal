99ebe50ad527e04efa2de8934f577d5400f73be5
José Antonio Calvo <jacalvo@zentyal.com>
2013-08-14 10:48:22 +0200

    Remove unnecessary and problematic desktop services code

--- main/core/src/EBox/Apache.pm
+++ main/core/src/EBox/Apache.pm
@@ -160,7 +160,6 @@ sub _setConf
     $self->_writeHttpdConfFile();
     $self->_writeCSSFiles();
     $self->_reportAdminPort();
-    $self->_setDesktopServicesPort();
     $self->enableRestartOnTrigger();
 }
 
@@ -191,11 +190,6 @@ sub _writeHttpdConfFile
 
     push @confFileParams, ( restrictedResources => $self->get_list('restricted_resources') );
     push @confFileParams, ( includes => $self->_includes(1) );
-
-    my $desktop_services_enabled = EBox::Config::configkey('desktop_services_enabled');
-    my $desktop_services_port = EBox::Config::configkey('desktop_services_port');
-    push @confFileParams, ( desktop_services_enabled => $desktop_services_enabled );
-    push @confFileParams, ( desktop_services_port => $desktop_services_port );
 
     my $debugMode = EBox::Config::boolean('debug');
     push @confFileParams, ( debug => $debugMode);
@@ -293,45 +287,6 @@ sub _reportAdminPort
 sub _httpdConfFile
 {
     return '/var/lib/zentyal/conf/apache2.conf';
-}
-
-sub _setDesktopServicesPort
-{
-    my $desktop_services_port = (EBox::Config::configkey('desktop_services_port') or 6895);
-    checkPort($desktop_services_port, __("Desktop services port"));
-
-    my $fw = EBox::Global->modInstance('firewall');
-    my $services = EBox::Global->modInstance('services');
-    if (defined($fw)) {
-        my $serviceName = 'desktop-services';
-        $fw->addInternalService(
-            'name'              => $serviceName,
-            'printableName'     => __('Desktop Services'),
-            'description'       => __('Desktop Services (API for Zentyal Desktop)'),
-            'protocol'          => 'tcp',
-            'sourcePort'        => 'any',
-            'destinationPort'   => $desktop_services_port,
-        );
-    }
-}
-
-# Method: initialSetup
-#
-# Overrides:
-#   EBox::Module::Base::initialSetup
-#
-sub initialSetup
-{
-    my ($self, $version) = @_;
-
-    # Create default rules and services
-    # only if installing the first time
-    unless ($version) {
-        $self->_setDesktopServicesPort();
-    }
-
-    # Execute initial-setup script
-    $self->SUPER::initialSetup($version);
 }
 
 sub port
