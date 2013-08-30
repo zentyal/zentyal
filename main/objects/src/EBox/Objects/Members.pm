f988d8fdc3a2c0af9e7e27acae87de1d7bef25c1
Enrique J. Hernández Blasco <ejhernandez@zentyal.com>
2013-08-31 01:35:24 +0200

    Minor fixes to Objects doc

--- main/objects/src/EBox/Objects/Members.pm
+++ main/objects/src/EBox/Objects/Members.pm
@@ -47,7 +47,7 @@ sub new
 #
 #       Return the network addresses of all members
 #
-# Parameters:
+# Named parameters:
 #
 #       mask - return also addresses' mask (named optional, default false)
 #
