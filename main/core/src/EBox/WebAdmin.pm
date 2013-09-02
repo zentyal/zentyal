f12340bf35a7ca6e34f325a0b6fdc5801dde0226
Javier Amor García <jamor@zentyal.com>
2013-09-02 17:53:32 +0200

    fix redirects so they are possible in usercorner

--- main/core/src/EBox/WebAdmin.pm
+++ main/core/src/EBox/WebAdmin.pm
@@ -438,13 +438,6 @@ sub updateAdminPortService
         my $services = $global->modInstance('services');
         $services->setAdministrationPort($port);
     }
-
-    my @restartMods = qw(usercorner captiveportal);
-    foreach my $modName (@restartMods) {
-        if ($global->modExists($modName)) {
-            $global->modInstance($modName)->setAsChanged();
-        }
-    }
 }
 
 sub logs
