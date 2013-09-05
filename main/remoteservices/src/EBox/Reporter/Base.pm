472aeaed63e0b6d7e8da8c9f4cc87b4b3e297c56
Enrique J. Hernández Blasco <ejhernandez@zentyal.com>
2013-09-05 11:46:07 +0200

    Added Ntop reporter reading from RRDs file by now

--- main/remoteservices/src/EBox/Reporter/Base.pm
+++ main/remoteservices/src/EBox/Reporter/Base.pm
@@ -13,6 +13,9 @@
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
+use warnings;
+use strict;
+
 package EBox::Reporter::Base;
 
 # Class: EBox::Reporter::Base
@@ -20,9 +23,6 @@ package EBox::Reporter::Base;
 #      Base class to perform the consolidation and send the result to
 #      the cloud
 #
-
-use warnings;
-use strict;
 
 use EBox;
 use EBox::Config;
@@ -202,7 +202,7 @@ sub log
 #
 # Returns:
 #
-#      Int - seconds among logging ops
+#      Int - seconds among <log> operations
 #
 sub logPeriod
 {
