af1d2746f87c6432e7f156b8b004c5e02462d8d9
José Antonio Calvo <jacalvo@zentyal.com>
2013-09-08 19:20:23 +0200

    fixed get of real ip

--- main/users/src/EBox/UsersSync/SOAPMaster.pm
+++ main/users/src/EBox/UsersSync/SOAPMaster.pm
@@ -61,7 +61,7 @@ sub registerSlave
     my ($self, $port, $cert) = @_;
 
     my $req = Apache2::RequestUtil->request();
-    my $host = $req->headers_in()->{'Host'};
+    my $host = $req->headers_in()->{'X-Real-IP'};
 
     my $users = EBox::Global->modInstance('users');
     my $master = $users->masterConf();
