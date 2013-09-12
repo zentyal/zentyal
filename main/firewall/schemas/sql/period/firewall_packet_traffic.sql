c3a4b8b6aba4ee0cf2bee7067e7a0f32d4634681
Enrique J. Hernández Blasco <ejhernandez@zentyal.com>
2013-09-12 14:37:37 +0200

    Use DATETIME type in date column in consolidation tables

--- main/firewall/schemas/sql/period/firewall_packet_traffic.sql
+++ main/firewall/schemas/sql/period/firewall_packet_traffic.sql
@@ -1,5 +1,5 @@
 CREATE TABLE IF NOT EXISTS firewall_packet_traffic(
-        `date` TIMESTAMP NOT NULL,
+        `date` DATETIME NOT NULL,
         `drop` BIGINT DEFAULT 0,
         INDEX(`date`)
 ) ENGINE = MyISAM;
--- main/squid/schemas/sql/period/squid_traffic.sql
+++ main/squid/schemas/sql/period/squid_traffic.sql
@@ -1,5 +1,5 @@
 CREATE TABLE IF NOT EXISTS squid_traffic (
-        `date` TIMESTAMP,
+        `date` DATETIME,
 
         rfc931 CHAR(255) DEFAULT '-',
 
