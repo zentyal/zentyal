916d3a6361ddf8e90a24571c96562b169d56a177
Javier Amor García <jamor@zentyal.com>
2013-10-22 18:04:54 +0200

    transfered changes from cloud-user-sync-fixes because that branch was made form master

--- main/users/src/EBox/Users.pm
+++ main/users/src/EBox/Users.pm
@@ -1945,8 +1945,7 @@ sub slaves
 sub master
 {
     my ($self) = @_;
-    my $row = $self->model('Master')->row();
-    return $row->elementByName('master')->value();
+    return $self->model('Master')->master();
 }
 
 # SyncProvider implementation
