54340d37430760cc36713fdd58f03dc45d7db3f5
Miguel Julián <mjulian@zentyal.com>
2013-11-13 18:26:04 +0100

    Fixed a Save Changes issue
     * outgoingDomain field was not found. Not it is shown but with hidden=>1

--- main/openchange/src/EBox/OpenChange/Model/Provision.pm
+++ main/openchange/src/EBox/OpenChange/Model/Provision.pm
@@ -86,13 +86,6 @@ sub _table
                     editable      => 1),
             ])
         );
-        push (@tableDesc, new EBox::Types::Select(
-            fieldName     => 'outgoingDomain',
-            printableName => __('Outgoing Mail Domain'),
-            foreignModel  => $self->modelGetter('mail', 'VDomains'),
-            foreignField  => 'vdomain',
-            editable      => 1)
-        );
         push (@tableDesc, new EBox::Types::Boolean(
             fieldName     => 'enableUsers',
             printableName => __('Enable all users after provision'),
@@ -107,6 +100,15 @@ sub _table
 #            editable      => 1)
 #        );
     }
+
+    push (@tableDesc, new EBox::Types::Select(
+        fieldName     => 'outgoingDomain',
+        printableName => __('Outgoing Mail Domain'),
+        foreignModel  => $self->modelGetter('mail', 'VDomains'),
+        foreignField  => 'vdomain',
+        editable      => ($self->parentModule->isProvisioned() ? 0 : 1),
+        hidden        => ($self->parentModule->isProvisioned() ? 1 : 0))
+    );
 
     my $customActions = [
 #        new EBox::Types::MultiStateAction(
