a5d7bffcafe85dca363046a9a2f381389b8b0ab4
Miguel Julián <mjulian@zentyal.com>
2013-11-13 11:34:13 +0100

    Outgoing sender address could be now be rewritten

--- main/openchange/src/EBox/OpenChange.pm
+++ main/openchange/src/EBox/OpenChange.pm
@@ -37,6 +37,8 @@ use constant SOGO_CONF_FILE => '/etc/sogo/sogo.conf';
 use constant SOGO_PID_FILE => '/var/run/sogo/sogo.pid';
 use constant SOGO_LOG_FILE => '/var/log/sogo/sogo.log';
 
+use constant REWRITE_POLICY_FILE => '/etc/postfix/generic';
+
 
 # Method: _create
 #
@@ -160,6 +162,7 @@ sub _setConf
     $self->_writeSOGoDefaultFile();
     $self->_writeSOGoConfFile();
     $self->_setupSOGoDatabase();
+    $self->_writeRewritePolicy();
 }
 
 sub _writeSOGoDefaultFile
@@ -230,6 +233,29 @@ sub _writeSOGoConfFile
     $self->writeConfFile(SOGO_CONF_FILE,
         'openchange/sogo.conf.mas',
         $array, { uid => 0, gid => $gid, mode => '640' });
+}
+
+sub _writeRewritePolicy
+{
+    my ($self) = @_;
+
+    my $sysinfo = $self->global()->modInstance('sysinfo');
+    my $defaultDomain = $sysinfo->hostDomain();
+
+    my $rewriteDomain = $self->model('Provision')->row()->printableValueByName('outgoingDomain');
+
+    my @rewriteParams;
+    push @rewriteParams, ('defaultDomain' => $defaultDomain);
+    push @rewriteParams, ('rewriteDomain' => $rewriteDomain);
+
+
+use  Data
+
+    $self->writeConfFile(REWRITE_POLICY_FILE,
+        'openchange/rewriteDomainPolicy.mas',
+        \@rewriteParams, { uid => 0, gid => 0, mode => '644' });
+
+    EBox::Sudo::root('/usr/sbin/postmap ' . REWRITE_POLICY_FILE);
 }
 
 # Method: menu
