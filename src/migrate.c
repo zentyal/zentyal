9a36ebf1fbd289d17e88e13e7cad1207c25f38fb
Carlos Perelló Marín <cperello@zentyal.com>
2013-11-20 19:13:57 +0100

    changed tabs with 4 spaces

--- src/migrate.c
+++ src/migrate.c
@@ -23,166 +23,166 @@
 
 enum
 {
-	OPT_DEBUG = 1000,
-	OPT_DUMPDATA
+    OPT_DEBUG = 1000,
+    OPT_DUMPDATA
 };
 
 static struct poptOption long_options[] =
 {
-	POPT_AUTOHELP
-	{ "debuglevel", 'd', POPT_ARG_STRING, NULL, OPT_DEBUG,
-		"set the debug level", NULL },
-	{"dump-data", 0, POPT_ARG_NONE, NULL, OPT_DUMPDATA,
-		"dump the hexadecimal and NDR data", NULL },
-	{NULL, 0, 0, NULL, 0, NULL, NULL},
+    POPT_AUTOHELP
+    { "debuglevel", 'd', POPT_ARG_STRING, NULL, OPT_DEBUG,
+        "set the debug level", NULL },
+    {"dump-data", 0, POPT_ARG_NONE, NULL, OPT_DUMPDATA,
+        "dump the hexadecimal and NDR data", NULL },
+    {NULL, 0, 0, NULL, 0, NULL, NULL},
 };
 
 void set_debug_level(TALLOC_CTX *mem_ctx, struct status *status, char *opt)
 {
-	char *opttmp = NULL;
-	char *endptr = NULL;
-	char *debuglevel;
-	int val = 0;
-	struct loadparm_context *lp_ctx;
+    char *opttmp = NULL;
+    char *endptr = NULL;
+    char *debuglevel;
+    int val = 0;
+    struct loadparm_context *lp_ctx;
 
-	opttmp = talloc_strdup(mem_ctx, opt);
-	val = strtol(opttmp, &endptr, 10);
-	if ((errno == ERANGE && (val == LONG_MAX || val == LONG_MIN)) ||
-		(errno != 0 && val == 0))
-	{
-		fprintf(stderr, "[!] Error parsing debug option\n");
-	}
-	if (endptr == opttmp) {
-		fprintf(stderr, "[!] Error parsing debug option\n");
-	}
-	talloc_free(opttmp);
+    opttmp = talloc_strdup(mem_ctx, opt);
+    val = strtol(opttmp, &endptr, 10);
+    if ((errno == ERANGE && (val == LONG_MAX || val == LONG_MIN)) ||
+        (errno != 0 && val == 0))
+    {
+        fprintf(stderr, "[!] Error parsing debug option\n");
+    }
+    if (endptr == opttmp) {
+        fprintf(stderr, "[!] Error parsing debug option\n");
+    }
+    talloc_free(opttmp);
 
-	lp_ctx = loadparm_init_global(true);
-	debuglevel = talloc_asprintf(mem_ctx, "%u", val);
-	lpcfg_set_cmdline(lp_ctx, "log level", debuglevel);
-	talloc_free(debuglevel);
+    lp_ctx = loadparm_init_global(true);
+    debuglevel = talloc_asprintf(mem_ctx, "%u", val);
+    lpcfg_set_cmdline(lp_ctx, "log level", debuglevel);
+    talloc_free(debuglevel);
 
-	status->local.debug_level = val;
-	status->remote.debug_level = val;
+    status->local.debug_level = val;
+    status->remote.debug_level = val;
 }
 
 int main(int argc, const char *argv[])
 {
-	TALLOC_CTX      *mem_ctx = NULL;
-	struct status   *status  = NULL;
-	struct pidfh    *pfh = NULL;;
-	int             opt = 0;
-	int 		ret = 0;
-	poptContext     pc;
-	pid_t           pid;
+    TALLOC_CTX      *mem_ctx = NULL;
+    struct status   *status  = NULL;
+    struct pidfh    *pfh = NULL;;
+    int             opt = 0;
+    int         ret = 0;
+    poptContext     pc;
+    pid_t           pid;
 
-	/* Initialize status structure */
-	mem_ctx = talloc_named(NULL, 0, "migrate");
-	if (mem_ctx == NULL) {
-		fprintf(stderr, "[!] Not enough memory\n");
-		exit(EXIT_FAILURE);
-	}
-	status = talloc_zero(mem_ctx, struct status);
-	if (!status) {
-		goto fail;
-	}
-	status->state = STATE_IDLE;
-	status->mem_ctx = mem_ctx;
+    /* Initialize status structure */
+    mem_ctx = talloc_named(NULL, 0, "migrate");
+    if (mem_ctx == NULL) {
+        fprintf(stderr, "[!] Not enough memory\n");
+        exit(EXIT_FAILURE);
+    }
+    status = talloc_zero(mem_ctx, struct status);
+    if (!status) {
+        goto fail;
+    }
+    status->state = STATE_IDLE;
+    status->mem_ctx = mem_ctx;
 
-	ret = pthread_spin_init(&status->lock, PTHREAD_PROCESS_PRIVATE);
-	if (ret) {
-		fprintf(stderr, "[!] pthread_spin_init: %s\n", strerror(ret));
-		goto fail;
-	}
+    ret = pthread_spin_init(&status->lock, PTHREAD_PROCESS_PRIVATE);
+    if (ret) {
+        fprintf(stderr, "[!] pthread_spin_init: %s\n", strerror(ret));
+        goto fail;
+    }
 
-	/* Parse command line options */
-	pc = poptGetContext("migrate", argc, argv, long_options, 0);
-	while ((opt = poptGetNextOpt(pc)) != -1) {
-		switch (opt) {
-			case OPT_DUMPDATA:
-				status->local.dumpdata = true;
-				status->remote.dumpdata = true;
-				break;
-			case OPT_DEBUG:
-				set_debug_level(status->mem_ctx,
-					status, poptGetOptArg(pc));
-				break;
-			default:
-				fprintf(stderr, "[!] Non-existent option\n");
-				goto fail;
-		}
-	}
+    /* Parse command line options */
+    pc = poptGetContext("migrate", argc, argv, long_options, 0);
+    while ((opt = poptGetNextOpt(pc)) != -1) {
+        switch (opt) {
+            case OPT_DUMPDATA:
+                status->local.dumpdata = true;
+                status->remote.dumpdata = true;
+                break;
+            case OPT_DEBUG:
+                set_debug_level(status->mem_ctx,
+                    status, poptGetOptArg(pc));
+                break;
+            default:
+                fprintf(stderr, "[!] Non-existent option\n");
+                goto fail;
+        }
+    }
 
-	/* Sanity check on options */
-	if (!status->opt_profdb) {
-		status->opt_profdb = talloc_asprintf(mem_ctx,
-				DEFAULT_PROFDB, getenv("HOME"));
-	}
+    /* Sanity check on options */
+    if (!status->opt_profdb) {
+        status->opt_profdb = talloc_asprintf(mem_ctx,
+                DEFAULT_PROFDB, getenv("HOME"));
+    }
 
-	/* Open PID file, exit if file locked (already running) */
-	pfh = pidfile_open(NULL, 0600, &pid);
-	if (pfh == NULL) {
-		if (errno == EEXIST) {
-			fprintf(stderr,
-				"[!] Daemon already running, pid: %jd\n",
-				(intmax_t)pid);
-		} else {
-			fprintf(stderr,
-				"[!] Cannot open or create pidfile: %s\n",
-				strerror(errno));
-		}
-		goto fail;
-	}
+    /* Open PID file, exit if file locked (already running) */
+    pfh = pidfile_open(NULL, 0600, &pid);
+    if (pfh == NULL) {
+        if (errno == EEXIST) {
+            fprintf(stderr,
+                "[!] Daemon already running, pid: %jd\n",
+                (intmax_t)pid);
+        } else {
+            fprintf(stderr,
+                "[!] Cannot open or create pidfile: %s\n",
+                strerror(errno));
+        }
+        goto fail;
+    }
 
-	/* Daemonize if no debug */
-	if (!status->local.debug_level && !status->remote.debug_level) {
-		if (daemon(0, 0) == -1) {
-			fprintf(stderr, "[!] Cannot daemonize: %s\n",
-				strerror(errno));
-			goto fail;
-		}
-	}
+    /* Daemonize if no debug */
+    if (!status->local.debug_level && !status->remote.debug_level) {
+        if (daemon(0, 0) == -1) {
+            fprintf(stderr, "[!] Cannot daemonize: %s\n",
+                strerror(errno));
+            goto fail;
+        }
+    }
 
-	/* This is child code. Write PID on file */
-	pidfile_write(pfh);
+    /* This is child code. Write PID on file */
+    pidfile_write(pfh);
 
-	/* Init RPC communications */
-	if (!rpc_open(status)) {
-		goto fail;
-	}
+    /* Init RPC communications */
+    if (!rpc_open(status)) {
+        goto fail;
+    }
 
-	/* Init the control loop */
-	status->rpc_run = true;
-	rpc_run(status);
+    /* Init the control loop */
+    status->rpc_run = true;
+    rpc_run(status);
 
-	/* Cleanup */
-	rpc_close(status);
+    /* Cleanup */
+    rpc_close(status);
 
-	ret = pthread_spin_destroy(&status->lock);
-	if (ret) {
-		fprintf(stderr, "[!] pthread_spin_destroy: %s\n", strerror(ret));
-	}
-	talloc_free(status);
-	talloc_free(mem_ctx);
-	poptFreeContext(pc);
+    ret = pthread_spin_destroy(&status->lock);
+    if (ret) {
+        fprintf(stderr, "[!] pthread_spin_destroy: %s\n", strerror(ret));
+    }
+    talloc_free(status);
+    talloc_free(mem_ctx);
+    poptFreeContext(pc);
 
-	/* Close and remove PID file */
-	pidfile_remove(pfh);
+    /* Close and remove PID file */
+    pidfile_remove(pfh);
 
-	exit(EXIT_SUCCESS);
+    exit(EXIT_SUCCESS);
 fail:
-	if (status) {
-		rpc_close(status);
-		talloc_free(status);
-	}
-	if (mem_ctx) {
-		talloc_free(mem_ctx);
-	}
-	if (pc) {
-		poptFreeContext(pc);
-	}
-	if (pfh) {
-		pidfile_remove(pfh);
-	}
-	exit(EXIT_FAILURE);
+    if (status) {
+        rpc_close(status);
+        talloc_free(status);
+    }
+    if (mem_ctx) {
+        talloc_free(mem_ctx);
+    }
+    if (pc) {
+        poptFreeContext(pc);
+    }
+    if (pfh) {
+        pidfile_remove(pfh);
+    }
+    exit(EXIT_FAILURE);
 }
--- src/migrate.h
+++ src/migrate.h
@@ -43,16 +43,16 @@
 #include <libmapi/libmapi.h>
 #include <libocpf/ocpf.h>
 
-#define	DEFAULT_PROFDB		"%s/.openchange/profiles.ldb"
+#define DEFAULT_PROFDB      "%s/.openchange/profiles.ldb"
 #define DEFAULT_EXPORT_PATH     "/var/tmp/openchange-migrate"
-#define	TDB_SYSFOLDER		"systemfolder.tdb"
-#define	TDB_FOLDERMAP		"foldermap.tdb"
+#define TDB_SYSFOLDER       "systemfolder.tdb"
+#define TDB_FOLDERMAP       "foldermap.tdb"
 
 
 #ifndef __BEGIN_DECLS
 #ifdef __cplusplus
-#define __BEGIN_DECLS		extern "C" {
-#define __END_DECLS		}
+#define __BEGIN_DECLS       extern "C" {
+#define __END_DECLS     }
 #else
 #define __BEGIN_DECLS
 #define __END_DECLS
@@ -60,149 +60,149 @@
 #endif
 
 struct mbox_tree_item {
-	struct mbox_tree_item	*parent;
-	struct mbox_tree_item	*children;
-	struct mbox_tree_item	*next;
-	struct mbox_tree_item	*prev;
-	mapi_id_t		id;
+    struct mbox_tree_item   *parent;
+    struct mbox_tree_item   *children;
+    struct mbox_tree_item   *next;
+    struct mbox_tree_item   *prev;
+    mapi_id_t       id;
 };
 
 struct mbox_counters {
-	uint64_t		total_folders;
-	uint64_t		exported_total_folders;
-	uint64_t		imported_total_folders;
+    uint64_t        total_folders;
+    uint64_t        exported_total_folders;
+    uint64_t        imported_total_folders;
 
-	uint64_t		total_items;
-	uint64_t		exported_total_items;
-	uint64_t		imported_total_items;
+    uint64_t        total_items;
+    uint64_t        exported_total_items;
+    uint64_t        imported_total_items;
 
-	uint64_t		total_bytes;
-	uint64_t		exported_total_bytes;
-	uint64_t		imported_total_bytes;
+    uint64_t        total_bytes;
+    uint64_t        exported_total_bytes;
+    uint64_t        imported_total_bytes;
 
-	uint64_t		email_items;
-	uint64_t		exported_email_items;
-	uint64_t		imported_email_items;
+    uint64_t        email_items;
+    uint64_t        exported_email_items;
+    uint64_t        imported_email_items;
 
-	uint64_t		email_bytes;
-	uint64_t		exported_email_bytes;
-	uint64_t		imported_email_bytes;
+    uint64_t        email_bytes;
+    uint64_t        exported_email_bytes;
+    uint64_t        imported_email_bytes;
 
-	uint64_t		attachment_items;
-	uint64_t		exported_attachment_items;
-	uint64_t		imported_attachment_items;
+    uint64_t        attachment_items;
+    uint64_t        exported_attachment_items;
+    uint64_t        imported_attachment_items;
 
-	uint64_t		attachment_bytes;
-	uint64_t		exported_attachment_bytes;
-	uint64_t		imported_attachment_bytes;
+    uint64_t        attachment_bytes;
+    uint64_t        exported_attachment_bytes;
+    uint64_t        imported_attachment_bytes;
 
-	uint64_t		note_items;
-	uint64_t		exported_note_items;
-	uint64_t		imported_note_items;
+    uint64_t        note_items;
+    uint64_t        exported_note_items;
+    uint64_t        imported_note_items;
 
-	uint64_t		note_bytes;
-	uint64_t		exported_note_bytes;
-	uint64_t		imported_note_bytes;
+    uint64_t        note_bytes;
+    uint64_t        exported_note_bytes;
+    uint64_t        imported_note_bytes;
 
-	uint64_t		appointment_items;
-	uint64_t		exported_appointment_items;
-	uint64_t		imported_appointment_items;
+    uint64_t        appointment_items;
+    uint64_t        exported_appointment_items;
+    uint64_t        imported_appointment_items;
 
-	uint64_t		appointment_bytes;
-	uint64_t		exported_appointment_bytes;
-	uint64_t		imported_appointment_bytes;
+    uint64_t        appointment_bytes;
+    uint64_t        exported_appointment_bytes;
+    uint64_t        imported_appointment_bytes;
 
-	uint64_t		task_items;
-	uint64_t		exported_task_items;
-	uint64_t		imported_task_items;
+    uint64_t        task_items;
+    uint64_t        exported_task_items;
+    uint64_t        imported_task_items;
 
-	uint64_t		task_bytes;
-	uint64_t		imported_task_bytes;
-	uint64_t		exported_task_bytes;
+    uint64_t        task_bytes;
+    uint64_t        imported_task_bytes;
+    uint64_t        exported_task_bytes;
 
-	uint64_t		contact_items;
-	uint64_t		imported_contact_items;
-	uint64_t		exported_contact_items;
+    uint64_t        contact_items;
+    uint64_t        imported_contact_items;
+    uint64_t        exported_contact_items;
 
-	uint64_t		contact_bytes;
-	uint64_t		imported_contact_bytes;
-	uint64_t		exported_contact_bytes;
+    uint64_t        contact_bytes;
+    uint64_t        imported_contact_bytes;
+    uint64_t        exported_contact_bytes;
 
-	uint64_t		journal_items;
-	uint64_t		imported_journal_items;
-	uint64_t		exported_journal_items;
+    uint64_t        journal_items;
+    uint64_t        imported_journal_items;
+    uint64_t        exported_journal_items;
 
-	uint64_t		journal_bytes;
-	uint64_t		imported_journal_bytes;
-	uint64_t		exported_journal_bytes;
+    uint64_t        journal_bytes;
+    uint64_t        imported_journal_bytes;
+    uint64_t        exported_journal_bytes;
 };
 
 struct mbox_data {
-	const char		*username;
-	time_t			start_time;
-	time_t			end_time;
-	struct mbox_counters	counters;
-	struct mbox_tree_item	*tree_root;
-	struct tdb_context	*tdb_sysfolder;
-	struct tdb_context	*tdb_foldermap;
+    const char      *username;
+    time_t          start_time;
+    time_t          end_time;
+    struct mbox_counters    counters;
+    struct mbox_tree_item   *tree_root;
+    struct tdb_context  *tdb_sysfolder;
+    struct tdb_context  *tdb_foldermap;
 };
 
 enum state {
-	STATE_IDLE		= 0,
-	STATE_ESTIMATING	= 1,
-	STATE_ESTIMATED		= 2,
-	STATE_EXPORTING		= 3,
-	STATE_EXPORTED		= 4,
-	STATE_IMPORTING		= 5,
-	STATE_IMPORTED		= 6
+    STATE_IDLE      = 0,
+    STATE_ESTIMATING    = 1,
+    STATE_ESTIMATED     = 2,
+    STATE_EXPORTING     = 3,
+    STATE_EXPORTED      = 4,
+    STATE_IMPORTING     = 5,
+    STATE_IMPORTED      = 6
 };
 
 struct connection
 {
-	struct mapi_context	*mapi_ctx;
-	struct mapi_session	*session;
-	char 	*server;
-	char 	*error;
-	bool	dumpdata;
-	int	debug_level;
+    struct mapi_context *mapi_ctx;
+    struct mapi_session *session;
+    char    *server;
+    char    *error;
+    bool    dumpdata;
+    int debug_level;
 };
 
 struct status
 {
-	pthread_spinlock_t	lock;		/* Structure lock */
-	enum state		state;		/* Internal state */
-	TALLOC_CTX		*mem_ctx;	/* Application memory ctx */
-	char			*opt_profdb;	/* Profile DB */
-	amqp_connection_state_t	conn;		/* AMQP connection */
-	bool			rpc_run;	/* RPC run loop flag */
-	struct json_tokener	*tokener;	/* JSON parser */
-	pthread_t		thread_id;	/* Worker thread id */
-	time_t			start_time;
-	time_t			end_time;
-	struct array_list	*mbox_list;
-	struct connection	remote;		/* Remote server connection */
-	struct connection	local;		/* Local server connection */
+    pthread_spinlock_t  lock;       /* Structure lock */
+    enum state      state;      /* Internal state */
+    TALLOC_CTX      *mem_ctx;   /* Application memory ctx */
+    char            *opt_profdb;    /* Profile DB */
+    amqp_connection_state_t conn;       /* AMQP connection */
+    bool            rpc_run;    /* RPC run loop flag */
+    struct json_tokener *tokener;   /* JSON parser */
+    pthread_t       thread_id;  /* Worker thread id */
+    time_t          start_time;
+    time_t          end_time;
+    struct array_list   *mbox_list;
+    struct connection   remote;     /* Remote server connection */
+    struct connection   local;      /* Local server connection */
 };
 
 __BEGIN_DECLS
 
 /* definitions from estimate.c */
-void		estimate_data_free(void *);
-void		*estimate_start_thread(void *);
-void		*export_start_thread(void *);
-void		*import_start_thread(void *);
+void        estimate_data_free(void *);
+void        *estimate_start_thread(void *);
+void        *export_start_thread(void *);
+void        *import_start_thread(void *);
 
 /* definitions from rpc.c */
-bool		rpc_open(struct status *);
-void		rpc_close(struct status *);
-void		rpc_run(struct status *);
+bool        rpc_open(struct status *);
+void        rpc_close(struct status *);
+void        rpc_run(struct status *);
 
 /* definitions from control.c */
-bool		control_init(struct status *);
-void		control_free(struct status *);
-void		control_abort(struct status *);
-json_object	*control_handle_status(struct status *, json_object *);
-amqp_bytes_t	control_handle(struct status *, amqp_bytes_t);
+bool        control_init(struct status *);
+void        control_free(struct status *);
+void        control_abort(struct status *);
+json_object *control_handle_status(struct status *, json_object *);
+amqp_bytes_t    control_handle(struct status *, amqp_bytes_t);
 
 __END_DECLS
 
