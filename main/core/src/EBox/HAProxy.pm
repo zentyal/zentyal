e070a59802c381fe2010447c7489a0c639bea05b
Carlos Perelló Marín <cperello@zentyal.com>
2014-01-27 01:15:34 +0100

    Used the generic service launch script wrapper

--- main/core/src/EBox/HAProxy.pm
+++ main/core/src/EBox/HAProxy.pm
@@ -243,8 +243,7 @@ sub _enforceServiceState
 {
     my ($self) = @_;
 
-    my $script = $self->INITDPATH() . 'haproxy restart';
-    EBox::Sudo::root($script);
+    EBox::Sudo::root('service haproxy restart');
 }
 
 # Method: modsWithHAProxyService
