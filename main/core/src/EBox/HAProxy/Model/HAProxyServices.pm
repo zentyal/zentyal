3882d95cf6259acb407c5fbcc2e5278d5170fce5
Carlos Perelló Marín <cperello@zentyal.com>
2014-02-12 04:31:32 +0100

    Added a way for HAProxy to retrieve the CA certificate entry that should be used

--- main/core/src/EBox/HAProxy/Model/HAProxyServices.pm
+++ main/core/src/EBox/HAProxy/Model/HAProxyServices.pm
@@ -371,11 +371,25 @@ sub validateTypedRow
                 if (EBox::Global->modExists('ca')) {
                     my $ca = EBox::Global->modInstance('ca');
                     my $certificates = $ca->model('Certificates');
-                    unless ($certificates->isEnabledService($module->printableName())) {
-                        throw EBox::Exceptions::External(__x(
-                            'You need to enable {module} on {ohref}Services Certificates{chref} to enable SSL for it.',
-                            module => $moduleName, ohref => '<a href="/CA/View/Certificates">', chref => '</a>'
-                        ));
+                    unless ($certificates->isEnabledService($module->caServiceIdForHAProxy())) {
+                        my $errorMsg = __x(
+                            'You need to enable the certificate for {module} on {ohref}Services Certificates{chref}',
+                            service => $module->displayName(), ohref => '<a href="/CA/View/Certificates">',
+                            chref => '</a>'
+                        );
+                        foreach my $certificate (@{$module->certificates}) {
+                            if ($certificate->{serviceId} eq $module->caServiceIdForHAProxy()) {
+                                my $serviceName = $certificate->{service};
+                                $errorMsg = __x(
+                                    'You need to enable the {serviceName} certificate for {module} on '.
+                                    '{ohref}Services Certificates{chref}',
+                                    serviceName => $serviceName, service => $module->displayName(),
+                                    ohref => '<a href="/CA/View/Certificates">', chref => '</a>'
+                                );
+                                last;
+                            }
+                        }
+                        throw EBox::Exceptions::External($errorMsg);
                     }
                 } else {
                     throw EBox::Exceptions::External(__x(
