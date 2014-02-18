c93261240993f2fce9e41db6b9ffe8719476f945
José Antonio Calvo <jacalvo@zentyal.com>
2014-02-18 18:44:08 +0100

    enable debug and crashreports by default
    also fixed paths according to the new samba packaging and remove unnecessary workaround for apparmor

--- main/core/src/EBox/SysInfo/CGI/CrashReport.pm
+++ main/core/src/EBox/SysInfo/CGI/CrashReport.pm
@@ -36,13 +36,13 @@ sub _process
     # FIXME: unhardcode samba if more daemon crashes are watched
 
     if ($action eq 'report') {
-        my @files = @{EBox::Sudo::root("ls $CRASH_DIR | grep ^_opt_samba4")};
+        my @files = @{EBox::Sudo::root("ls $CRASH_DIR | grep ^_usr_sbin_samba")};
         foreach my $file (@files) {
             EBox::info("Sending crash report: $file");
             EBox::Sudo::root("/usr/share/zentyal/crash-report $CRASH_DIR/$file");
         }
     } elsif ($action eq 'discard') {
-        EBox::Sudo::root('rm -f /var/crash/_opt_samba4_*');
+        EBox::Sudo::root('rm -f /var/crash/_usr_sbin_samba*');
     }
 }
 
