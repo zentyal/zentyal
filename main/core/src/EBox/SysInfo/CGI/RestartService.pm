c3badc33eaaea5f2064072993ca33a63a06ad126
Enrique J. Hernández Blasco <ejhernandez@zentyal.com>
2014-02-26 23:07:38 +0100

    Minor fix in the changelog + up copyright year

--- main/core/src/EBox/SysInfo/CGI/RestartService.pm
+++ main/core/src/EBox/SysInfo/CGI/RestartService.pm
@@ -1,4 +1,4 @@
-# Copyright (C) 2008-2013 Zentyal S.L.
+# Copyright (C) 2008-2014 Zentyal S.L.
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License, version 2, as
