700f7ffe480037e59ab18bd87d5a535d175fad46
Javier Amor García <jamor@zentyal.com>
2014-03-04 08:58:46 +0100

    check also couriermailias to see if a address is not in use

--- main/users/src/EBox/Users.pm
+++ main/users/src/EBox/Users.pm
@@ -2589,7 +2589,7 @@ sub checkMailNotInUse
     my $usersMod = $self->global()->modInstance('users');
     my %searchParams = (
         base => $usersMod->ldap()->dn(),
-        filter => "&(|(objectclass=couriermailaccount)(objectclass=zentyalDistributionGroup))(mail=$addr)",
+        filter => "&(|(objectclass=couriermailaccount)(objectclass=couriermailalias)(objectclass=zentyalDistributionGroup))(mail=$addr)",
         scope => 'sub'
     );
 
