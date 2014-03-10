e362ebdc9519d8aff8a40a8f372b2f1ac6d5d74f
Samuel Cabrero <scabrero@zentyal.com>
2014-03-10 12:28:52 +0100

    Add debug trace

--- main/core/src/EBox/CGI/ClientRawBase.pm
+++ main/core/src/EBox/CGI/ClientRawBase.pm
@@ -196,12 +196,14 @@ sub run
                     "a bug, relevant information can ".
                     "be found in the logs.");
             $self->_print_error($error);
+            EBox::trace();
         } else {
             if ($ex->can('text')) {
                 $logger->error('Exception: ' . $ex->text());
             } else {
                 $logger->error("Unknown exception");
             }
+            EBox::trace();
 
             throw $ex;
         }
