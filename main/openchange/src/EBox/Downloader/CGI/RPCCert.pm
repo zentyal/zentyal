04a82d00a4695303053ce7c35335bbe536ed70d7
Javier Amor García <jamor@zentyal.com>
2014-03-13 19:12:11 +0100

    added back downlaoder cgi

---
+++ main/openchange/src/EBox/Downloader/CGI/RPCCert.pm
@@ -0,0 +1,50 @@
+# Copyright (C) 2013 Zentyal S.L.
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License, version 2, as
+# published by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+use strict;
+use warnings;
+
+package EBox::Downloader::CGI::RPCCert;
+use base 'EBox::Downloader::CGI::Base';
+
+use EBox::Global;
+use EBox::Config;
+
+sub new
+{
+    my ($class, %params) = @_;
+    my $self = $class->SUPER::new(@_);
+    bless($self, $class);
+    return  $self;
+}
+
+sub _path
+{
+    return EBox::Config::downloads() . 'rpcproxy.crt';
+}
+
+sub _process
+{
+    my ($self) = @_;
+
+    my $file = $self->_path();
+    if (not -r $file) {
+        my $openchange = EBox::Global->modInstance('openchange');
+        $openchange->_createRPCProxyCertificate();
+    }
+    $self->SUPER::_process();
+}
+
+1;
