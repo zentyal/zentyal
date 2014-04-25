1a9db8ef7092fcac10bef87dc83ff50bd3a8212c
Julio García <jjgarcia@zentyal.com>
2014-04-25 11:46:33 +0200

    Adding title to the different sections

--- extra/scripts/run-check-and-tests-in-docker.sh
+++ extra/scripts/run-check-and-tests-in-docker.sh
@@ -8,7 +8,9 @@ docker build -t $CONTAINER_NAME $ZENTYAL_FOLDER/extra/scripts/.
 
 rm -rf $ZENTYAL_FOLDER/test
 
+echo " == RUNNING SYNTAX CHECK == "
 docker run -v $ZENTYAL_FOLDER:/zentyal-repo:rw --rm $CONTAINER_NAME /zentyal-repo/extra/scripts/zentyal-syntax-check --update --path=/zentyal-repo --release=precise
 
+echo " == RUNNING UNIT TESTS == "
 docker run -u="testUser" -v $ZENTYAL_FOLDER:/zentyal-repo:rw --rm $CONTAINER_NAME /zentyal-repo/extra/scripts/zentyal-unit-tests -js common > $ZENTYAL_FOLDER/test_results.xml
 
