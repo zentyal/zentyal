ff6e51e42edac932bf854c52816c181e6b446872
Julio García <jjgarcia@zentyal.com>
2014-04-25 11:11:37 +0200

    Changing archive country, removing warning and make xml results clean

--- extra/scripts/run-check-and-tests-in-docker.sh
+++ extra/scripts/run-check-and-tests-in-docker.sh
@@ -8,7 +8,7 @@ docker build -t $CONTAINER_NAME $ZENTYAL_FOLDER/extra/scripts/.
 
 rm -rf $ZENTYAL_FOLDER/test
 
-docker run -v $ZENTYAL_FOLDER:/zentyal-repo:rw $CONTAINER_NAME /zentyal-repo/extra/scripts/zentyal-syntax-check --update --path=/zentyal-repo --release=precise
+docker run -v $ZENTYAL_FOLDER:/zentyal-repo:rw --rm $CONTAINER_NAME /zentyal-repo/extra/scripts/zentyal-syntax-check --update --path=/zentyal-repo --release=precise
 
-docker run -u="testUser" -v $ZENTYAL_FOLDER:/zentyal-repo:rw $CONTAINER_NAME /zentyal-repo/extra/scripts/zentyal-unit-tests -js common > $ZENTYAL_FOLDER/test_results.xml
+docker run -u="testUser" -v $ZENTYAL_FOLDER:/zentyal-repo:rw --rm $CONTAINER_NAME /zentyal-repo/extra/scripts/zentyal-unit-tests -js common > $ZENTYAL_FOLDER/test_results.xml
 
