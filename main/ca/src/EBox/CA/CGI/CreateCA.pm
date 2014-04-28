756473670e4d4ed742a883eff07d828a639a5feb
Javier Amor García <jamor@zentyal.com>
2014-04-28 20:01:25 +0200

    better CN for CA certificate

--- main/ca/src/EBox/CA/CGI/CreateCA.pm
+++ main/ca/src/EBox/CA/CGI/CreateCA.pm
@@ -120,8 +120,10 @@ sub actuate
                                              days => $days));
     }
 
-    my $retVal = $ca->createCA( orgName       => $orgName,
-                                countryName   => $countryName,
+    my $commonName = __x('{org} Authority Certificate', org => $orgName);
+    my $retVal = $ca->createCA( commonName    => $commonName,
+                                orgName       => $orgName,
+                                acountryName   => $countryName,
                                 localityName  => $localityName,
                                 stateName     => $stateName,
                                 days          => $days,
