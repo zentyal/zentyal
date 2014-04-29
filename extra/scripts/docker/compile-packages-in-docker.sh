5699cc4d08d2c1f17eac132b7af4c0344fbe5690
Julio García <jjgarcia@zentyal.com>
2014-04-29 15:30:52 +0200

    Improvements done to the Docker Image and the scripts

--- extra/scripts/docker/compile-packages-in-docker.sh
+++ extra/scripts/docker/compile-packages-in-docker.sh
@@ -5,7 +5,8 @@ ZENTYAL_FOLDER=$1
 USER_FOLDER=$HOME
 CONTAINER_NAME="zentyal32"
 
-docker build -t $CONTAINER_NAME $ZENTYAL_FOLDER/extra/scripts/docker/.
+echo " == BUILDING DOCKER IMAGE == "
+$ZENTYAL_FOLDER/extra/scripts/docker/build-docker-image.sh $ZENTYAL_FOLDER $CONTAINER_NAME
 
 echo " == COMPILE PACKAGES == "
 sudo docker run -w /zentyal-repo/main -v $ZENTYAL_FOLDER:/zentyal-repo:rw -v $USER_FOLDER/.gnupg://.gnupg:rw --rm $CONTAINER_NAME /zentyal-repo/extra/scripts/zentyal-ppa-build.sh $2
