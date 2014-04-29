d28a5967868cb60123446ccb4a63d3a9f4c1201e
Julio García <jjgarcia@zentyal.com>
2014-04-29 15:30:52 +0200

    Improvements done to the Docker Image and the scripts
    Conflicts:
    	extra/scripts/docker/Dockerfile

--- extra/scripts/docker/run-check-and-tests-in-docker.sh
+++ extra/scripts/docker/run-check-and-tests-in-docker.sh
@@ -4,14 +4,15 @@ set -e
 ZENTYAL_FOLDER=$1
 CONTAINER_NAME="zentyal30"
 
-docker build -t $CONTAINER_NAME $ZENTYAL_FOLDER/extra/scripts/docker/.
+echo " == BUILDING DOCKER IMAGE == "
+$ZENTYAL_FOLDER/extra/scripts/docker/build-docker-image.sh $ZENTYAL_FOLDER $CONTAINER_NAME
 
-sudo rm -rf $ZENTYAL_FOLDER/test
 
 echo " == RUNNING SYNTAX CHECK == "
-docker run -v $ZENTYAL_FOLDER:/zentyal-repo:rw --rm $CONTAINER_NAME /zentyal-repo/extra/scripts/zentyal-syntax-check --update --path=/zentyal-repo --release=precise
-
 sudo rm -rf $ZENTYAL_FOLDER/test
+docker run -v $ZENTYAL_FOLDER:/zentyal-repo:rw --rm $CONTAINER_NAME /zentyal-repo/extra/scripts/zentyal-syntax-check --update --path=/zentyal-repo --release=precise
+sudo rm -rf $ZENTYAL_FOLDER/test
+
 
 echo " == RUNNING UNIT TESTS == "
 docker run -u="testUser" -v $ZENTYAL_FOLDER:/zentyal-repo:rw --rm $CONTAINER_NAME /zentyal-repo/extra/scripts/zentyal-unit-tests -js common > $ZENTYAL_FOLDER/test_results.xml
