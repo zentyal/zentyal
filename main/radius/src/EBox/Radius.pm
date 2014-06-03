905af41591ee8d06a90498f7192cb6a34a4a1cb8
José Antonio Calvo <jacalvo@zentyal.com>
2014-06-04 01:40:34 +0200

    use _regenConfig from EBox::Module::LDAP in radius

--- main/radius/src/EBox/Radius.pm
+++ main/radius/src/EBox/Radius.pm
@@ -17,7 +17,7 @@ use warnings;
 
 package EBox::Radius;
 
-use base qw(EBox::Module::Service EBox::LogObserver);
+use base qw(EBox::Module::LDAP EBox::LogObserver);
 
 use EBox::Global;
 use EBox::Gettext;
