adf2136b2133e5eafa59070910ca3f721c801cf4
Samuel Cabrero <scabrero@zentyal.com>
2014-06-12 11:35:05 +0200

    Link secrets keytab to system-wide keytab

--- main/samba/src/EBox/Samba/Provision.pm
+++ main/samba/src/EBox/Samba/Provision.pm
@@ -284,13 +284,19 @@ sub setupKerberos
 
     EBox::info("Setting up kerberos");
     my $systemFile = EBox::Samba::SYSTEM_WIDE_KRB5_CONF_FILE();
+    my $systemKeytab = EBox::Samba::SYSTEM_WIDE_KRB5_KEYTAB();
+    my $provisionGeneratedKeytab = EBox::Samba::SECRETS_KEYTAB();
     if ($self->isProvisioned()) {
+        if (EBox::Sudo::fileTest('-f', $systemKeytab)) {
+            EBox::Sudo::root("mv '$systemKeytab' '$systemKeytab.bak'");
+        }
         my $samba = EBox::Global->modInstance('samba');
         my $realm = $samba->kerberosRealm();
         my @params = ('realm' => $realm);
         $samba->writeConfFile($systemFile, 'samba/krb5.conf.mas', \@params);
+        EBox::Sudo::root("ln -sf '$provisionGeneratedKeytab' '$systemKeytab'");
     } else {
-        EBox::Sudo::root("rm -f '$systemFile'");
+        EBox::Sudo::root("rm -f '$systemKeytab'");
     }
 }
 
