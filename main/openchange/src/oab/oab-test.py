3871c13071db1ae650ffdfebf929bc6fb93beb95
Javier Amor García <jamor@zentyal.com>
2014-06-18 11:26:36 +0200

    oab wip

---
+++ main/openchange/src/oab/oab-test.py
@@ -0,0 +1,80 @@
+import unittest
+import struct
+from pprint import pprint
+
+import sys
+sys.path.append('.')
+
+class TestOAB(unittest.TestCase):
+    def _check_rdn_consistence(self, rdnContents):
+        rdnContentsSize = len(rdnContents)
+
+        nAccounts = struct.unpack('<I', rdnContents[8:12])
+        self.assertTrue(nAccounts > 0)
+
+        print 'rdnContents[12:16] -> '
+        pprint(rdnContents[12:16])
+
+        oRootBytes = str(bytearray(rdnContents[12:16]))
+        print 'oRootBytes=' + str(type(oRootBytes)) + ' -> '
+        pprint(oRootBytes)
+
+
+#        oRoot = struct.unpack('<I', oRootBytes)[0]
+        oRoot = self._unpack_uint(rdnContents[12:16])
+        print 'oRoot= ' + str(type(oRoot)) + ' -> ' + str(oRoot)
+        self.assertTrue(oRoot > 16) # always will be more that 18(size header)
+
+
+
+        pdnPart = rdnContents[16:oRoot]
+        self.assertTrue(len(pdnPart) > 0)
+
+        pdnByOffset = {}
+        begSearch = 0
+        pdnOffset = 16
+        while begSearch < len(pdnPart):
+             found = pdnPart.find(b'\x00', begSearch)
+             if found == -1:
+                 self.assertTrue(False, msg='Not NUL byte find in pdn search beginning at ' + begSearch)
+             pdnBytes = pdnPart[begSearch:found] # we not use found +1 bz we are not interested in NUL byte
+             pdnOffset += len(pdnBytes) + 1 # +1 -> NUL
+             pdn = pdnBytes.decode(encoding="utf-8", errors="strict")
+             print "found pdn " + pdn # DDD
+             pdnByOffset[pdnOffset] = pdn
+
+        rdnPart = pdnPart[oRoot:]
+        # checking using prev/next links
+        # XXX degenerate tree; tree not checked
+        prevLink = 0
+        nextLink = oRoot
+        while nextLink != 0:
+            oPrev = struct.unpack('<I', rdnPart[nextLink+12:nextLink+16])
+            self.assertTrue(oPrev < rdnContentsSize)
+            self.assertTrue(oPrev == prevLink)
+
+            oNext = struct.unpack('<I', rdnPart[nextLink+16:nextLink+20])
+            self.assertTrue(oNext < rdnContentsSize)
+
+            oParentDN = struct.unpack('<I', rdnPart[nextLink+20:nextLink+24])
+            self.assertTrue(oParentDN in pdnByOffset)
+
+            prevLink = nextLink
+            nextLink = oNext
+
+
+
+    def test_rdn_consistence(self):
+        cases = [
+          bytearray(b'\x0e\x00\x00\x00\x00\x00\x00\x00\x05\x00\x00\x00Y\x00\x00\x00OU=\xd1\x80\xd1\x83\xd1\x81\xd0\xba\xd0\xb8,DC=zentyal-domain,DC=lan\x00CN=Users,DC=zentyal-domain,DC=lan\x00\x00\x00\x00\x00\x80\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x80\x00\x00\x00\x10\x00\x00\x00CN=asdvsdffv a0Y\x00\x00\x00\xa9\x00\x00\x00\x00\x00\x00\x00Y\x00\x00\x00\xa9\x00\x00\x007\x00\x00\x00CN=Administrator0\x80\x00\x00\x00\xcf\x00\x00\x00\x00\x00\x00\x00\x80\x00\x00\x00\xcf\x00\x00\x007\x00\x00\x00CN=sdsdsd dfg0\xa9\x00\x00\x00\xf0\x00\x00\x00\x00\x00\x00\x00\xa9\x00\x00\x00\xf0\x00\x00\x007\x00\x00\x00CN=Guest0\xcf\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\xcf\x00\x00\x00\x00\x00\x00\x007\x00\x00\x00CN=a ad0')
+          ]
+        for case in cases:
+            self._check_rdn_consistence(case)
+
+    def _unpack_uint(self, barray):
+        bstr = str(bytearray(barray[0:4]))
+        return struct.unpack('<I', bstr)[0]
+
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ main/openchange/src/oab/oab.py
@@ -0,0 +1,237 @@
+import struct
+
+# TESTY
+from samba.param import LoadParm
+from samba.auth import system_session
+from samba import samdb, ldb
+from pprint import pprint
+
+
+class OAB:
+    MAX_UL_TOT_RECORDS = 16777212
+
+    def __init__(self):
+        pass
+
+    def createFiles(self, accounts, directory):
+        nAccounts = len(accounts)
+        if (nAccounts == 0):
+            raise Exception('Trying to create OAB files without accounts');
+        elif (nAccounts  > OAB.MAX_UL_TOT_RECORDS):
+            # XXX removing excessive accounts
+            nAccounts = nAccounts[0:OAB.MAX_UL_TOT_RECORDS-1]
+
+        browseFileContents = self._browseFileContents(accounts)
+        print 'Browse file:'
+        pprint(browseFileContents)
+
+        rdnFileContents    = self._rdnFileContents(accounts)
+        print 'RDN file:'
+        pprint(rdnFileContents)
+
+
+    def _browseFileContents(self, accounts):
+            contents = self._browseFileHeader(accounts)
+            for acc in accounts:
+                record  = self._browseRecord(acc)
+                contents += record
+
+            # calculate RDN hash for put in browse file
+            return contents
+
+    def _browseFileHeader(self, accounts):
+        header = bytearray(10)
+        # ulVersion
+        header[0] =  0xA
+        header[1] =  0x0
+        # ulSerial
+        # TODO 2.3.3
+        packedNAccounts = struct.pack('<I', len(accounts))
+        header[4] = packedNAccounts[0]
+        header[5] = packedNAccounts[1]
+        header[6] = packedNAccounts[2]
+        header[7] = packedNAccounts[3]
+        return header
+
+    def _browseRecord(self, account):
+        record = bytearray(32)
+        # TODO oRDN 4
+        # TODO oDetails 4
+        # TODO cbDetails 2
+        # bDispType
+        if account['type'] == 'mailuser':
+            record[10] = 0x0
+        elif account['type'] == 'distlist':
+            record[10] = 0x1
+        else:
+            raise Exception("Unknow account type " + record['type'])
+
+        # a 1b 1 (can receive rich content)
+        if account['SendRichInfo']:
+            record[11] = 0x80
+        else:
+            record[11] = 0x00
+        # mailobj (7b)
+        if account['type'] == 'mailuser':
+            record[11] += 0x06
+        elif account['type'] == 'distlist':
+            record[11] += 0x08
+        else:
+            raise Exception("Unknow account type " + record['type'])
+
+        # oAlias (4 bytes): A 32-bit unsigned integer that specifies the offset of the alias record in the
+        # ANR Index file.
+
+        # oLocation (4 bytes): A 32-bit unsigned integer that specifies the offset of the office location
+        # record in the ANR Index file.
+
+        # oSurname (4 bytes): A 32-bit unsigned integer that specifies the offset of the surname record
+        # in the ANR Index file.
+        return record
+
+    def _rdnFileContents(self, accounts):
+        contents = self._rdnHeader(accounts)
+
+        pdn = self._rdnPdnRecords(accounts, len(contents))
+        pprint(pdn)
+        contents += pdn[1]
+
+        # now we have the offset of the first RDN and we can set oRoot
+        print "oRoot len(ciontnts) _> " + str(len(contents))
+#        oRootPacked = struct.pack('<I', len(contents))
+        oRootPacked = self._pack_uint(len(contents))
+        contents[12:16]  = oRootPacked[0:4]
+
+        oPrev = 0;
+        oNextBase = len(contents)
+        lastAccount = len(accounts) - 1
+        for i in range(0, lastAccount +1):
+            acc = accounts[i]
+            if i == lastAccount:
+                oNextBase = 0
+            record = self._rdnRecord(acc, pdn[0], oPrev, oNextBase)
+            oPrev = len(contents)
+            contents += record
+            oNextBase = len(contents)
+
+        return contents
+
+
+    def _rdnHeader(self, accounts):
+        header = bytearray(16)
+        # ulVersion
+        header[0:4] = 0x0E, 0x00, 0x00, 0x00
+
+        # ulSerial (4 bytes): A 32-bit hexadecimal string that specifies the hash of the RDN (1) values for
+        #   the current set of OAB records. The value of this field is calculated as specified in section
+        # TODO
+
+        # ulTotRecs (4 bytes)
+        packedNAccounts = struct.pack('<I', len(accounts))
+        header[8:12] = packedNAccounts
+
+        # oRoot (4 bytes): A 32-bit unsigned integer that specifies the offset of the root RDN2_REC
+        # to be calculated and set later
+        return header
+
+        # returns the tuple (offsetByPdn, pdnRecordsByteArray)
+    def _rdnPdnRecords(self, accounts, offset):
+        offsetByPdn = {}
+        records = bytearray();
+        for acc in accounts:
+            dn = acc['dn']
+            rdn, pdn = dn.split(',', 1)
+            if pdn in offsetByPdn:
+                continue
+            pdnBytes = bytearray(pdn)
+            pdnBytes.append(0x00);
+            offsetByPdn[pdn] = offset;
+            offset += len(pdnBytes)
+            records += pdnBytes
+
+        return (offsetByPdn, records)
+
+    def _rdnRecord(self, account, offsetByPdn, oPrev, oNextBase):
+        record = bytearray(24) # min size, RDN records are variable
+        rdn, pdn = account['dn'].split(',', 1)
+
+        # XXX degenerate tree: oLT, rLT -> oPrev, oNext
+        # oLT 4b
+        record[0:4] = struct.pack('<I', oPrev)[0:4]
+        # rLT 4b
+        # iBrowse (4 bytes):
+        # TODO
+        # oPrev (4 bytes)
+        record[12:16] = struct.pack('<I', oPrev)[0:4]
+        # oNext (4 bytes)
+        # set later
+
+        # oParentDN (4 bytes)
+        record[20:24] = struct.pack('<I', offsetByPdn[pdn])[0:4]
+
+        # acKey (variable):
+        record +=  bytearray(rdn) + b'0'
+
+        oNext = 0
+        if (oNextBase != 0):
+            oNext = oNextBase + len(record)
+        # rLT
+        record[4:8] = struct.pack('<I', oNext)[0:4]
+        # oNext
+        record[16:20] = struct.pack('<I', oNext)[0:4]
+
+        return record
+
+    def _pack_uint(self, uint):
+        return struct.pack('<I', uint)
+
+
+    def endClass():
+        pass
+
+
+
+
+def accountsList():
+    samdb_url = '/var/lib/samba/private/sam.ldb'
+    db = samdb.SamDB(url=samdb_url, session_info=system_session(), lp=LoadParm())
+
+    accounts = []
+    basedn =  db.domain_dn()
+    res = db.search(base=basedn, scope=ldb.SCOPE_SUBTREE, expression="(|(objectclass=user))(objectclass=group)))")
+
+    for entry in res:
+        mailAttr =  entry.get('mail')
+        if not mailAttr:
+            continue
+
+        account = {}
+        account['mail'] = mailAttr.get(0)
+        account['dn'] = str(entry.dn)
+        account_type = ''
+        for oclass in entry.get('objectclass'):
+            if oclass == "user":
+                account_type = 'mailuser'
+                break;
+            elif oclass == "group":
+                account_type = 'distlist'
+                break
+
+            if account_type == '':
+                # Not valid objectclass!
+                continue
+
+        account['type'] = account_type
+        account['samAccountName']= entry.get('samAccountName').get(0)
+        account['SendRichInfo'] = 1 # for now always on
+
+        accounts.append(account)
+
+    return accounts
+
+
+# test code
+accounts = accountsList()
+pprint(accounts)
+oab = OAB()
+oab.createFiles(accounts, '/tmp')
