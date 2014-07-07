zentyal/zentyal

commit ee986d82909d14cc5e032892acb8545f2fb374b5

avoid race conditions whit captiveportal when restarting firewall

main/captiveportal/src/EBox/CaptivePortalFirewall.pm
@@ -207,4 +207,12 @@ sub _exceptionsRules
     return \@rules;
 }
 
+# we stop captiveportal to avoid race condition with not-yet added captive
+# portal rules
+sub beforeFwRestart
+{
+    my ($self) = @_;
+    $self->{captiveportal}->stopService();
+}
+
 1;

main/firewall/src/EBox/Firewall.pm
@@ -185,6 +185,12 @@ sub _enforceServiceState
     use EBox::Iptables;
     my $ipt = new EBox::Iptables;
     if($self->isEnabled()) {
+        foreach my $mod (@{ $self->global()->modInstancesOfType('EBox::FirewallObserver') }) {
+            my $helper = $mod->firewallHelper();
+            if ($helper) {
+                $helper->beforeFwRestart();
+            }
+        }
         $ipt->start();
     } else {
         $ipt->stop();

main/firewall/src/EBox/FirewallHelper.pm
@@ -300,4 +300,15 @@ sub _inputIface # (iface)
     }
 }
 
+# Method: beforeFwRestart
+#
+#  called before the restart of the firewall when it is enabled
+#
+#  The default implementation does nothing
+#
+sub beforeFwRestart
+{
+}
+
+
 1;

commit 4c933439dc97169211a4f731cede31d40d0cd281

Start captive portal after stopping it
This fixes a regression introduced by ee986d82.

This is done by adding <afterFwRestart> method to firewall helpers

main/captiveportal/src/EBox/CaptivePortalFirewall.pm
@@ -1,4 +1,4 @@
-# Copyright (C) 2011-2013 Zentyal S.L.
+# Copyright (C) 2011-2014 Zentyal S.L.
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License, version 2, as
@@ -207,17 +207,47 @@ sub _exceptionsRules
     return \@rules;
 }
 
-# we stop captiveportal to avoid race condition with not-yet added captive
-# portal rules
+# Method: beforeFwRestart
+#
+#    Stop captiveportal to avoid race condition with not-yet added captive
+#    portal rules
+#
+# Overrides:
+#
+#    <EBox::FirewallHelper::beforeFwRestart>
+#
 sub beforeFwRestart
 {
     my ($self) = @_;
     if ($self->{captiveportal}->needsSaveAfterConfig()) {
-        # not really started and damoen file not configured, no need to stop
+        # not really started and daemon file not configured, no need to stop
         return;
     }
 
-    $self->{captiveportal}->stopService();
+    $self->{captiveportal}->_stopService();
+}
+
+# Method: afterFwRestart
+#
+#    Start captiveportal stopped by <beforeFWRestart> to avoid race
+#    condition with not-yet added captive portal rules
+#
+# Overrides:
+#
+#    <EBox::FirewallHelper::afterFwRestart>
+#
+sub afterFwRestart
+{
+    my ($self) = @_;
+
+    if ($self->{captiveportal}->needsSaveAfterConfig()) {
+        # not really started and daemon file not configured, no need
+        # to start it
+        return;
+    }
+
+    $self->{captiveportal}->_startService();
+
 }
 
 1;

main/firewall/src/EBox/Firewall.pm
@@ -193,6 +193,7 @@ sub _enforceServiceState
 
     EBox::Util::Lock::lock('iptables', 1, BLOCKED_TIMEOUT);
     try {
+        my @helpers = ();
         if ($self->isEnabled()) {
             foreach my $mod (@{ $self->global()->modInstancesOfType('EBox::FirewallObserver') }) {
                 if (not $mod->configured() and not $mod->isEnabled()) {
@@ -201,9 +202,13 @@ sub _enforceServiceState
                 my $helper = $mod->firewallHelper();
                 if ($helper) {
                     $helper->beforeFwRestart();
+                    push(@helpers, $helper);
                 }
             }
             $ipt->start();
+            foreach my $helper (@helpers) {
+                $helper->afterFwRestart();
+            }
         } else {
             $ipt->stop();
         }

main/firewall/src/EBox/FirewallHelper.pm
@@ -1,5 +1,5 @@
 # Copyright (C) 2004-2007 Warp Networks S.L.
-# Copyright (C) 2008-2013 Zentyal S.L.
+# Copyright (C) 2008-2014 Zentyal S.L.
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License, version 2, as
@@ -302,11 +302,21 @@ sub _inputIface # (iface)
 
 # Method: beforeFwRestart
 #
-#  called before the restart of the firewall when it is enabled
+#  called before firewall module restart when it is enabled
 #
 #  The default implementation does nothing
 #
 sub beforeFwRestart
+{
+}
+
+# Method: afterFwRestart
+#
+#  Called after firewall module restart when it is enabled
+#
+#  The default implementation does nothing
+#
+sub afterFwRestart
 {
 }
 