5793a89f5155595d7ff7c40fbea7eb6fda8c12ef
José Antonio Calvo <jacalvo@zentyal.com>
2014-08-23 12:27:44 +0200

    custom class in <body> for first install

--- main/core/src/EBox/Html.pm
+++ main/core/src/EBox/Html.pm
@@ -138,12 +138,6 @@ sub header
 
     my $serverName = __('Zentyal');
     my $global = EBox::Global->getInstance();
-    if ( $global->modExists('remoteservices') ) {
-        my $remoteServicesMod = $global->modInstance('remoteservices');
-        if ( $remoteServicesMod->eBoxSubscribed() ) {
-            $serverName = $remoteServicesMod->eBoxCommonName();
-        }
-    }
 
     if ($title) {
         $title = "$serverName - $title";
@@ -152,7 +146,7 @@ sub header
     }
 
     my $favicon = $global->theme()->{'favicon'};
-    my $html = makeHtml('header.mas', title => $title, favicon => $favicon, folder => $folder);
+    my $html = makeHtml('header.mas', title => $title, favicon => $favicon, folder => $folder, first => $global->first());
     return $html;
 
 }
