540e142b35083754485b31f639485ab390494e24
José Antonio Calvo <jacalvo@zentyal.com>
2014-09-21 18:34:14 +0200

    New welcome page on first install

---
+++ main/software/src/EBox/Software/CGI/Welcome.pm
@@ -0,0 +1,50 @@
+# Copyright (C) 2014 Zentyal S.L.
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License, version 2, as
+# published by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+use strict;
+use warnings;
+
+package EBox::Software::CGI::Welcome;
+
+use base 'EBox::CGI::ClientBase';
+
+use EBox::Global;
+use EBox::Gettext;
+
+sub new
+{
+    my $class = shift;
+    my $self = $class->SUPER::new('title' => __('Zentyal Installation'),
+                                  'template' => 'software/welcome.mas', @_);
+    bless($self, $class);
+    return $self;
+}
+
+sub _menu
+{
+    my ($self) = @_;
+
+    my $software = EBox::Global->modInstance('software');
+    return $software->firstTimeMenu(1);
+}
+
+sub _top
+{
+    my ($self) = @_;
+
+    return $self->_topNoAction();
+}
+
+1;
