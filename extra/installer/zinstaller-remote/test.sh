5a0a557780abb4643f005a186cc578a0b34d9ce9
José Antonio Calvo <jacalvo@zentyal.com>
2014-10-06 11:56:21 +0200

    Revert "changes to debug locally (to be reverted)"
    This reverts commit 92ad4075e50618cefa3a1a2584d078708aa2fe26.
    
    Conflicts:
    	extra/installer/zinstaller-remote/debian/postinst

--- extra/installer/zinstaller-remote/test.sh
+++ extra/installer/zinstaller-remote/test.sh
@@ -2,6 +2,10 @@
 
 sudo true
 
+sed -i "s:^db_get netcfg/get_hostname:RET=$HOSTNAME:" debian/postinst
+sed -i "/udeb/d" debian/control
+sed -i "/XB-Installer/d" debian/control
+
 dpkg-buildpackage
 
 echo PURGE | sudo debconf-communicate zinstaller-remote
