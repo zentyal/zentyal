69161b775f26a4df5d64bff518d8df99006e53e5
José Antonio Calvo <jacalvo@zentyal.com>
2014-10-10 18:00:55 +0200

    fixed typo

--- extra/installer/scripts/10_custom_ubuntu-keyring.sh
+++ extra/installer/scripts/10_custom_ubuntu-keyring.sh
@@ -35,7 +35,7 @@ sudo rm squashfs-root/ubuntu-keyring*.deb
 
 # Add https apt method to be able to retrieve from QA updates repo
 mkdir -p squashfs-root/usr/lib/apt/methods
-sudo cp /usr/lib/apt/methods/https sqashfs-root/usr/lib/apt/methods
+sudo cp /usr/lib/apt/methods/https squashfs-root/usr/lib/apt/methods/
 
 sudo mksquashfs squashfs-root filesystem.squashfs
 mv -f filesystem.squashfs $CD_BUILD_DIR/install/
