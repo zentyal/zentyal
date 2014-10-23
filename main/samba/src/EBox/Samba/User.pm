88d508b239a74cb442e8b18bb86202c3a07319e3
Julio García <jjgarcia@zentyal.com>
2014-10-23 18:25:16 +0200

    Fix realUsers count to remove Administrator and Guest account from it

--- main/samba/src/EBox/Samba/User.pm
+++ main/samba/src/EBox/Samba/User.pm
@@ -658,11 +658,17 @@ sub isInternal
 
     # FIXME: whitelist Guest account, Administrator account
     # do this better removing isCriticalSystemObject check
-    if ( ($self->sid() =~ /^S-1-5-21-.*-501$/) or ($self->sid() =~ /^S-1-5-21-.*-500$/) ) {
+    if ( $self->isAdministratorOrGuest() ) {
         return 0;
     }
 
     return ($self->isInAdvancedViewOnly() or $self->get('isCriticalSystemObject'));
+}
+
+sub isAdministratorOrGuest
+{
+    my ($self) = @_;
+    return ($self->sid() =~ /^S-1-5-21-.*-501$/) or ($self->sid() =~ /^S-1-5-21-.*-500$/)
 }
 
 sub setInternal
