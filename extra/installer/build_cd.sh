11a6feca3845739719e2248a783c515c15de5ea9
José Antonio Calvo <jacalvo@zentyal.com>
2014-12-15 13:30:00 +0100

    remove no longer needed workaround that in fact causes regression

--- extra/installer/build_cd.sh
+++ extra/installer/build_cd.sh
@@ -79,11 +79,6 @@ do
         mkdir -p $UDEB_DIR
         rm $UDEB_DIR/*
         cp $TMPDIR/zinstaller-remote*.udeb $UDEB_DIR/
-
-        # FIXME: workaround until https://bugs.launchpad.net/ubuntu/+source/curl/+bug/1312241 is fixed
-        UDEB_DIR=$CD_BUILD_DIR/pool/main/c/curl
-        cp ~/build/curl-udeb_7.35.0-1ubuntu2+ccu1_amd64.udeb $UDEB_DIR/curl-udeb_7.35.0-1ubuntu2_amd64.udeb
-        cp ~/build/libcurl3-udeb_7.35.0-1ubuntu2+ccu1_amd64.udeb $UDEB_DIR/libcurl3-udeb_7.35.0-1ubuntu2_amd64.udeb
     fi
 
     # Add zinstaller-headless udeb
