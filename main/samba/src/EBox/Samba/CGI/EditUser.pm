6fc7bd67ff314144a052d149bb39c4a99d5086a2
Javier Amor García <jamor@zentyal.com>
2015-12-11 15:44:04 +0100

    Add systemQuotas object class when setting user quota
    When adding users by means other than Zentyal interface is frequent
    that users are missing the systemQuotas object class.
    Without this class the user quota cannot be set, so this changeset
    make sure to add it if needed.

--- main/samba/src/EBox/Samba/CGI/EditUser.pm
+++ main/samba/src/EBox/Samba/CGI/EditUser.pm
@@ -75,7 +75,10 @@ sub _process
         } elsif ($quotaTypeSelected eq 'quota_size') {
             $quota = $self->param('User_quota_size');
         }
-        if (defined ($quota) and $user->hasValue('objectClass', 'systemQuotas')) {
+        if (defined $quota) {
+            if (not $user->hasValue('objectClass', 'systemQuotas')) {
+                $user->add('objectClass', 'systemQuotas');
+            }
             $user->set('quota', $quota, 1);
         }
 
