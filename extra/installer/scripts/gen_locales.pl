3975a7d799ab07ecd1cb80a7f6f28dfc989e558c
José Antonio Calvo <jacalvo@zentyal.com>
2016-10-20 01:36:21 +0200

    remove zentyal-common dependency on gen_locales script

--- extra/installer/scripts/gen_locales.pl
+++ extra/installer/scripts/gen_locales.pl
@@ -18,8 +18,6 @@
 use strict;
 use warnings;
 
-use EBox::Gettext;
-
 use constant FILE_NAME => 'locale.gen';
 
 my ($dstDir) = @ARGV;
@@ -30,12 +28,47 @@ if (not $dstDir) {
 
 my $path = "$dstDir/" . FILE_NAME;
 
-my $langs = EBox::Gettext::langs();
-
 open my $FH, ">$path" or
 die  "Cannot open $path: $!";
 
-my @locales = sort keys %{ $langs };
+my @locales = qw(
+    an_ES.UTF-8
+    bn_BD.UTF-8
+    bg_BG.UTF-8
+    es_ES.UTF-8
+    et_EE.UTF-8
+    ca_ES.UTF-8
+    cs_CZ.UTF-8
+    da_DK.UTF-8
+    de_DE.UTF-8
+    el_GR.UTF-8
+    en_US.UTF-8
+    eu_ES.UTF-8
+    fa_IR.UTF-8
+    fr_FR.UTF-8
+    gl_ES.UTF-8
+    he_HE.UTF-8
+    hu_HU.UTF-8
+    it_IT.UTF-8
+    ja_JP.UTF-8
+    lt_LT.UTF-8
+    nb_NO.UTF-8
+    nl_BE.UTF-8
+    pl_PL.UTF-8
+    pt_BR.UTF-8
+    pt_PT.UTF-8
+    ro_RO.UTF-8
+    ru_RU.UTF-8
+    sl_SL.UTF-8
+    sv_SE.UTF-8
+    th_TH.UTF-8
+    tr_TR.UTF-8
+    uk_UA.UTF-8
+    vi_VI.UTF-8
+    yo_NG.UTF-8
+    zh_CN.UTF-8
+    zh_TW.UTF-8
+);
 
 foreach my $locale (@locales) {
     if ($locale eq 'C') {
