67ec931912772521127055edc69438e4bd24e01a
José Antonio Calvo <jacalvo@zentyal.com>
2017-02-08 10:15:04 +0100

    drop remoteservices for old unsupported versions
    affects 2.0, 2.2, 3.0, 3.3, 3.4, 3.5

--- main/remoteservices/schemas/sql/remoteservices_passwd_report.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE IF NOT EXISTS remoteservices_passwd_report (
-    timestamp TIMESTAMP,
-    username VARCHAR(256),
-    level ENUM ('weak', 'average'),
-    source ENUM ('LDAP', 'system'),
-    fullname VARCHAR(512),
-    email VARCHAR(512),
-    INDEX(timestamp)
-) ENGINE = MyISAM;
