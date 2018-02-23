567cecefe72cad215f9264ad02392c283d752869
José Antonio Calvo <jacalvo@zentyal.com>
2018-02-23 10:56:55 +0100

    copy thumbnail photos to share to avoid conflicts with cron

--- main/samba/src/EBox/Samba.pm
+++ main/samba/src/EBox/Samba.pm
@@ -808,7 +808,7 @@ sub _installPhotoCronFile
 {
     my ($self) = @_;
 
-    my $share = EBox::Config::configkey('photo_share_path');
+    my $share = EBox::Config::configkey('photo_share_name');
 
     return unless ($share);
 
