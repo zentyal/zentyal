3c232ce0bea1ab9b3435b06ba362812f3abdcf2a
José Antonio Calvo <jacalvo@zentyal.com>
2018-03-18 11:16:05 +0100

    Disable firefox data reporting on default profile

--- extra/installer/zenbuntu-desktop/firefox-profile/ebox.default/prefs.js
+++ extra/installer/zenbuntu-desktop/firefox-profile/ebox.default/prefs.js
@@ -3,5 +3,6 @@ user_pref("browser.startup.homepage", "https://localhost");
 user_pref("browser.startup.homepage_override.mstone", "rv:1.9.0.18");
 user_pref("browser.tabs.autoHide", true);
 user_pref("browser.rights.3.shown", true);
+user_pref("datareporting.policy.dataSubmissionEnabled", false);
 user_pref("extensions.testpilot@labs.mozilla.install-event-fired", true);
 user_pref("extensions.testpilot.alreadyCustomizedToolbar", true);
