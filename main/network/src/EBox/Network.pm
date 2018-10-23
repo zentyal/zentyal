289858d7d01c4088695091f0f7638f1e76beacef
José Antonio Calvo <jacalvo@zentyal.com>
2018-10-23 16:14:17 +0200

    Revert "proper remove of systemd-resolved resolvconf interface"
    This reverts commit 64546a0c40b34144dd790fe089b228eff3297541.

--- main/network/src/EBox/Network.pm
+++ main/network/src/EBox/Network.pm
@@ -228,6 +228,7 @@ sub initialSetup
 
     EBox::Sudo::silentRoot('systemctl stop systemd-resolved');
     EBox::Sudo::silentRoot('systemctl disable systemd-resolved');
+    EBox::Sudo::silentRoot('resolvconf -d systemd-resolved');
     EBox::Sudo::silentRoot('rm -f /etc/dhcp/dhclient-enter-hooks.d/resolved');
 
     foreach my $service (@{$self->_defaultServices()}) {
@@ -3087,8 +3088,6 @@ sub _setChanged # (interface)
 sub _generateResolvconfConfig
 {
     my ($self) = @_;
-
-    EBox::Sudo::silentRoot('resolvconf -d systemd-resolved');
 
     # Generate base, head and tail
     $self->writeConfFile(RESOLVCONF_BASE,
