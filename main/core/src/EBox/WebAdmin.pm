99dcd76914b8064b77f4414598adf3dfa5f3a6f8
Bruno Lorente Cantarero <blorente@novadevs.com>
2020-06-03 11:09:58 +0200

    add the "extra" component to sources.list

--- main/core/src/EBox/WebAdmin.pm
+++ main/core/src/EBox/WebAdmin.pm
@@ -601,7 +601,7 @@ sub _setEdition
             chomp ($hash);
         }
         push (@cmds,
-            "echo 'deb http://packages.zentyal.com/zentyal-qa $version main' > /etc/apt/sources.list.d/zentyal-qa.list",
+            "echo 'deb http://packages.zentyal.com/zentyal-qa $version main extra' > /etc/apt/sources.list.d/zentyal-qa.list",
             "echo 'machine packages.zentyal.com login $lk password $hash' > /etc/apt/auth.conf",
             'chmod 600 /etc/apt/auth.conf',
             'sed -i "/packages.zentyal/d" /etc/apt/sources.list'
