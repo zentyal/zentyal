3bcb9bd6f894d2893e06b24e7d568b29c8417667
Bruno Lorente Cantarero <blorente@novadevs.com>
2023-07-07 21:55:20 +0200

    Remove old installer. Save and update needed packages and scripts. Add new repos to core.

--- extra/zenbuntu-core/second-boot.sh
+++ extra/zenbuntu-core/second-boot.sh
@@ -1,19 +1,18 @@
 #!/bin/bash
-
 # If Zentyal is already installed...
 if ! [ -f '/var/lib/zentyal/.first' ]
 then
     # Disable auto login once installation is done
     sed -i "s/.*autologin=.*/# autologin=nobody/" /etc/lxdm/default.conf
-
     # Remove temporal local repository
-    sed -i "/deb.*file.*zentyal-packages/d" /etc/apt/sources.list
-    rm -rf /var/tmp/zentyal-packages
-    apt-get clean
-
+    if [ -f '/etc/apt/sources.list.d/zentyal-temporal.list' ]
+    then
+        rm -f /etc/apt/sources.list.d/zentyal-temporal.list
+        rm -rf /var/tmp/zentyal-packages/
+        apt clean
+    fi
     # Restore default rc.local and execute it
     cp /usr/share/zenbuntu-core/rc.local /etc/rc.local
     . /etc/rc.local
 fi
-
 exit 0
