c78120e9407bd827da7c221122b1da0887df762b
djoven89 <smrdaniel30@gmail.com>
2024-02-15 09:51:56 +0100

    Increase size of the username field in samba_access table

--- main/samba/schemas/sql/samba_access.sql
+++ main/samba/schemas/sql/samba_access.sql
@@ -1,6 +1,6 @@
 CREATE TABLE IF NOT EXISTS samba_access (
     client INT UNSIGNED,
-    username VARCHAR(24),
+    username VARCHAR(50),
     resource VARCHAR(240),
     event VARCHAR(16),
     timestamp TIMESTAMP,
