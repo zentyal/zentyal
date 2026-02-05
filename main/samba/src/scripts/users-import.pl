cc8941eb227d93a4d65eb6573a1e07191bed2fdf
djoven89 <smrdaniel30@gmail.com>
2026-02-05 09:49:43 +0100

    Update import and export scripts

--- main/samba/src/scripts/users-import.pl
+++ main/samba/src/scripts/users-import.pl
@@ -1,4 +1,5 @@
 #!/usr/bin/perl
+
 use strict;
 use warnings;
 
@@ -13,39 +14,53 @@ use TryCatch;
 
 EBox::init();
 
-sub createLDAPUsers 
+sub createLDAPUsers
 {
     my(@lines) = @_;
 
     for my $line(@lines) {
-        my($samAccountName, $parentDN, $givenName, $initials, $sn, $displayName, $description, $mail, $password, $isSystemUser, $uid, $groups) = split(';', $line);
+        my (
+            $samAccountName,
+            $parentDN,
+            $givenName,
+            $sn,
+            $initials,
+            $displayName,
+            $description,
+            $mail,
+            $password,
+            $groups
+        ) = split(';', $line);
+
+        next if $line =~ /^\s*$/;   # Is empty
+        next if $line =~ /^\s*#/;   # Is comment
+
 	    try {
             my $user = EBox::Samba::User->create(
-                samAccountName => $samAccountName, 
+                samAccountName => $samAccountName,
                 parent => getLDAPContainer($parentDN),
                 givenName => $givenName,
                 initials => $initials,
                 sn => $sn,
                 displayName => $displayName,
                 description => $description,
-                mail => $mail,
                 name => $samAccountName,
                 password => $password,
             );
-            my $global = EBox::Global->getInstance();
-            my $mod = $global->modExists('mail') ? $global->modInstance('mail') : $global->modInstance('samba');
-            $mod->checkMailNotInUse($mail);
-            $user->set('mail', $mail, 1);
-            $user->save();
-            print "$samAccountName OK\n";
+            print "Domain user '$samAccountName' imported successfully.";
+
+    	    addToGroup($samAccountName, $groups);
+
+            if ($mail) {
+                set_user_mail($user, $mail);
+            }
         } catch ($e){
-            warn "Caught error: $e";
+            warn "Failed to import the domain user '$samAccountName': $e\n";
         }
-	    addToGroup($samAccountName, $groups);
     }
 }
 
-sub getLDAPContainer 
+sub getLDAPContainer
 {
     my ($parentDN) = @_;
     my $container;
@@ -53,7 +68,7 @@ sub getLDAPContainer
         $container = EBox::Samba::Container->new( dn => $parentDN );
     }
     catch ($e) {
-	    print "$e\n"; 
+	    warn "Failed to get LDAP container for DN '$parentDN': $e\n";
         $container = EBox::Samba::User->defaultContainer();
 	    print "LDAP Object with DN $parentDN not found, giving default container: " . $container->dn() . "\n";
     }
@@ -61,30 +76,72 @@ sub getLDAPContainer
     return $container;
 }
 
-sub addToGroup 
-{ 
+sub addToGroup
+{
     my ($user, $groupsString) = @_;
+
+    return unless $groupsString;
+
     my @groups = split(/:/,$groupsString);
+
     foreach my $group (@groups) {
-        my $g = EBox::Samba::Group->new( dn => $group );
-        my $u = EBox::Samba::User->new( samAccountName => $user);
         try {
-                $g->addMember($u);
+            my $g = EBox::Samba::Group->new( dn => $group );
+            my $u = EBox::Samba::User->new( samAccountName => $user);
+            $g->addMember($u);
+            print "Domain user '$user' added to group '$group'\n";
         } catch ($e) {
-            warn "$e\n";
+            warn "Cannot add user to group '$group': $e\n";
         }
-        print "$user added to $group\n";
     }
 }
 
-sub readCSV 
+sub set_user_mail {
+    my ($user, $mail) = @_;
+
+    my $global = EBox::Global->getInstance();
+
+    if ($global->modExists('mail') && $global->modInstance('mail')->isEnabled()) {
+        setMailWithMail($user, $mail);
+    } else {
+        setMailWithSamba($user, $mail);
+    }
+}
+
+sub setMailWithMail {
+    my ($user, $mail) = @_;
+
+    my $global = EBox::Global->getInstance();
+    my $mod = $global->modInstance('mail');
+    my $mailUser = EBox::MailUserLdap->new();
+
+    $mod->checkMailNotInUse($mail);
+
+    $mailUser->setUserAccount($user, $mail);
+    print "\nMail '$mail' assigned using Mail module to the domain user '$user'\n";
+}
+
+sub setMailWithSamba {
+    my ($user, $mail) = @_;
+
+    my $global = EBox::Global->getInstance();
+    my $mod = $global->modInstance('samba');
+
+    $mod->checkMailNotInUse($mail);
+
+    $user->set('mail', $mail, 1);
+    $user->save();
+    print "\nMail '$mail' assigned using Samba module to the domain user '$user'\n";
+}
+
+sub readCSV
 {
     my($p) = getPath(@_);
     my @lines = read_file($p);
     createLDAPUsers(@lines);
 }
 
-sub getPath 
+sub getPath
 {
     my($path) = @_;
     $path = abs_path($path);
@@ -92,14 +149,14 @@ sub getPath
     return $path;
 }
 
-sub getParms 
+sub getParms
 {
     my(@args) = @_;
-    if (scalar @args < 1 or scalar @args > 1) {
-        print "Usage: ./user-importer <source-file> \n";
-    } else {
-        readCSV($args[0]);
-    }
+
+    die "Usage: $0 <source-file>\n" unless @args == 1;
+
+    print "Importing users from file: $args[0]\n";
+    readCSV($args[0]);
 }
 
 getParms(@ARGV);
